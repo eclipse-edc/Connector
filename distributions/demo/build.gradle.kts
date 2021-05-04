0d21de5f24c01f9590d59b129f84552a15315afb
Jim Marino <jim.marino@gmail.com>
2021-05-04 10:18:19 +0200

    Switch back to OAuth as default

--- distributions/demo/build.gradle.kts
+++ distributions/demo/build.gradle.kts
@@ -26,8 +26,8 @@ dependencies {
     implementation(project(":extensions:demo:demo-nifi"))
 
     implementation(project(":extensions:security:security-fs"))
-    implementation(project(":extensions:iam:iam-mock"))
-//    implementation(project(":extensions:iam:oauth2"))
+//    implementation(project(":extensions:iam:iam-mock"))
+    implementation(project(":extensions:iam:oauth2"))
     implementation(project(":extensions:configuration:configuration-fs"))
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
