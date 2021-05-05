792e1a3a781df6389f40e05407d52fb19cd806bb
Jim Marino <jim.marino@gmail.com>
2021-05-05 10:07:03 +0200

    Fix race condition

--- integration/integration-core/src/test/java/ClientRunner.java
+++ integration/integration-core/src/test/java/ClientRunner.java
@@ -32,29 +32,31 @@ public class ClientRunner {
     @Test
     void processClientRequest(TransferProcessManager processManager) throws InterruptedException {
 
-        EasyMock.expect(identityService.obtainClientCredentials(EasyMock.isA(String.class))).andReturn(US_TOKEN);
-        EasyMock.expect(identityService.obtainClientCredentials(EasyMock.isA(String.class))).andReturn(EU_TOKEN);
+        EasyMock.expect(identityService.obtainClientCredentials(EasyMock.isA(String.class))).andReturn(US_TOKEN).anyTimes();
         EasyMock.replay(identityService);
 
         CountDownLatch latch = new CountDownLatch(1);
 
         // Initiate a request as a U.S.-based connector for an EU-restricted artifact (will be denied)
-        DataRequest usRequest = DataRequest.Builder.newInstance().id("us-request").protocol("ids-rest").dataEntry(EU_ARTIFACT).connectorId(PROVIDER_CONNECTOR).connectorAddress(PROVIDER_CONNECTOR).destinationType(S3BucketSchema.TYPE).build();
+        var usRequest = createRequest("us-request", EU_ARTIFACT);
 
         processManager.initiateClientRequest(usRequest);
 
         // Initiate a request as a U.S.-based connector for an EU or US allowed artifact (will be accepted)
-        DataRequest usOrEuRequest = DataRequest.Builder.newInstance().id("us-eu--request").protocol("ids-rest").dataEntry(US_OR_EU_ARTIFACT).connectorId(PROVIDER_CONNECTOR).connectorAddress(PROVIDER_CONNECTOR).destinationType(S3BucketSchema.TYPE).build();
+        var usOrEuRequest = createRequest("us-eu-request", US_OR_EU_ARTIFACT);
 
         processManager.initiateClientRequest(usOrEuRequest);
 
         latch.await(1, TimeUnit.DAYS);
     }
 
-
     @BeforeEach
     void before(DagxExtension extension) {
         identityService = EasyMock.createMock(IdentityService.class);
         extension.registerSystemExtension(ServiceExtension.class, TestExtensions.mockIamExtension(identityService));
+    }
+
+    private DataRequest createRequest(String id, DataEntry<?> artifactId) {
+        return DataRequest.Builder.newInstance().id(id).protocol("ids-rest").dataEntry(artifactId).connectorId(PROVIDER_CONNECTOR).connectorAddress(PROVIDER_CONNECTOR).destinationType(S3BucketSchema.TYPE).build();
     }
 }
