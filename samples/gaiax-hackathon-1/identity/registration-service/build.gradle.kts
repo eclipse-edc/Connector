bb733b0d99061329b9355491775af614ea99c161
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-08-12 11:40:19 +0200

    renamed ion impl. folder

--- samples/gaiax-hackathon-1/identity/registration-service/build.gradle.kts
+++ samples/gaiax-hackathon-1/identity/registration-service/build.gradle.kts
@@ -9,7 +9,7 @@ val rsApi: String by project
 dependencies {
     api(project(":spi"))
     implementation(project(":common:util"))
-    implementation(project(":samples:gaiax-hackathon-1:identity:ion"))
+    implementation(project(":samples:gaiax-hackathon-1:identity:ion-core"))
     implementation("org.quartz-scheduler:quartz:2.3.0")
 
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
