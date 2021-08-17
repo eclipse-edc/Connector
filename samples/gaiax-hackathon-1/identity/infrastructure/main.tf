78386d8716eb745e7c353982647406e060b4b253
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-08-17 17:05:24 +0200

    added provider and junit-consumer capabilities

--- samples/gaiax-hackathon-1/identity/infrastructure/main.tf
+++ samples/gaiax-hackathon-1/identity/infrastructure/main.tf
@@ -234,10 +234,39 @@ resource "azurerm_container_group" "gx-registration-service" {
 //}
 
 ## KEYVAULT SECRETS
+
+# KeyVault secrets
 resource "azurerm_key_vault_secret" "blobstorekey" {
   name = "${azurerm_storage_account.main-blobstore.name}-key1"
   value = azurerm_storage_account.main-blobstore.primary_access_key
   key_vault_id = azurerm_key_vault.main-vault.id
+  depends_on = [
+    azurerm_role_assignment.current-user]
+}
+
+resource "azurerm_key_vault_secret" "aws-keyid" {
+  name = "dataspaceconnector-aws-access-key"
+  value = aws_iam_access_key.gx_access_key.id
+  key_vault_id = azurerm_key_vault.main-vault.id
+  depends_on = [
+    azurerm_role_assignment.current-user]
+}
+
+resource "azurerm_key_vault_secret" "aws-secret" {
+  name = "dataspaceconnector-aws-secret-access-key"
+  value = aws_iam_access_key.gx_access_key.secret
+  key_vault_id = azurerm_key_vault.main-vault.id
+  depends_on = [
+    azurerm_role_assignment.current-user]
+}
+
+resource "azurerm_key_vault_secret" "aws-credentials" {
+  key_vault_id = azurerm_key_vault.main-vault.id
+  name = "aws-credentials"
+  value = jsonencode({
+    "accessKeyId" = aws_iam_access_key.gx_access_key.id,
+    "secretAccessKey" = aws_iam_access_key.gx_access_key.secret
+  })
   depends_on = [
     azurerm_role_assignment.current-user]
 }
