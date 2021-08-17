6390ced69e9f99b09cb984ac11c3dfe80a36092a
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-08-17 11:51:16 +0200

    query real ion api instead of randomized stuff

--- samples/gaiax-hackathon-1/identity/registration-service/src/main/java/org/eclipse/dataspaceconnector/samples/identity/registrationservice/crawler/CrawlerJob.java
+++ samples/gaiax-hackathon-1/identity/registration-service/src/main/java/org/eclipse/dataspaceconnector/samples/identity/registrationservice/crawler/CrawlerJob.java
@@ -1,50 +1,92 @@
 package org.eclipse.dataspaceconnector.samples.identity.registrationservice.crawler;
 
 import com.nimbusds.jose.jwk.ECKey;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import org.eclipse.dataspaceconnector.iam.ion.IonRequestException;
 import org.eclipse.dataspaceconnector.iam.ion.crypto.KeyPairFactory;
 import org.eclipse.dataspaceconnector.iam.ion.dto.did.DidDocument;
 import org.eclipse.dataspaceconnector.iam.ion.dto.did.Service;
-import org.eclipse.dataspaceconnector.iam.ion.spi.DidStore;
-import org.eclipse.dataspaceconnector.samples.identity.registrationservice.events.CrawlerEventPublisher;
-import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.jetbrains.annotations.Nullable;
 import org.quartz.Job;
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 
+import java.io.IOException;
 import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.String.format;
 
 // has to be "public", otherwise quartz won't be able to access is
 public class CrawlerJob implements Job {
+
+    private static final String IDENTIFIERS_PATH = "identifiers";
+    private String ionApiUrl = "http://23.97.144.59:3000/";
+
     @Override
     public void execute(JobExecutionContext context) {
-        var store = (DidStore) context.getJobDetail().getJobDataMap().get("STORE");
-        var monitor = (Monitor) context.getJobDetail().getJobDataMap().get("MONITOR");
-        var publisher = (CrawlerEventPublisher) context.getJobDetail().getJobDataMap().get("PUBLISHER");
+        JobDataMap jobDataMap = context.getJobDetail().getJobDataMap();
+        var cc = (CrawlerContext) jobDataMap.get(CrawlerContext.KEY);
+
+        if (cc.getIonHost() != null) {
+            ionApiUrl = cc.getIonHost().toString();
+        }
 
 
         // get latest did document to obtain continuation token
-        var latestDocument = store.getLatest();
+        var latestDocument = cc.getDidStore().getLatest();
 
 
         var continuationToken = latestDocument != null ? latestDocument.getId() : null;
 
+        var monitor = cc.getMonitor();
         monitor.info("CrawlerJob: browsing ION to obtain new DIDs" + (continuationToken != null ? ", starting at " + continuationToken : ""));
-        var newDids = getNewDidsFromIon(continuationToken);
+        var newDids = getDidsSince(continuationToken, cc.getDidTypes());
         monitor.info("CrawlerJob: found " + newDids.size() + " new dids on ION");
 
         if (newDids.size() > 0) {
-            publisher.discoveryFinished(newDids.size());
+            cc.getPublisher().discoveryFinished(newDids.size());
         }
 
-        store.saveAll(newDids);
+        cc.getDidStore().saveAll(newDids);
 
     }
 
-    private List<DidDocument> getNewDidsFromIon(@Nullable String continuationToken) {
+    private List<DidDocument> getDidsSince(@Nullable String continuationToken, String... types) {
+        var client = createClient();
+
+        var url = HttpUrl.parse(ionApiUrl)
+                .newBuilder()
+                .addPathSegment(IDENTIFIERS_PATH)
+                .addQueryParameter("since", continuationToken); //doesn't matter if its null or empty
+
+        Arrays.stream(types).forEach(t -> url.addQueryParameter("type", t));
+
+        var request = new Request.Builder()
+                .url(url.build().url())
+                .get()
+                .build();
+
+        try (var response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                var json = Objects.requireNonNull(response.body()).string();
+                return new ArrayList<>();
+            } else {
+                throw new IonRequestException(format("Could not get DIDs: error=%s, message=%s", response.code(), response.body().string()));
+            }
+        } catch (IOException e) {
+            throw new IonRequestException(e);
+        }
+    }
+
+    private OkHttpClient createClient() {
+        return new OkHttpClient.Builder().connectTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).build();
+    }
+
+    private List<DidDocument> getRandomizedDid(@Nullable String continuationToken) {
 
 
         // TODO: once the query API for Ion is in place, we need to actually query it
