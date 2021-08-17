d7c82347c0d79437283dca7b305a0f206b092d9b
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-08-17 17:46:48 +0200

    azure blob -> s3 works

--- samples/gaiax-hackathon-1/identity/cloud-transfer/src/main/java/org/eclipse/dataspaceconnector/transfer/BlobStoreWriter.java
+++ samples/gaiax-hackathon-1/identity/cloud-transfer/src/main/java/org/eclipse/dataspaceconnector/transfer/BlobStoreWriter.java
@@ -1,10 +1,16 @@
 package org.eclipse.dataspaceconnector.transfer;
 
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+import org.eclipse.dataspaceconnector.spi.types.TypeManager;
 import org.eclipse.dataspaceconnector.spi.types.domain.transfer.DataAddress;
 
 class BlobStoreWriter implements DataWriter {
+    public BlobStoreWriter(Monitor monitor, TypeManager typeManager) {
+
+    }
+
     @Override
-    public void write(DataAddress destination, byte[] data) {
+    public void write(DataAddress destination, String name, byte[] data, String secretToken) {
 
     }
 }
