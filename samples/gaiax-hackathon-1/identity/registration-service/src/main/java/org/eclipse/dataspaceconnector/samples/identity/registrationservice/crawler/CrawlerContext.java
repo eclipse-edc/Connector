0ca1b57c1598ea4bd7eac1c0bdff2eea50d566cc
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-08-17 11:51:16 +0200

    query real ion api instead of randomized stuff

---
+++ samples/gaiax-hackathon-1/identity/registration-service/src/main/java/org/eclipse/dataspaceconnector/samples/identity/registrationservice/crawler/CrawlerContext.java
@@ -0,0 +1,86 @@
+package org.eclipse.dataspaceconnector.samples.identity.registrationservice.crawler;
+
+import org.eclipse.dataspaceconnector.iam.ion.dto.did.DidDocument;
+import org.eclipse.dataspaceconnector.iam.ion.spi.DidStore;
+import org.eclipse.dataspaceconnector.samples.identity.registrationservice.events.CrawlerEventPublisher;
+import org.eclipse.dataspaceconnector.spi.iam.ObjectStore;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+
+public class CrawlerContext {
+    public static final String KEY = "edc:ion-crawler:config";
+    private DidStore didStore;
+    private Monitor monitor;
+    private CrawlerEventPublisher publisher;
+    private String ionHost;
+    private String[] didTypes;
+
+    public DidStore getDidStore() {
+        return didStore;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public CrawlerEventPublisher getPublisher() {
+        return publisher;
+    }
+
+    public String getIonHost() {
+        return ionHost;
+    }
+
+    public String[] getDidTypes() {
+        return didTypes;
+    }
+
+    public static final class Builder {
+        private ObjectStore<DidDocument> didStore;
+        private Monitor monitor;
+        private CrawlerEventPublisher publisher;
+        private String ionHost;
+        private String[] didTypes;
+
+        private Builder() {
+        }
+
+        public static Builder create() {
+            return new Builder();
+        }
+
+        public Builder didStore(ObjectStore<DidDocument> didStore) {
+            this.didStore = didStore;
+            return this;
+        }
+
+        public Builder monitor(Monitor monitor) {
+            this.monitor = monitor;
+            return this;
+        }
+
+        public Builder publisher(CrawlerEventPublisher publisher) {
+            this.publisher = publisher;
+            return this;
+        }
+
+        public Builder ionHost(String ionHost) {
+            this.ionHost = ionHost;
+            return this;
+        }
+
+        public Builder didTypes(String[] didTypes) {
+            this.didTypes = didTypes;
+            return this;
+        }
+
+        public CrawlerContext build() {
+            CrawlerContext crawlerConfig = new CrawlerContext();
+            crawlerConfig.didTypes = didTypes;
+            crawlerConfig.ionHost = ionHost;
+            crawlerConfig.publisher = publisher;
+            crawlerConfig.didStore = (DidStore) didStore;
+            crawlerConfig.monitor = monitor;
+            return crawlerConfig;
+        }
+    }
+}
