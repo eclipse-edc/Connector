a21d1719d4fbb1daa51d24d5435f2520a641b850
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-08-18 13:01:49 +0200

    added deployment for consumer connector

--- samples/gaiax-hackathon-1/identity/infrastructure/main.tf
+++ samples/gaiax-hackathon-1/identity/infrastructure/main.tf
@@ -1,34 +1,34 @@
 # Configure the Azure provider
 terraform {
   backend "azurerm" {
-    resource_group_name = "edc-infrastructure"
+    resource_group_name  = "edc-infrastructure"
     storage_account_name = "edcstate"
-    container_name = "terraform-state-hackathon"
-    key = "terraform.state"
+    container_name       = "terraform-state-hackathon"
+    key                  = "terraform.state"
   }
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 2.72.0"
     }
     azuread = {
-      source = "hashicorp/azuread"
+      source  = "hashicorp/azuread"
       version = ">=1.6.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = ">=2.0.3"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
       version = ">= 2.1.0"
     }
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "3.45.0"
     }
     http = {
-      source = "hashicorp/http"
+      source  = "hashicorp/http"
       version = ">=2.1.0"
     }
   }
@@ -37,7 +37,7 @@ terraform {
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy = true
+      purge_soft_delete_on_destroy    = true
       recover_soft_deleted_key_vaults = false
     }
   }
@@ -68,88 +68,88 @@ data "azurerm_subscription" "primary" {}
 
 
 resource "azurerm_resource_group" "core-resourcegroup" {
-  name = "${var.environment}-resources"
+  name     = "${var.environment}-resources"
   location = var.location
   tags = {
-    "Environment": "Hackathon"
+    "Environment" : "Hackathon"
   }
 }
 
 # App registration for the primary identity
 resource "azuread_application" "demo-app-id" {
-  display_name = "PrimaryIdentity-${var.environment}"
+  display_name               = "PrimaryIdentity-${var.environment}"
   available_to_other_tenants = false
 }
 
 resource "azuread_application_certificate" "demo-main-identity-cert" {
-  type = "AsymmetricX509Cert"
+  type                  = "AsymmetricX509Cert"
   application_object_id = azuread_application.demo-app-id.id
-  value = var.CERTIFICATE
-  end_date_relative = "2400h"
+  value                 = var.CERTIFICATE
+  end_date_relative     = "2400h"
 }
 
 resource "azuread_service_principal" "main-app-sp" {
-  application_id = azuread_application.demo-app-id.application_id
+  application_id               = azuread_application.demo-app-id.application_id
   app_role_assignment_required = false
   tags = [
-    "terraform"]
+  "terraform"]
 }
 
 # Keyvault
 resource "azurerm_key_vault" "main-vault" {
-  name = "${var.environment}-vault"
-  location = azurerm_resource_group.core-resourcegroup.location
-  resource_group_name = azurerm_resource_group.core-resourcegroup.name
+  name                        = "${var.environment}-vault"
+  location                    = azurerm_resource_group.core-resourcegroup.location
+  resource_group_name         = azurerm_resource_group.core-resourcegroup.name
   enabled_for_disk_encryption = false
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days = 7
-  purge_protection_enabled = false
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
 
-  sku_name = "standard"
+  sku_name                  = "standard"
   enable_rbac_authorization = true
 
 }
 
 # Role assignment so that the primary identity may access the vault
 resource "azurerm_role_assignment" "primary-id" {
-  scope = azurerm_key_vault.main-vault.id
+  scope                = azurerm_key_vault.main-vault.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id = azuread_service_principal.main-app-sp.object_id
+  principal_id         = azuread_service_principal.main-app-sp.object_id
 }
 
 # Role assignment that the primary identity may provision/deprovision azure resources
 resource "azurerm_role_assignment" "primary-id-arm" {
-  principal_id = azuread_service_principal.main-app-sp.object_id
-  scope = data.azurerm_subscription.primary.id
+  principal_id         = azuread_service_principal.main-app-sp.object_id
+  scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Contributor"
 }
 
 #Role assignment so that the currently logged in user may access the vault, needed to add secrets
 resource "azurerm_role_assignment" "current-user" {
-  scope = azurerm_key_vault.main-vault.id
+  scope                = azurerm_key_vault.main-vault.id
   role_definition_name = "Key Vault Secrets Officer"
-  principal_id = data.azurerm_client_config.current.object_id
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
 #storage account
 resource "azurerm_storage_account" "main-blobstore" {
-  name = "${replace(var.environment, "-", "")}gpstorage"
-  resource_group_name = azurerm_resource_group.core-resourcegroup.name
-  location = azurerm_resource_group.core-resourcegroup.location
-  account_tier = "Standard"
+  name                     = "${replace(var.environment, "-", "")}gpstorage"
+  resource_group_name      = azurerm_resource_group.core-resourcegroup.name
+  location                 = azurerm_resource_group.core-resourcegroup.location
+  account_tier             = "Standard"
   account_replication_type = "GRS"
-  account_kind = "StorageV2"
+  account_kind             = "StorageV2"
   //allows for blobs, queues, fileshares, etc.
 }
 
 # registration service = ion crawler
 resource "azurerm_container_group" "gx-registration-service" {
-  name = "gaiax-registration-service"
-  location = azurerm_resource_group.core-resourcegroup.location
+  name                = "gaiax-registration-service"
+  location            = azurerm_resource_group.core-resourcegroup.location
   resource_group_name = azurerm_resource_group.core-resourcegroup.name
-  os_type = "Linux"
-  ip_address_type = "public"
-  dns_name_label = "${var.environment}-reg-svc"
+  os_type             = "Linux"
+  ip_address_type     = "public"
+  dns_name_label      = "${var.environment}-reg-svc"
   //  image_registry_credential {
   //    password = var.docker_repo_password
   //    server = var.docker_repo_url
@@ -158,44 +158,44 @@ resource "azurerm_container_group" "gx-registration-service" {
   container {
     cpu = 2
     //    image = "${var.docker_repo_url}/microsoft/edc-connector-demo/edc-demo-consumer:latest"
-    image = "beardyinc/gx-reg-svc:latest"
+    image  = "beardyinc/gx-reg-svc:latest"
     memory = "2"
-    name = "gx-reg-svc"
+    name   = "gx-reg-svc"
 
     ports {
-      port = 8181
+      port     = 8181
       protocol = "TCP"
     }
 
     environment_variables = {
-      CLIENTID = azuread_application.demo-app-id.application_id,
-      TENANTID = data.azurerm_client_config.current.tenant_id,
-      VAULTNAME = azurerm_key_vault.main-vault.name,
+      CLIENTID       = azuread_application.demo-app-id.application_id,
+      TENANTID       = data.azurerm_client_config.current.tenant_id,
+      VAULTNAME      = azurerm_key_vault.main-vault.name,
       CONNECTOR_NAME = "gx-reg-svc"
-      TOPIC_NAME = azurerm_eventgrid_topic.control-topic.name
+      TOPIC_NAME     = azurerm_eventgrid_topic.control-topic.name
       TOPIC_ENDPOINT = azurerm_eventgrid_topic.control-topic.endpoint
-      ION_URL = "http://23.97.144.59:3000/"
+      ION_URL        = "http://23.97.144.59:3000/"
     }
 
     volume {
-      mount_path = "/cert"
-      name = "certificates"
-      share_name = "certificates"
-      storage_account_key = var.backend_account_key
+      mount_path           = "/cert"
+      name                 = "certificates"
+      share_name           = "certificates"
+      storage_account_key  = var.backend_account_key
       storage_account_name = var.backend_account_name
-      read_only = true
+      read_only            = true
     }
   }
 }
 
 # connector that acts as data provider
 resource "azurerm_container_group" "gx-provider" {
-  name = "gaiax-provider"
-  location = azurerm_resource_group.core-resourcegroup.location
+  name                = "gaiax-provider"
+  location            = azurerm_resource_group.core-resourcegroup.location
   resource_group_name = azurerm_resource_group.core-resourcegroup.name
-  os_type = "Linux"
-  ip_address_type = "public"
-  dns_name_label = "${var.environment}-provider"
+  os_type             = "Linux"
+  ip_address_type     = "public"
+  dns_name_label      = "${var.environment}-provider"
   //  image_registry_credential {
   //    password = var.docker_repo_password
   //    server = var.docker_repo_url
@@ -204,68 +204,113 @@ resource "azurerm_container_group" "gx-provider" {
   container {
     cpu = 2
     //    image = "${var.docker_repo_url}/microsoft/edc-connector-demo/edc-demo-consumer:latest"
-    image = "beardyinc/gx-provider:latest"
+    image  = "beardyinc/gx-provider:latest"
     memory = "2"
-    name = "gx-reg-svc"
+    name   = "gx-provider"
 
     ports {
-      port = 8181
+      port     = 8181
       protocol = "TCP"
     }
 
     environment_variables = {
-      CLIENTID = azuread_application.demo-app-id.application_id,
-      TENANTID = data.azurerm_client_config.current.tenant_id,
-      VAULTNAME = azurerm_key_vault.main-vault.name,
+      CLIENTID       = azuread_application.demo-app-id.application_id,
+      TENANTID       = data.azurerm_client_config.current.tenant_id,
+      VAULTNAME      = azurerm_key_vault.main-vault.name,
       CONNECTOR_NAME = "gx-provider"
-      TOPIC_NAME = azurerm_eventgrid_topic.control-topic.name
+      TOPIC_NAME     = azurerm_eventgrid_topic.control-topic.name
       TOPIC_ENDPOINT = azurerm_eventgrid_topic.control-topic.endpoint
     }
 
     volume {
-      mount_path = "/cert"
-      name = "certificates"
-      share_name = "certificates"
-      storage_account_key = var.backend_account_key
+      mount_path           = "/cert"
+      name                 = "certificates"
+      share_name           = "certificates"
+      storage_account_key  = var.backend_account_key
       storage_account_name = var.backend_account_name
-      read_only = true
+      read_only            = true
+    }
+  }
+}
+
+# connector that acts as data consumer
+resource "azurerm_container_group" "gx-consumer" {
+  name                = "gaiax-consumer"
+  location            = azurerm_resource_group.core-resourcegroup.location
+  resource_group_name = azurerm_resource_group.core-resourcegroup.name
+  os_type             = "Linux"
+  ip_address_type     = "public"
+  dns_name_label      = "${var.environment}-consumer"
+  //  image_registry_credential {
+  //    password = var.docker_repo_password
+  //    server = var.docker_repo_url
+  //    username = var.docker_repo_username
+  //  }
+  container {
+    cpu = 2
+    //    image = "${var.docker_repo_url}/microsoft/edc-connector-demo/edc-demo-consumer:latest"
+    image  = "beardyinc/gx-consumer:latest"
+    memory = "2"
+    name   = "gx-consumer"
+
+    ports {
+      port     = 8181
+      protocol = "TCP"
+    }
+
+    environment_variables = {
+      CLIENTID       = azuread_application.demo-app-id.application_id,
+      TENANTID       = data.azurerm_client_config.current.tenant_id,
+      VAULTNAME      = azurerm_key_vault.main-vault.name,
+      CONNECTOR_NAME = "gx-consumer"
+      TOPIC_NAME     = azurerm_eventgrid_topic.control-topic.name
+      TOPIC_ENDPOINT = azurerm_eventgrid_topic.control-topic.endpoint
+    }
+
+    volume {
+      mount_path           = "/cert"
+      name                 = "certificates"
+      share_name           = "certificates"
+      storage_account_key  = var.backend_account_key
+      storage_account_name = var.backend_account_name
+      read_only            = true
     }
   }
 }
 
 resource "azurerm_key_vault_secret" "blobstorekey" {
-  name = "${azurerm_storage_account.main-blobstore.name}-key1"
-  value = azurerm_storage_account.main-blobstore.primary_access_key
+  name         = "${azurerm_storage_account.main-blobstore.name}-key1"
+  value        = azurerm_storage_account.main-blobstore.primary_access_key
   key_vault_id = azurerm_key_vault.main-vault.id
   depends_on = [
-    azurerm_role_assignment.current-user]
+  azurerm_role_assignment.current-user]
 }
 
 resource "azurerm_key_vault_secret" "aws-keyid" {
-  name = "dataspaceconnector-aws-access-key"
-  value = aws_iam_access_key.gx_access_key.id
+  name         = "dataspaceconnector-aws-access-key"
+  value        = aws_iam_access_key.gx_access_key.id
   key_vault_id = azurerm_key_vault.main-vault.id
   depends_on = [
-    azurerm_role_assignment.current-user]
+  azurerm_role_assignment.current-user]
 }
 
 resource "azurerm_key_vault_secret" "aws-secret" {
-  name = "dataspaceconnector-aws-secret-access-key"
-  value = aws_iam_access_key.gx_access_key.secret
+  name         = "dataspaceconnector-aws-secret-access-key"
+  value        = aws_iam_access_key.gx_access_key.secret
   key_vault_id = azurerm_key_vault.main-vault.id
   depends_on = [
-    azurerm_role_assignment.current-user]
+  azurerm_role_assignment.current-user]
 }
 
 resource "azurerm_key_vault_secret" "aws-credentials" {
   key_vault_id = azurerm_key_vault.main-vault.id
-  name = "aws-credentials"
+  name         = "aws-credentials"
   value = jsonencode({
-    "accessKeyId" = aws_iam_access_key.gx_access_key.id,
+    "accessKeyId"     = aws_iam_access_key.gx_access_key.id,
     "secretAccessKey" = aws_iam_access_key.gx_access_key.secret
   })
   depends_on = [
-    azurerm_role_assignment.current-user]
+  azurerm_role_assignment.current-user]
 }
 
 ## CLUSTER DEPLOYMENTS
