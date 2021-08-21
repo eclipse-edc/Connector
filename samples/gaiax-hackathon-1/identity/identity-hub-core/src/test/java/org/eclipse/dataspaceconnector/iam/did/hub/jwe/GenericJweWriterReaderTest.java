ae44dbfa299a53313b5c61c7477da197269b0e29
Jim Marino <jim.marino@gmail.com>
2021-08-21 22:50:17 +0200

    Work on Identity Hub implementation

---
+++ samples/gaiax-hackathon-1/identity/identity-hub-core/src/test/java/org/eclipse/dataspaceconnector/iam/did/hub/jwe/GenericJweWriterReaderTest.java
@@ -0,0 +1,34 @@
+package org.eclipse.dataspaceconnector.iam.did.hub.jwe;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.eclipse.dataspaceconnector.iam.did.hub.jwe.JweTestFunctions.loadAndGetKey;
+
+/**
+ *
+ */
+class GenericJweWriterReaderTest {
+
+    @Test
+    void verifyWriteRead() throws Exception {
+        var key = loadAndGetKey();
+        var privateKey = key.toRSAPrivateKey();
+        var publicKey = key.toRSAPublicKey();
+
+        var objectMapper = new ObjectMapper();
+
+        var jwe = new GenericJweWriter()
+                .privateKey(privateKey)
+                .publicKey(publicKey)
+                .objectMapper(objectMapper)
+                .payload(Map.of("foo", "bar"))
+                .buildJwe();
+
+        var deserialized = new GenericJweReader().privateKey(privateKey).mapper(objectMapper).jwe(jwe).readType(Map.class);
+        Assertions.assertEquals("bar", deserialized.get("foo"));
+    }
+}
---
+++ samples/gaiax-hackathon-1/identity/identity-hub-spi/src/test/java/org/eclipse/dataspaceconnector/iam/did/hub/spi/message/HubObjectTest.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+package org.eclipse.dataspaceconnector.iam.did.hub.spi.message;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.eclipse.dataspaceconnector.iam.did.hub.spi.message.InterfaceType.Actions;
+
+/**
+ *
+ */
+class HubObjectTest {
+
+    @Test
+    void verifySerializeDeserialize() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        var serialized = mapper.writeValueAsString(HubObject.Builder.newInstance().interfaze(Actions).type("foo").id("123").createdBy("ion:did:foo").sub("sub").build());
+        var deserialized = mapper.readValue(serialized, HubObject.class);
+        Assertions.assertNotNull(deserialized);
+        Assertions.assertEquals(Actions, deserialized.getInterface());
+    }
+
+}
