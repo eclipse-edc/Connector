c0033edee51109978a4cdfaf142c25011cef5165
Jim Marino <jim.marino@gmail.com>
2021-08-23 15:01:48 +0200

    Fix version clash; add DID extensions to runtime

--- samples/gaiax-hackathon-1/identity/identity-hub-core/build.gradle.kts
+++ samples/gaiax-hackathon-1/identity/identity-hub-core/build.gradle.kts
@@ -7,7 +7,8 @@ val rsApi: String by project
 dependencies {
     api(project(":samples:gaiax-hackathon-1:identity:identity-hub-spi"))
 
-    implementation("com.nimbusds:nimbus-jose-jwt:9.12.1")
+    // newer Nimbus versions create a version conflict with the MSAL library which uses this version as a transitive dependency
+    implementation("com.nimbusds:nimbus-jose-jwt:8.20.1")
 
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
 }
