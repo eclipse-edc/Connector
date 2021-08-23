a9dbf4f11bff3ca5babaed92f32351f351892d7b
Jim Marino <jim.marino@gmail.com>
2021-08-23 22:19:10 +0200

    Work on identity hub

--- samples/gaiax-hackathon-1/identity/identity-hub-core/src/main/java/org/eclipse/dataspaceconnector/iam/did/hub/CoreIdentityHubExtension.java
+++ samples/gaiax-hackathon-1/identity/identity-hub-core/src/main/java/org/eclipse/dataspaceconnector/iam/did/hub/CoreIdentityHubExtension.java
@@ -13,16 +13,29 @@
  */
 package org.eclipse.dataspaceconnector.iam.did.hub;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.RSAKey;
 import org.eclipse.dataspaceconnector.iam.did.hub.spi.DidPublicKeyResolver;
 import org.eclipse.dataspaceconnector.iam.did.hub.spi.IdentityHub;
 import org.eclipse.dataspaceconnector.iam.did.hub.spi.IdentityHubStore;
+import org.eclipse.dataspaceconnector.spi.EdcException;
 import org.eclipse.dataspaceconnector.spi.EdcSetting;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.protocol.web.WebService;
 import org.eclipse.dataspaceconnector.spi.security.PrivateKeyResolver;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtension;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.interfaces.RSAPrivateKey;
 import java.util.Set;
+import java.util.function.Supplier;
+
+import static java.lang.String.format;
 
 /**
  *
@@ -38,17 +51,14 @@ public class CoreIdentityHubExtension implements ServiceExtension {
 
     @Override
     public void initialize(ServiceExtensionContext context) {
-        var privateKeyAlias = context.getSetting(PRIVATE_KEY_ALIAS, "privateKeyAlias");
-        var privateKeyResolver = context.getService(PrivateKeyResolver.class);
-
         var hubStore = context.getService(IdentityHubStore.class);
 
         var objectMapper = context.getTypeManager().getMapper();
 
-        // TODO: implement a resolver
-        DidPublicKeyResolver publicKeyResolver = (did) -> null;
+        // TODO: implement key resolvers
+        var resolverPair = temporaryLoadResolvers(context);
 
-        var hub = new IdentityHubImpl(hubStore, () -> privateKeyResolver.resolvePrivateKey(privateKeyAlias), publicKeyResolver, objectMapper);
+        var hub = new IdentityHubImpl(hubStore, resolverPair.privateKeyResolver, resolverPair.publicKeyResolver, objectMapper);
         context.registerService(IdentityHub.class, hub);
 
         var controller = new IdentityHubController(hub);
@@ -58,4 +68,59 @@ public class CoreIdentityHubExtension implements ServiceExtension {
         context.getMonitor().info("Initialized Core Identity Hub extension");
     }
 
+    // TODO: TEMPORARY local key management to be replaced by vault storage
+    private static final String TEST_KEYSTORE = "edc-test-keystore.jks";
+    private static final String PASSWORD = "test123";
+
+    private ResolverPair temporaryLoadResolvers(ServiceExtensionContext context) {
+        var keys = loadKeys(context.getMonitor());
+        if (keys == null) {
+            return new ResolverPair(() -> null, (dis) -> null);
+        }
+        var privateKeyAlias = context.getSetting(PRIVATE_KEY_ALIAS, "privateKeyAlias");
+        // var privateKeyResolver = context.getService(PrivateKeyResolver.class);
+        PrivateKeyResolver delegateResolver = id -> {
+            try {
+                return keys.toRSAPrivateKey();
+            } catch (JOSEException e) {
+                throw new EdcException(e);
+            }
+        };
+
+        Supplier<RSAPrivateKey> privateKeyResolver = () -> delegateResolver.resolvePrivateKey(privateKeyAlias);
+
+        DidPublicKeyResolver publicKeyResolver = (did) -> {
+            try {
+                return keys.toPublicKey();
+            } catch (JOSEException e) {
+                throw new EdcException(e);
+            }
+        };
+        return new ResolverPair(privateKeyResolver, publicKeyResolver);
+    }
+
+    @Nullable
+    private RSAKey loadKeys(Monitor monitor) {
+        try {
+            var url = Paths.get("secrets" + File.separator + TEST_KEYSTORE).toUri().toURL();
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            try (InputStream stream = url.openStream()) {
+                keyStore.load(stream, PASSWORD.toCharArray());
+            }
+            return RSAKey.load(keyStore, "testkey", PASSWORD.toCharArray());
+        } catch (Exception e) {
+            monitor.info(format("Cannot load test keys - the keystore %s should be placed in the secrets directory", TEST_KEYSTORE));
+            return null;
+        }
+    }
+
+    private static class ResolverPair {
+        Supplier<RSAPrivateKey> privateKeyResolver;
+        DidPublicKeyResolver publicKeyResolver;
+
+        public ResolverPair(Supplier<RSAPrivateKey> privateKeyResolver, DidPublicKeyResolver publicKeyResolver) {
+            this.privateKeyResolver = privateKeyResolver;
+            this.publicKeyResolver = publicKeyResolver;
+        }
+    }
 }
---
+++ samples/gaiax-hackathon-1/identity/identity-hub-core/src/test/java/org/eclipse/dataspaceconnector/iam/did/hub/gaiax/ClientQueryTool.java
@@ -0,0 +1,110 @@
+/*
+ *  Copyright (c) 2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+package org.eclipse.dataspaceconnector.iam.did.hub.gaiax;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.RSAKey;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import org.eclipse.dataspaceconnector.iam.did.hub.jwe.GenericJweReader;
+import org.eclipse.dataspaceconnector.iam.did.hub.jwe.GenericJweWriter;
+import org.eclipse.dataspaceconnector.iam.did.hub.spi.message.CommitQuery;
+import org.eclipse.dataspaceconnector.iam.did.hub.spi.message.CommitQueryRequest;
+import org.eclipse.dataspaceconnector.iam.did.hub.spi.message.CommitQueryResponse;
+import org.eclipse.dataspaceconnector.iam.did.hub.spi.message.HubMessage;
+import org.eclipse.dataspaceconnector.iam.did.hub.spi.message.ObjectQuery;
+import org.eclipse.dataspaceconnector.iam.did.hub.spi.message.ObjectQueryRequest;
+import org.eclipse.dataspaceconnector.iam.did.hub.spi.message.ObjectQueryResponse;
+import org.eclipse.dataspaceconnector.spi.EdcException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.eclipse.dataspaceconnector.iam.did.hub.gaiax.GaiaXAssumptions.assumptions;
+import static org.eclipse.dataspaceconnector.iam.did.hub.gaiax.GaiaxConstants.CONSUMER_COMMIT_QUERY_URL;
+import static org.eclipse.dataspaceconnector.iam.did.hub.gaiax.GaiaxConstants.CONSUMER_OBJECT_QUERY_URL;
+import static org.eclipse.dataspaceconnector.iam.did.hub.test.JweTestFunctions.loadAndGetKey;
+
+/**
+ *
+ */
+public class ClientQueryTool {
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private RSAKey keys;
+
+    public ClientQueryTool() throws Exception {
+        keys = loadAndGetKey();
+    }
+
+    @Test
+    public void queryConsumerCredentials() throws Exception {
+        assumptions();
+
+        var privateKey = keys.toRSAPrivateKey();
+        var publicKey = keys.toRSAPublicKey();
+
+        var query = ObjectQuery.Builder.newInstance().context("GAIA-X").type("RegistrationCredentials").build();
+        var queryRequest = ObjectQueryRequest.Builder.newInstance().query(query).iss("123").aud("aud").sub("sub").build();
+
+        var objectRequestJwe = new GenericJweWriter()
+                .privateKey(privateKey)
+                .publicKey(publicKey)
+                .objectMapper(objectMapper)
+                .payload(queryRequest)
+                .buildJwe();
+
+        var ObjectQueryResponse = executeQuery(ObjectQueryResponse.class, objectRequestJwe, CONSUMER_OBJECT_QUERY_URL);
+
+        ObjectQueryResponse.getObjects().forEach(hubObject -> {
+            var commitQuery = CommitQuery.Builder.newInstance().objectId(hubObject.getId()).build();
+            var commitQueryRequest = CommitQueryRequest.Builder.newInstance().query(commitQuery).iss("123").aud("aud").sub("sub").build();
+
+            var commitRequestJwe = new GenericJweWriter()
+                    .privateKey(privateKey)
+                    .publicKey(publicKey)
+                    .objectMapper(objectMapper)
+                    .payload(commitQueryRequest)
+                    .buildJwe();
+
+            var commitQueryResponse = executeQuery(CommitQueryResponse.class, commitRequestJwe, CONSUMER_COMMIT_QUERY_URL);
+            commitQueryResponse.getCommits().forEach(commit -> {
+                try {
+                    System.out.println("Commit:" + objectMapper.writeValueAsString(commit.getPayload()));
+                } catch (JsonProcessingException e) {
+                    throw new EdcException(e);
+                }
+            });
+
+        });
+    }
+
+
+    private <M extends HubMessage> M executeQuery(Class<M> type, String jwe, String url) {
+        var httpClient = new OkHttpClient.Builder().build();
+        var requestBody = RequestBody.create(jwe, MediaType.get("application/json"));
+        var request = new Request.Builder().url(url).post(requestBody).build();
+
+        try (var response = httpClient.newCall(request).execute()) {
+            var body = response.body();
+            assert body != null;
+            return new GenericJweReader().mapper(objectMapper).jwe(body.string()).privateKey(keys.toRSAPrivateKey()).readType(type);
+        } catch (IOException | JOSEException e) {
+            throw new EdcException(e);
+        }
+    }
+}
