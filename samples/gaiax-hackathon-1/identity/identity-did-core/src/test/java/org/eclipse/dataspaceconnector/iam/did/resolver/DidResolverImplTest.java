716b363c3d8729c541527dd821ef6f06e9df868f
Jim Marino <jim.marino@gmail.com>
2021-08-24 11:14:05 +0200

    Add assertion

--- samples/gaiax-hackathon-1/identity/identity-did-core/src/test/java/org/eclipse/dataspaceconnector/iam/did/resolver/DidResolverImplTest.java
+++ samples/gaiax-hackathon-1/identity/identity-did-core/src/test/java/org/eclipse/dataspaceconnector/iam/did/resolver/DidResolverImplTest.java
@@ -2,6 +2,7 @@ package org.eclipse.dataspaceconnector.iam.did.resolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.eclipse.dataspaceconnector.iam.did.util.GaiaXAssumptions.assumptions;
@@ -17,6 +18,6 @@ class DidResolverImplTest {
         assumptions();
         var resolver = new DidResolverImpl(RESOLVER_URL, new OkHttpClient(), new ObjectMapper());
         var did = resolver.resolveDid("did:ion:EiDfkaPHt8Yojnh15O7egrj5pA9tTefh_SYtbhF1-XyAeA");
-        System.out.println();
+        Assertions.assertNotNull(did);
     }
 }
