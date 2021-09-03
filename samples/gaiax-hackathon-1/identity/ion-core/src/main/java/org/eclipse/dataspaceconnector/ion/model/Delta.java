c99de9e1e7f871eb070cad5ac25ce4667b83d341
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-03 13:32:33 +0200

    moved/renamed model classes

---
+++ samples/gaiax-hackathon-1/identity/ion-core/src/main/java/org/eclipse/dataspaceconnector/ion/model/Delta.java
@@ -0,0 +1,26 @@
+package org.eclipse.dataspaceconnector.ion.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.util.List;
+import java.util.Map;
+
+@JsonPropertyOrder(alphabetic = true)
+public class Delta {
+    private final String updateCommitment;
+    private final List<Map<String, Object>> patches;
+
+    public Delta(@JsonProperty("updateCommitment") String updateCommitment, @JsonProperty("patches") List<Map<String, Object>> patches) {
+        this.updateCommitment = updateCommitment;
+        this.patches = patches;
+    }
+
+    public String getUpdateCommitment() {
+        return updateCommitment;
+    }
+
+    public List<Map<String, Object>> getPatches() {
+        return patches;
+    }
+}
---
+++ samples/gaiax-hackathon-1/identity/ion-core/src/test/java/org/eclipse/dataspaceconnector/ion/util/JsonCanonicalizerTest.java
@@ -0,0 +1,47 @@
+package org.eclipse.dataspaceconnector.ion.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JsonCanonicalizerTest {
+
+    @Test
+    void canonicalizeAsBytes() {
+        var map = Map.of("key1", "value1",
+                "key2", 3.14159,
+                "key3", Arrays.asList("one", "two", "three"),
+                "key4", "",
+                "key5", Collections.emptyList());
+
+        var canon = JsonCanonicalizer.canonicalizeAsBytes(map);
+        var json = new String(canon);
+
+        assertThat(json).contains("key1");
+        assertThat(json).doesNotContain("key4");
+        assertThat(json).doesNotContain("key5");
+    }
+
+    @Test
+    void verifyCanonicalForm() {
+        var map1 = Map.of("key1", "value1",
+                "key2", 3.14159,
+                "key3", Arrays.asList("one", "two", "three"),
+                "key4", "",
+                "key5", Collections.emptyList());
+
+        // the same as map1, but in a different sequence
+        var map2 = Map.of("key1", "value1",
+                "key4", "",
+                "key3", Arrays.asList("one", "two", "three"),
+                "key2", 3.14159,
+                "key5", Collections.emptyList());
+
+
+        assertThat(JsonCanonicalizer.canonicalizeAsBytes(map1)).isEqualTo(JsonCanonicalizer.canonicalizeAsBytes(map2));
+    }
+}
