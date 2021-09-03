91bc86b56e2073b034f44e285b7a29d61e45c0d4
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-03 13:32:33 +0200

    moved/renamed model classes

---
+++ samples/gaiax-hackathon-1/identity/ion-core/src/main/java/org/eclipse/dataspaceconnector/ion/crypto/KeyPairFactory.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2020, 2020-2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+package org.eclipse.dataspaceconnector.ion.crypto;
+
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWK;
+import org.eclipse.dataspaceconnector.ion.IonException;
+import org.eclipse.dataspaceconnector.ion.model.JwkKeyPair;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.ECGenParameterSpec;
+
+public class KeyPairFactory {
+
+    public static JwkKeyPair generateKeyPair() {
+        try {
+
+            ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
+            KeyPairGenerator g = KeyPairGenerator.getInstance("EC");
+            g.initialize(ecSpec, new SecureRandom());
+            KeyPair keyPair = g.generateKeyPair();
+
+
+            JWK jwk = new ECKey.Builder(Curve.SECP256K1, (ECPublicKey) keyPair.getPublic())
+                    .privateKey((ECPrivateKey) keyPair.getPrivate())
+                    .build();
+
+
+            return JwkKeyPair.from(jwk);
+        } catch (Exception e) {
+            throw new IonException(e);
+        }
+    }
+
+}
---
+++ samples/gaiax-hackathon-1/identity/ion-core/src/main/java/org/eclipse/dataspaceconnector/ion/model/DidOperationType.java
@@ -0,0 +1,22 @@
+/*
+ *  Copyright (c) 2020, 2020-2021 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+package org.eclipse.dataspaceconnector.ion.model;
+
+public enum DidOperationType {
+    CREATE,
+    UPDATE,
+    RECOVER,
+    DEACTIVATE
+}
