331faec6c7b33968961e2f11fa8f32649c224376
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-06 08:09:46 +0200

    moved identity-service stuff to IAM and extensions packages respectively

--- samples/gaiax-hackathon-1/identity/identity-gaiax-verifier/build.gradle.kts
+++ samples/gaiax-hackathon-1/identity/identity-gaiax-verifier/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     api(project(":spi"))
 
-    api(project(":samples:gaiax-hackathon-1:identity:identity-did-spi"))
+    api(project(":extensions:iam:distributed-identity:identity-did-spi"))
 
     testImplementation(testFixtures(project(":samples:gaiax-hackathon-1:identity:identity-common-test")))
 
