91399ede3f99afde3a3eee11f9144859d47ebf98
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-06 12:22:22 +0200

    moved azure/s3 implementation to samples

--- samples/gaiax-hackathon-1/identity/catalog-service/src/main/java/org/eclipse/dataspaceconnector/metadata/catalog/CatalogService.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.dataspaceconnector.metadata.catalog;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
-public interface CatalogService {
-    CompletableFuture<List<String>> listArtifacts(String connectorName, String connectorAddress);
-}
