1e1055a435d1d22ceb59bd2b7415372790f170d1
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-13 15:20:29 +0200

    straightened out terraform scripts

--- deployment/ion-demo/aws.tf
+++ deployment/ion-demo/aws.tf
@@ -4,30 +4,30 @@ provider "aws" {
 }
 
 
-resource "aws_iam_user" "hackathon-user" {
-  name          = "hackathon-user-paul"
+resource "aws_iam_user" "aws-user" {
+  name          = var.aws_user
   path          = "/"
   force_destroy = true
 }
 
 resource "aws_iam_access_key" "gx_access_key" {
-  user = aws_iam_user.hackathon-user.name
+  user = aws_iam_user.aws-user.name
 
 }
 
 resource "aws_iam_user_policy_attachment" "gx-s3fullaccess" {
-  user       = aws_iam_user.hackathon-user.name
+  user       = aws_iam_user.aws-user.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_user_policy_attachment" "gx-iamfullaccess" {
-  user       = aws_iam_user.hackathon-user.name
+  user       = aws_iam_user.aws-user.name
   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
 }
 
 
 resource "aws_s3_bucket" "src-bucket" {
-  bucket = "hackathon-src-bucket"
+  bucket = "ion-demo-src-bucket"
 }
 
 output "new_user" {
