abf2d6f92f6a445c3c404d871051244dce6a8657
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-16 18:20:29 +0200

    added MIT license header

--- data-protocols/ion/ion-core/src/main/java/org/eclipse/dataspaceconnector/ion/util/Base16.java
+++ data-protocols/ion/ion-core/src/main/java/org/eclipse/dataspaceconnector/ion/util/Base16.java
@@ -1,7 +1,29 @@
 package org.eclipse.dataspaceconnector.ion.util;
 
 /**
- * This is a 1:1 copy of the Base16 implementation found on Github:
+ * MIT License
+ * <p>
+ * Copyright (c) 2015 Ian Preston
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * <p>
+ * Copied from:
  * https://github.com/multiformats/java-multibase/blob/master/src/main/java/io/ipfs/multibase/Base16.java
  * <p>
  * Date copied: Sept 16, 2021
--- data-protocols/ion/ion-core/src/main/java/org/eclipse/dataspaceconnector/ion/util/Multihash.java
+++ data-protocols/ion/ion-core/src/main/java/org/eclipse/dataspaceconnector/ion/util/Multihash.java
@@ -12,7 +12,30 @@ import java.util.Map;
 
 
 /**
- * This is a 1:1 copy of the Java Multihash implementation from Github:
+ * MIT License
+ * <p>
+ * Copyright (c) 2015 Ian Preston
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * <p>
+ * <p>
+ * Code copied from:
  * https://github.com/multiformats/java-multihash/blob/master/src/main/java/io/ipfs/multihash/Multihash.java
  * <p>
  * <p>
