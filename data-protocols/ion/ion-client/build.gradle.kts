50320d97b0a77f262b8f7ad951fb645cf749a43a
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-28 19:18:18 +0200

    further decoupled some of the modules

--- data-protocols/ion/ion-client/build.gradle.kts
+++ data-protocols/ion/ion-client/build.gradle.kts
@@ -8,8 +8,10 @@ val nimbusVersion: String by project
 
 dependencies {
     api(project(":data-protocols:ion:ion-core"))
-    implementation(project(":extensions:iam:distributed-identity:identity-did-core"))
+
     implementation("org.bouncycastle:bcprov-jdk15on:1.69") // for argon2id
+
+    testImplementation(project(":extensions:iam:distributed-identity:identity-did-core")) // for the KeyPairFactory
 }
 
 publishing {
