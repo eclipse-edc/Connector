43b9ed91cc84646fd0d78a9da14f30f776d6acc9
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-28 19:18:18 +0200

    further decoupled some of the modules

--- data-protocols/ion/ion-client/src/main/java/org/eclipse/dataspaceconnector/ion/IonClientExtension.java
+++ data-protocols/ion/ion-client/src/main/java/org/eclipse/dataspaceconnector/ion/IonClientExtension.java
@@ -1,6 +1,8 @@
 package org.eclipse.dataspaceconnector.ion;
 
+import org.eclipse.dataspaceconnector.iam.did.spi.resolution.DidPublicKeyResolver;
 import org.eclipse.dataspaceconnector.iam.did.spi.resolution.DidResolver;
+import org.eclipse.dataspaceconnector.ion.crypto.IonDidPublicKeyResolver;
 import org.eclipse.dataspaceconnector.ion.spi.IonClient;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtension;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
@@ -14,7 +16,7 @@ public class IonClientExtension implements ServiceExtension {
 
     @Override
     public Set<String> provides() {
-        return Set.of(IonClient.FEATURE, DidResolver.FEATURE);
+        return Set.of(IonClient.FEATURE, DidResolver.FEATURE, DidPublicKeyResolver.FEATURE);
     }
 
     @Override
@@ -24,6 +26,9 @@ public class IonClientExtension implements ServiceExtension {
         var client = new DefaultIonClient(ionEndpoint, context.getTypeManager().getMapper());
         context.registerService(IonClient.class, client);
         context.registerService(DidResolver.class, client);
+
+        //registering ION Public Key Resolver
+        context.registerService(DidPublicKeyResolver.class, new IonDidPublicKeyResolver(client));
         context.getMonitor().info("Initialized IonClientExtension");
     }
 
