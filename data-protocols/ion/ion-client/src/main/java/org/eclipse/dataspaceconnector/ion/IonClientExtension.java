b9b040039eff264cf39897f961b07841def1d7d1
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-09-30 09:31:59 +0200

    some refactoring, moving around classes

--- data-protocols/ion/ion-client/src/main/java/org/eclipse/dataspaceconnector/ion/IonClientExtension.java
+++ data-protocols/ion/ion-client/src/main/java/org/eclipse/dataspaceconnector/ion/IonClientExtension.java
@@ -1,8 +1,6 @@
 package org.eclipse.dataspaceconnector.ion;
 
-import org.eclipse.dataspaceconnector.iam.did.spi.resolution.DidPublicKeyResolver;
 import org.eclipse.dataspaceconnector.iam.did.spi.resolution.DidResolver;
-import org.eclipse.dataspaceconnector.ion.crypto.IonDidPublicKeyResolver;
 import org.eclipse.dataspaceconnector.ion.spi.IonClient;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtension;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
@@ -16,7 +14,7 @@ public class IonClientExtension implements ServiceExtension {
 
     @Override
     public Set<String> provides() {
-        return Set.of(IonClient.FEATURE, DidResolver.FEATURE, DidPublicKeyResolver.FEATURE);
+        return Set.of(IonClient.FEATURE, DidResolver.FEATURE);
     }
 
     @Override
@@ -27,8 +25,7 @@ public class IonClientExtension implements ServiceExtension {
         context.registerService(IonClient.class, client);
         context.registerService(DidResolver.class, client);
 
-        //registering ION Public Key Resolver
-        context.registerService(DidPublicKeyResolver.class, new IonDidPublicKeyResolver(client));
+
         context.getMonitor().info("Initialized IonClientExtension");
     }
 
