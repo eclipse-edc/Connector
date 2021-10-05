3d5c5af7177c0c6e616f0205cf5cf7a35de71fc8
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-10-05 13:18:12 +0200

    added tests

--- extensions/catalog/federated-catalog-cache/src/main/java/org/eclipse/dataspaceconnector/crawler/Crawler.java
+++ extensions/catalog/federated-catalog-cache/src/main/java/org/eclipse/dataspaceconnector/crawler/Crawler.java
@@ -3,10 +3,8 @@ package org.eclipse.dataspaceconnector.crawler;
 import net.jodah.failsafe.RetryPolicy;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 
-import java.time.temporal.ChronoUnit;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.util.Queue;
 
 import static java.lang.String.format;
 import static net.jodah.failsafe.Failsafe.with;
@@ -15,10 +13,10 @@ public class Crawler implements Runnable {
 
     private final List<CrawlerProtocolAdapter> adapters;
     private final Monitor monitor;
-    private final BlockingQueue<UpdateResponse> queue;
+    private final Queue<UpdateResponse> queue;
     private final RetryPolicy<Object> retryPolicy;
 
-    public Crawler(List<CrawlerProtocolAdapter> adapters, Monitor monitor, BlockingQueue<UpdateResponse> queue, RetryPolicy<Object> retryPolicy) {
+    public Crawler(List<CrawlerProtocolAdapter> adapters, Monitor monitor, Queue<UpdateResponse> queue, RetryPolicy<Object> retryPolicy) {
         this.adapters = adapters;
         this.monitor = monitor;
         this.queue = queue;
@@ -45,13 +43,9 @@ public class Crawler implements Runnable {
 
     private void handleResponse(UpdateResponse updateResponse) {
         monitor.info(format("update-response received: %s", updateResponse.toString()));
-        try {
-            var offered = with(retryPolicy).get(() -> queue.offer(updateResponse, 500, TimeUnit.of(ChronoUnit.MILLIS)));
-            if (!offered) {
-                monitor.severe("Inserting update-response into queue failed due to timeout!");
-            }
-        } catch (Exception e) { //an InterruptedException may still be thrown
-            monitor.severe("Inserting update-response into queue failed!", e);
+        var offered = with(retryPolicy).get(() -> queue.offer(updateResponse));
+        if (!offered) {
+            monitor.severe("Inserting update-response into queue failed due to timeout!");
         }
     }
 }
--- extensions/catalog/federated-catalog-cache/src/test/java/org/eclipse/dataspaceconnector/crawler/CrawlerTest.java
+++ extensions/catalog/federated-catalog-cache/src/test/java/org/eclipse/dataspaceconnector/crawler/CrawlerTest.java
@@ -1,6 +1,7 @@
 package org.eclipse.dataspaceconnector.crawler;
 
 import net.jodah.failsafe.RetryPolicy;
+import org.eclipse.dataspaceconnector.spi.EdcException;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.easymock.EasyMock.*;
 
 class CrawlerTest {
+    public static final int QUEUE_CAPACITY = 3;
     private Crawler crawler;
     private CrawlerProtocolAdapter protocolAdapterMock;
     private ArrayBlockingQueue<UpdateResponse> queue;
@@ -24,7 +26,7 @@ class CrawlerTest {
     @BeforeEach
     void setUp() {
         protocolAdapterMock = strictMock(CrawlerProtocolAdapter.class);
-        queue = new ArrayBlockingQueue<>(3);
+        queue = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
         adapters = new ArrayList<>();
         adapters.add(protocolAdapterMock);
         monitorMock = niceMock(Monitor.class);
@@ -44,6 +46,25 @@ class CrawlerTest {
     }
 
     @Test
+    @DisplayName("Should not insert into queue when the request fails")
+    void shouldNotInsertInQueue_whenRequestFails() {
+
+
+        expect(protocolAdapterMock.sendRequest(isA(UpdateRequest.class))).andReturn(CompletableFuture.failedFuture(new EdcException("")));
+        replay(protocolAdapterMock);
+
+        monitorMock.severe(anyString(), isA(EdcException.class));
+        expectLastCall().once();
+        replay(monitorMock);
+
+        crawler.run();
+
+        assertThat(queue).isEmpty();
+        verify(protocolAdapterMock);
+        verify(monitorMock);
+    }
+
+    @Test
     @DisplayName("Should insert only those items into queue that have succeeded")
     void shouldInsertInQueue_onlySuccessfulRequests() {
 
@@ -55,11 +76,37 @@ class CrawlerTest {
         expect(secondAdapter.sendRequest(isA(UpdateRequest.class))).andReturn(CompletableFuture.failedFuture(new RuntimeException()));
         replay(secondAdapter);
 
+        monitorMock.severe(anyString(), isA(RuntimeException.class));
+        expectLastCall().once();
+        replay(monitorMock);
+
         crawler.run();
 
         assertThat(queue).hasSize(1);
         verify(protocolAdapterMock);
         verify(secondAdapter);
+        verify(monitorMock);
+    }
+
+    @Test
+    @DisplayName("Should not insert when Queue is at capacity")
+    void shouldLogError_whenQueueFull() {
+        queue.add(new UpdateResponse());
+        queue.add(new UpdateResponse());
+        queue.add(new UpdateResponse()); //queue is full now
+
+        expect(protocolAdapterMock.sendRequest(isA(UpdateRequest.class))).andReturn(CompletableFuture.supplyAsync(UpdateResponse::new));
+        replay(protocolAdapterMock);
+
+        monitorMock.severe(anyString());
+        expectLastCall().once();
+        replay(monitorMock);
+
+        crawler.run();
+
+        assertThat(queue).hasSize(3);
+        verify(protocolAdapterMock);
+        verify(monitorMock);
     }
 
     private RetryPolicy<Object> createRetryPolicy() {
