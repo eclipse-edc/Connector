a9a72458083e97669f582d676e6765eefa9bf39f
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-10-18 17:08:14 +0200

    checkstyle

--- extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/QueryAdapterRegistry.java
+++ extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/QueryAdapterRegistry.java
@@ -23,16 +23,16 @@ public interface QueryAdapterRegistry {
     void register(QueryAdapter adapter);
 
     /**
-     * Attempts to execute a query by forwarding it to all suitable {@link QueryAdapter} implementations.
-     *
-     * @param query The {@link CacheQuery}
-     * @return a {@link QueryResponse} with {@link QueryResponse#getStatus()} is equal to
+     * Attempts to execute a query by forwarding it to all suitable {@link QueryAdapter}s. It returns a query response with a status equal to
      * {@link QueryResponse.Status#ACCEPTED} if there was at least one adapter that could accept the query. If no suitable adapter was found, the status will be
-     * {@link QueryResponse.Status#NO_ADAPTER_FOUND}.  The actual result of the query, which could be mixed as some adapters might succeed, others might fail, can be
-     * obtained from {@link QueryResponse#getAssets()} and {@link QueryResponse#getErrors()}. The earlier returns an aggregated stream of {@link org.eclipse.dataspaceconnector.spi.types.domain.asset.Asset}, the latter
-     * contains a list of errors.
+     * {@link QueryResponse.Status#NO_ADAPTER_FOUND}. The actual result of the query, which could be mixed as some adapters might succeed, others might fail, can be
+     * obtained from {@link QueryResponse#getAssets()} and {@link QueryResponse#getErrors()}. The earlier list returns an aggregated stream of
+     * {@link org.eclipse.dataspaceconnector.spi.types.domain.asset.Asset}, the latter contains a list of errors.
      * <p>
      * For example, when 1 of 5 adapters that receive the query times out, there will be results from 4, errors from 1.
+     *
+     * @param query The {@link CacheQuery}
+     * @return a {@link QueryResponse} with {@link QueryResponse#getStatus()} that contains the result (Assets) and potentially errors.
      */
     QueryResponse executeQuery(CacheQuery query);
 }
