0c93c3d4d76bdee205cea89a648f6fecf9794cf0
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-10-18 15:36:37 +0200

    Improved Query subsystem
    improved query subsystem

--- extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/model/CacheQuery.java
+++ extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/model/CacheQuery.java
@@ -1,5 +1,48 @@
 package org.eclipse.dataspaceconnector.catalog.spi.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import org.eclipse.dataspaceconnector.spi.asset.Criterion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@JsonDeserialize(builder = CacheQuery.Builder.class)
 public class CacheQuery {
     // will be implemented in a subsequent PR
+    private final List<Criterion> criteria;
+
+    private CacheQuery(List<Criterion> criteria) {
+        this.criteria = criteria;
+    }
+
+    public List<Criterion> getCriteria() {
+        return criteria;
+    }
+
+
+    @JsonPOJOBuilder(withPrefix = "")
+    public static final class Builder {
+        // will be implemented in a subsequent PR
+        private final List<Criterion> criteria;
+
+        private Builder() {
+            criteria = new ArrayList<>();
+        }
+
+        @JsonCreator
+        public static Builder newInstance() {
+            return new Builder();
+        }
+
+        public Builder where(Criterion criterion) {
+            this.criteria.add(criterion);
+            return this;
+        }
+
+        public CacheQuery build() {
+            return new CacheQuery(criteria);
+        }
+    }
 }
