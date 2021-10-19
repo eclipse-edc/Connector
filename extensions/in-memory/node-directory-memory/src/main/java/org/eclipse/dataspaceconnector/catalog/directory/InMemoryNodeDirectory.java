c86af4a288f265537095224e7d698cecd4e461c9
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-10-19 13:20:11 +0200

    preparation for demo deployment

--- extensions/in-memory/node-directory-memory/src/main/java/org/eclipse/dataspaceconnector/catalog/directory/InMemoryNodeDirectory.java
+++ extensions/in-memory/node-directory-memory/src/main/java/org/eclipse/dataspaceconnector/catalog/directory/InMemoryNodeDirectory.java
@@ -2,43 +2,26 @@ package org.eclipse.dataspaceconnector.catalog.directory;
 
 import org.eclipse.dataspaceconnector.catalog.spi.FederatedCacheNode;
 import org.eclipse.dataspaceconnector.catalog.spi.FederatedCacheNodeDirectory;
-import org.eclipse.dataspaceconnector.spi.EdcException;
-import org.jetbrains.annotations.NotNull;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Stream;
 
 public class InMemoryNodeDirectory implements FederatedCacheNodeDirectory {
+    private final List<FederatedCacheNode> cache = new CopyOnWriteArrayList<>();
+
     @Override
     public List<FederatedCacheNode> getAll() {
-        return IntStream.range(0, 10).mapToObj(i -> getNode(i)).collect(Collectors.toList());
-    }
-
-    @NotNull
-    private FederatedCacheNode getNode(int number) {
-        return new FederatedCacheNode() {
-            @Override
-            public URL getUrl() {
-                try {
-                    return new URL("https://test.url1.com/" + number);
-                } catch (MalformedURLException e) {
-                    throw new EdcException(e);
-                }
-            }
-
-            @Override
-            public List<String> getSupportedProtocols() {
-                return List.of("ids-rest");
-            }
-        };
+        return List.copyOf(cache); //never return the internal copy
     }
 
     @Override
     public Stream<FederatedCacheNode> getAllAsync() {
         return Stream.of(getAll().toArray(new FederatedCacheNode[0]));
+    }
+
+    @Override
+    public void insert(FederatedCacheNode node) {
+        cache.add(node);
     }
 }
