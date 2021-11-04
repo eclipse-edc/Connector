0d0a9ea1e8737f3979f87c5de0f71cb20cc8c1b8
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-11-04 09:10:53 +0100

    removed unneeded dependency

--- extensions/catalog/federated-catalog-cache/build.gradle.kts
+++ extensions/catalog/federated-catalog-cache/build.gradle.kts
@@ -14,12 +14,10 @@ dependencies {
 
     implementation(project(":extensions:catalog:federated-catalog-spi"))
     implementation(project(":common:util"))
-    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
 
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
 
     // generates random names
-    // fixme: remove before PR submit
     implementation("info.schnatterer.moby-names-generator:moby-names-generator:20.10.0-r0")
 
     testImplementation(project(":core:bootstrap")) //for the console monitor
