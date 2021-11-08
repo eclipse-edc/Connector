77782c5a8439af6a788a9f2f3cdd68928d52f350
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-11-08 20:46:56 +0100

    PR remarks from @ndr-brt

--- extensions/catalog/federated-catalog-cache/src/test/java/org/eclipse/dataspaceconnector/catalog/cache/TestWorkQueue.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.dataspaceconnector.catalog.cache;
-
-public class TestWorkQueue extends DefaultWorkItemQueue {
-    public TestWorkQueue(int cap) {
-        super(cap);
-    }
-}
