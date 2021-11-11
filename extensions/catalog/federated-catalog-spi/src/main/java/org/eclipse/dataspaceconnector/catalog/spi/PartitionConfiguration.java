171db7a36f9f80b21356790241046cb420959544
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-11-11 13:38:25 +0100

    start crawling with an initial delay to avoid race conditions

--- extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/PartitionConfiguration.java
+++ extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/PartitionConfiguration.java
@@ -5,6 +5,7 @@ import org.eclipse.dataspaceconnector.catalog.spi.model.RecurringExecutionPlan;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
 
 import java.time.Duration;
+import java.util.Random;
 
 /**
  * Object that provides configuration for the {@link PartitionManager}.
@@ -18,6 +19,7 @@ public class PartitionConfiguration {
     private static final String PART_LOADER_BATCH_SIZE_SETTING = "edc.catalog.cache.loader.batch-size";
     private static final String PART_LOADER_RETRY_TIMEOUT = "edc.catalog.cache.loader.timeout-millis";
     private static final String PART_EXECUTION_PLAN_PERIOD_MINUTES = "edc.catalog.cache.execution.period-minutes";
+    private static final String PART_EXECUTION_PLAN_DELAY_MINUTES = "edc.catalog.cache.execution.delay-minutes";
     private final ServiceExtensionContext context;
 
     public PartitionConfiguration(ServiceExtensionContext context) {
@@ -44,6 +46,22 @@ public class PartitionConfiguration {
 
     public ExecutionPlan getExecutionPlan() {
         var minutes = Integer.parseInt(context.getSetting(PART_EXECUTION_PLAN_PERIOD_MINUTES, "10"));
-        return new RecurringExecutionPlan(Duration.ofMinutes(minutes));
+        var setting = context.getSetting(PART_EXECUTION_PLAN_DELAY_MINUTES, null);
+        int initialDelaySeconds;
+        if ("random".equals(setting) || setting == null) {
+            initialDelaySeconds = randomSeconds();
+        } else {
+            try {
+                initialDelaySeconds = Integer.parseInt(setting);
+            } catch (NumberFormatException ex) {
+                initialDelaySeconds = 0;
+            }
+        }
+        return new RecurringExecutionPlan(Duration.ofMinutes(minutes), Duration.ofSeconds(initialDelaySeconds));
+    }
+
+    private int randomSeconds() {
+        var rnd = new Random();
+        return 10 + rnd.nextInt(90);
     }
 }
