6e69a85ae903991fd2726001706e7c914b3aac50
ndr_brt <andrea.bertagnolli@gmail.com>
2021-11-22 20:31:59 +0100

    extensions: enable/delete @Disabled tests (#270)
    Co-authored-by: ndr_brt <Andrea.Bertagnolli@partner.bmwgroup.com>

--- extensions/azure/vault/src/test/java/org/eclipse/dataspaceconnector/security/azure/AzureVaultIntegrationTest.java
+++ extensions/azure/vault/src/test/java/org/eclipse/dataspaceconnector/security/azure/AzureVaultIntegrationTest.java
@@ -35,7 +35,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@Disabled
 @IntegrationTest
 public class AzureVaultIntegrationTest {
 
