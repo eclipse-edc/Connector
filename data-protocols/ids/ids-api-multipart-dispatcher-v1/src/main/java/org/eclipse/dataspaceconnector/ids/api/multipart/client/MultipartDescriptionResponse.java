43bdc509015514948f99bccfea5c3b0e9c399506
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2021-11-23 09:57:04 +0100

    ids-multipart: use return type ModelClass for MultipartDescriptionResponse

--- data-protocols/ids/ids-api-multipart-dispatcher-v1/src/main/java/org/eclipse/dataspaceconnector/ids/api/multipart/client/MultipartDescriptionResponse.java
+++ data-protocols/ids/ids-api-multipart-dispatcher-v1/src/main/java/org/eclipse/dataspaceconnector/ids/api/multipart/client/MultipartDescriptionResponse.java
@@ -1,18 +1,19 @@
 package org.eclipse.dataspaceconnector.ids.api.multipart.client;
 
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import de.fraunhofer.iais.eis.Message;
+import de.fraunhofer.iais.eis.ModelClass;
 import de.fraunhofer.iais.eis.ResponseMessage;
 
-public class MultipartDescriptionResponse<T> implements MultipartResponse<T> {
+public class MultipartDescriptionResponse implements MultipartResponse<ModelClass> {
 
-    private final ResponseMessage header;
+    private ResponseMessage header;
 
-    private final T payload;
+    private ModelClass payload;
 
-    public MultipartDescriptionResponse(final ResponseMessage header, final T payload) {
-        this.header = header;
-        this.payload = payload;
-    }
+    private MultipartDescriptionResponse() { }
 
     @Override
     public Message getHeader() {
@@ -20,10 +21,37 @@ public class MultipartDescriptionResponse<T> implements MultipartResponse<T> {
     }
 
     @Override
-    public T getPayload() {
+    public ModelClass getPayload() {
         return payload;
     }
 
-    //TODO builder
+    public static class Builder {
+        private final MultipartDescriptionResponse descriptionResponse;
+
+        private Builder() {
+            this.descriptionResponse = new MultipartDescriptionResponse();
+        }
+
+        @JsonCreator
+        public static Builder newInstance() {
+            return new Builder();
+        }
+
+        public Builder header(ResponseMessage header) {
+            this.descriptionResponse.header = header;
+            return this;
+        }
+
+        public Builder payload(ModelClass payload) {
+            this.descriptionResponse.payload = payload;
+            return this;
+        }
+
+        public MultipartDescriptionResponse build() {
+            Objects.requireNonNull(descriptionResponse.header, "header");
+            Objects.requireNonNull(descriptionResponse.payload, "payload");
+            return descriptionResponse;
+        }
+    }
 
 }
--- data-protocols/ids/ids-api-multipart-dispatcher-v1/src/main/java/org/eclipse/dataspaceconnector/ids/api/multipart/client/MultipartResponse.java
+++ data-protocols/ids/ids-api-multipart-dispatcher-v1/src/main/java/org/eclipse/dataspaceconnector/ids/api/multipart/client/MultipartResponse.java
@@ -2,7 +2,6 @@ package org.eclipse.dataspaceconnector.ids.api.multipart.client;
 
 import de.fraunhofer.iais.eis.Message;
 
-//TODO T extends ModelClass (later Infomodel version)
 public interface MultipartResponse<T> {
 
     Message getHeader();
