55c22cb70329adcac14661f9fe7a4e7e1cc0dea0
jpampus <julia.pampus@isst.fraunhofer.de>
2021-12-02 20:10:04 +0100

    fix: add missing dependency

--- data-protocols/ids/ids-api-multipart-endpoint-v1/build.gradle.kts
+++ data-protocols/ids/ids-api-multipart-endpoint-v1/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     api(project(":data-protocols:ids:ids-spi"))
     api(project(":data-protocols:ids:ids-core"))
     api(project(":data-protocols:ids:ids-transform-v1"))
+    api(project(":core:transfer"))
 
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
     implementation("org.glassfish.jersey.media:jersey-media-multipart:${jerseyVersion}")
