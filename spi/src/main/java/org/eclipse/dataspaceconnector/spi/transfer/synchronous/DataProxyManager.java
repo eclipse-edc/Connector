d9d979595010d2f551cb53e9a9e67ba1480b8b23
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2021-12-07 19:33:51 +0100

    fixed dataproxy feature

--- spi/src/main/java/org/eclipse/dataspaceconnector/spi/transfer/synchronous/DataProxyManager.java
+++ spi/src/main/java/org/eclipse/dataspaceconnector/spi/transfer/synchronous/DataProxyManager.java
@@ -3,7 +3,7 @@ package org.eclipse.dataspaceconnector.spi.transfer.synchronous;
 import org.eclipse.dataspaceconnector.spi.types.domain.transfer.DataRequest;
 
 public interface DataProxyManager {
-    String FEATURE = "edc:transfer:dataproxyregistry";
+    String FEATURE = "edc:transfer:dataproxymanager";
 
     void addProxy(String type, DataProxy proxy);
 
