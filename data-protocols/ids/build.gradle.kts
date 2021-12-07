fc2492df7564c43c7b1ce422bf7d5d5b66a44044
Dominik Pinsel <dominik.pinsel@daimler.com>
2021-12-07 15:40:16 +0100

    ids: delete transfer extension
    Signed-off-by: Dominik Pinsel <dominik.pinsel@daimler.com>

--- data-protocols/ids/build.gradle.kts
+++ data-protocols/ids/build.gradle.kts
@@ -21,7 +21,6 @@ dependencies {
     api(project(":spi"))
     api(project(":data-protocols:ids:ids-spi"))
     api(project(":data-protocols:ids:ids-core"))
-    api(project(":data-protocols:ids:ids-api-transfer"))
     api(project(":data-protocols:ids:ids-transform-v1"))
     api(project(":data-protocols:ids:ids-api-multipart-endpoint-v1"))
     api(project(":data-protocols:ids:ids-api-multipart-dispatcher-v1"))
