69317475473c2893ba48f8d97c9e77afd5e66f0b
ndr_brt <andrea.bertagnolli@partner.bmwgroup.com>
2021-12-17 08:55:50 +0100

    extensions: extract methods

--- extensions/aws/s3/provision/src/main/java/org/eclipse/dataspaceconnector/provision/aws/s3/S3DeprovisionPipeline.java
+++ extensions/aws/s3/provision/src/main/java/org/eclipse/dataspaceconnector/provision/aws/s3/S3DeprovisionPipeline.java
@@ -19,21 +19,25 @@ import net.jodah.failsafe.RetryPolicy;
 import org.eclipse.dataspaceconnector.provision.aws.provider.ClientProvider;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.transfer.provision.DeprovisionResponse;
-import org.eclipse.dataspaceconnector.spi.transfer.provision.ProvisionResponse;
 import software.amazon.awssdk.services.iam.IamAsyncClient;
 import software.amazon.awssdk.services.iam.model.DeleteRolePolicyRequest;
+import software.amazon.awssdk.services.iam.model.DeleteRolePolicyResponse;
 import software.amazon.awssdk.services.iam.model.DeleteRoleRequest;
 import software.amazon.awssdk.services.iam.model.DeleteRoleResponse;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.Delete;
 import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
+import software.amazon.awssdk.services.s3.model.DeleteBucketResponse;
 import software.amazon.awssdk.services.s3.model.DeleteObjectsRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectsResponse;
 import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 public class S3DeprovisionPipeline {
     private final ClientProvider clientProvider;
@@ -56,32 +60,44 @@ public class S3DeprovisionPipeline {
         monitor.info("S3 Deprovisioning: list bucket contents");
         String role = resource.getRole();
         return s3Client.listObjectsV2(listBucket(bucketName))
-                .thenCompose(listObjectsResponse -> {
-                    //todo: collect identifiers into a single request using the stream api
-                    var identifiers = listObjectsResponse.contents().stream()
-                            .map(s3object -> ObjectIdentifier.builder().key(s3object.key()).build())
-                            .collect(Collectors.toList());
-                    var deleteRequest = DeleteObjectsRequest.builder().bucket(bucketName).delete(Delete.builder().objects(identifiers).build()).build();
-                    monitor.info("S3 Deprovisioning: delete bucket contents: " + identifiers.stream().map(ObjectIdentifier::key).collect(Collectors.joining(", ")));
-                    return s3Client.deleteObjects(deleteRequest);
-                })
-                .thenCompose(deleteObjectsResponse -> Failsafe.with(retryPolicy).getStageAsync(() -> {
-                    monitor.info("S3 Deprovisioning: delete bucket");
-                    return s3Client.deleteBucket(deleteBucket(bucketName));
-                }))
-                .thenCompose(listAttachedRolePoliciesResponse -> Failsafe.with(retryPolicy).getStageAsync(() -> {
-                    monitor.info("S3 Deprovisioning: deleting inline policies for Role " + role);
-                    return iamClient.deleteRolePolicy(DeleteRolePolicyRequest.builder().roleName(role).policyName(role).build());
-                }))
-                .thenCompose(deleteRolePolicyResponse -> Failsafe.with(retryPolicy).getStageAsync(() -> {
-                    monitor.info("S3 Deprovisioning: delete role");
-                    return iamClient.deleteRole(DeleteRoleRequest.builder().roleName(role).build());
-                }))
+                .thenCompose(listObjectsResponse -> deleteObjects(s3Client, bucketName, listObjectsResponse))
+                .thenCompose(deleteObjectsResponse -> deleteBucket(s3Client, bucketName))
+                .thenCompose(listAttachedRolePoliciesResponse -> deleteRolePolicy(iamClient, role))
+                .thenCompose(deleteRolePolicyResponse -> deleteRole(iamClient, role))
                 .thenApply(response -> DeprovisionResponse.Builder.newInstance().ok().resource(resource).build());
     }
 
-    private DeleteBucketRequest deleteBucket(String bucketName) {
-        return DeleteBucketRequest.builder().bucket(bucketName).build();
+    private CompletableFuture<DeleteRoleResponse> deleteRole(IamAsyncClient iamClient, String role) {
+        return Failsafe.with(retryPolicy).getStageAsync(() -> {
+            monitor.info("S3 Deprovisioning: delete role");
+            return iamClient.deleteRole(DeleteRoleRequest.builder().roleName(role).build());
+        });
+    }
+
+    private CompletableFuture<DeleteRolePolicyResponse> deleteRolePolicy(IamAsyncClient iamClient, String role) {
+        return Failsafe.with(retryPolicy).getStageAsync(() -> {
+            monitor.info("S3 Deprovisioning: deleting inline policies for Role " + role);
+            return iamClient.deleteRolePolicy(DeleteRolePolicyRequest.builder().roleName(role).policyName(role).build());
+        });
+    }
+
+    private CompletableFuture<DeleteBucketResponse> deleteBucket(S3AsyncClient s3Client, String bucketName) {
+        return Failsafe.with(retryPolicy).getStageAsync(() -> {
+            monitor.info("S3 Deprovisioning: delete bucket");
+            return s3Client.deleteBucket(DeleteBucketRequest.builder().bucket(bucketName).build());
+        });
+    }
+
+    private CompletableFuture<DeleteObjectsResponse> deleteObjects(S3AsyncClient s3Client, String bucketName, ListObjectsV2Response listObjectsResponse) {
+        var identifiers = listObjectsResponse.contents().stream()
+                .map(s3object -> ObjectIdentifier.builder().key(s3object.key()).build())
+                .collect(Collectors.toList());
+
+        var deleteRequest = DeleteObjectsRequest.builder()
+                .bucket(bucketName).delete(Delete.builder().objects(identifiers).build())
+                .build();
+        monitor.info("S3 Deprovisioning: delete bucket contents: " + identifiers.stream().map(ObjectIdentifier::key).collect(joining(", ")));
+        return s3Client.deleteObjects(deleteRequest);
     }
 
     private ListObjectsV2Request listBucket(String bucketName) {
