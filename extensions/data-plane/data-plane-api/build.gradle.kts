369df24a1ade6f74984b418d15dcad8a0d65e2d4
Jim Marino <jim.marino@gmail.com>
2022-01-27 23:08:16 +0100

    Fix rebase; add stub modules

---
+++ extensions/data-plane/data-plane-api/build.gradle.kts
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+val okHttpVersion: String by project
+val jodahFailsafeVersion: String by project
+
+plugins {
+    `java-library`
+}
+
+dependencies {
+    implementation(project(":spi:protocol-spi"))
+    implementation(project(":extensions:data-plane:data-plane-spi"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("data-plane-api") {
+            artifactId = "data-plane-api"
+            from(components["java"])
+        }
+    }
+}
