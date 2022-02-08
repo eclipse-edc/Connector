5c1bea6cd81d508c4de51120a0b3c2a831f1091e
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-02-08 14:38:20 +0100

    Added generic exception mapper (#630)
    * added exception handler
    
    * removed unneeded class
    
    * pr remarks
    
    Co-authored-by: Paul Latzelsperger <paul.latzelsperger@beardyinc.com>

---
+++ extensions/api/api-core/build.gradle.kts
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (c) 2021 Daimler TSS GmbH
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Daimler TSS GmbH - Initial API and Implementation
+ *
+ */
+
+
+val infoModelVersion: String by project
+val rsApi: String by project
+val jerseyVersion: String by project
+
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api(project(":spi"))
+    implementation(project(":common:util"))
+    testImplementation(project(":core:base"))
+
+    implementation("jakarta.ws.rs:jakarta.ws.rs-api:${rsApi}")
+    testImplementation("org.glassfish.jersey.core:jersey-common:${jerseyVersion}")
+    testImplementation("org.glassfish.jersey.core:jersey-server:${jerseyVersion}")
+
+    testImplementation(testFixtures(project(":launchers:junit")))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("api-core") {
+            artifactId = "api-core"
+            from(components["java"])
+        }
+    }
+}
