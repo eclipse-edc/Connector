5a60efa8eb6a6f71b2f99af49bbff2a9040d6680
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2022-02-14 11:58:36 +0100

    re-added testutil tests

--- common/util/src/test/java/org/eclipse/dataspaceconnector/common/testfixtures/TestUtilsTest.java
+++ common/util/src/test/java/org/eclipse/dataspaceconnector/common/testfixtures/TestUtilsTest.java
@@ -16,15 +16,48 @@ package org.eclipse.dataspaceconnector.common.testfixtures;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TestUtilsTest {
 
     private static final int EPHEMERAL_PORT_MIN = 1024;
     private static final int EPHEMERAL_PORT_MAX = 65535;
 
+
     @Test
-    void findsRandomPort() {
-        assertThat(TestUtils.getFreePort()).isBetween(EPHEMERAL_PORT_MIN, EPHEMERAL_PORT_MAX);
+    void getFreePort() {
+        assertThat(TestUtils.getFreePort()).isGreaterThan(EPHEMERAL_PORT_MIN).isLessThan(EPHEMERAL_PORT_MAX);
+    }
+
+    @Test
+    void getFreePort_lowerBound() {
+        assertThat(TestUtils.getFreePort(5000)).isGreaterThanOrEqualTo(5000).isLessThan(EPHEMERAL_PORT_MAX);
+
+        assertThatThrownBy(() -> TestUtils.getFreePort(-1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> TestUtils.getFreePort(0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> TestUtils.getFreePort(EPHEMERAL_PORT_MAX)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getFreePort_withUpperAndLowerBound() {
+        assertThat(TestUtils.getFreePort(5000, 7000)).isGreaterThanOrEqualTo(5000).isLessThan(7000);
+
+        assertThatThrownBy(() -> TestUtils.getFreePort(5000, 4999)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> TestUtils.getFreePort(5000, 5000)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> TestUtils.getFreePort(5000, EPHEMERAL_PORT_MAX + 1)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> TestUtils.getFreePort(5000, 0)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getFreePort_whenOccupied() throws IOException {
+        var port = TestUtils.getFreePort(EPHEMERAL_PORT_MIN);
+
+        try (var socket = new ServerSocket(port)) {
+            assertThat(TestUtils.getFreePort(EPHEMERAL_PORT_MIN)).describedAs("Next free port").isGreaterThan(socket.getLocalPort());
+        }
     }
 }
