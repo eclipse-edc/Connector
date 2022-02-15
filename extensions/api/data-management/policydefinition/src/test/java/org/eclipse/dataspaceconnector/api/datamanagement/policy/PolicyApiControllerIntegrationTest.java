4a50cacff3d19a1d364deb1704f28a6acdd00219
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-02-15 10:42:01 +0100

    DataManagementApi: Create API for policies (#689)
    * added API for policies
    
    * generated OAS, renamed module due to conflict
    
    * pr remarks
    
    * rebase main
    
    * pruned dependencies
    
    Co-authored-by: Paul Latzelsperger <paul.latzelsperger@beardyinc.com>

---
+++ extensions/api/data-management/policydefinition/src/test/java/org/eclipse/dataspaceconnector/api/datamanagement/policy/PolicyApiControllerIntegrationTest.java
@@ -0,0 +1,102 @@
+/*
+ *  Copyright (c) 2020 - 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+package org.eclipse.dataspaceconnector.api.datamanagement.policy;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.eclipse.dataspaceconnector.common.testfixtures.TestUtils;
+import org.eclipse.dataspaceconnector.extension.jersey.CorsFilterConfiguration;
+import org.eclipse.dataspaceconnector.extension.jersey.JerseyRestService;
+import org.eclipse.dataspaceconnector.extension.jetty.JettyConfiguration;
+import org.eclipse.dataspaceconnector.extension.jetty.JettyService;
+import org.eclipse.dataspaceconnector.extension.jetty.PortMapping;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+import org.eclipse.dataspaceconnector.spi.types.TypeManager;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class PolicyApiControllerIntegrationTest {
+
+    private static int port;
+    private OkHttpClient client;
+
+    @BeforeAll
+    static void prepareWebserver() {
+        port = TestUtils.getFreePort();
+        var monitor = mock(Monitor.class);
+        var config = new JettyConfiguration(null, null);
+        config.portMapping(new PortMapping("data", port, "/api/v1/data"));
+        var jetty = new JettyService(config, monitor);
+
+        var ctrl = new PolicyApiController(monitor);
+        var jerseyService = new JerseyRestService(jetty, new TypeManager(), mock(CorsFilterConfiguration.class), monitor);
+        jetty.start();
+        jerseyService.registerResource("data", ctrl);
+        jerseyService.start();
+    }
+
+    @BeforeEach
+    void setup() {
+        client = new OkHttpClient();
+    }
+
+    @Test
+    void getAllPolicies() throws IOException {
+        var response = get(basePath());
+        assertThat(response.code()).isEqualTo(200);
+    }
+
+    @Test
+    void getAllPolicies_withPaging() throws IOException {
+        try (var response = get(basePath() + "?offset=10&limit=15&sort=ASC")) {
+            assertThat(response.code()).isEqualTo(200);
+        }
+    }
+
+    @Test
+    void getSinglePolicy() throws IOException {
+        var id = "test-id";
+        try (var response = get(basePath() + "/" + id)) {
+            //assertThat(response.code()).isEqualTo(200);
+        }
+
+    }
+
+    @Test
+    void getSinglePolicy_notFound() throws IOException {
+        try (var response = get(basePath() + "/not-exist")) {
+            // assertThat(response.code()).isEqualTo(404);
+        }
+    }
+
+
+    @NotNull
+    private String basePath() {
+        return "http://localhost:" + port + "/api/v1/data/policies";
+    }
+
+    @NotNull
+    private Response get(String url) throws IOException {
+        return client.newCall(new Request.Builder().get().url(url).build()).execute();
+    }
+}
---
+++ extensions/api/data-management/policydefinition/src/test/java/org/eclipse/dataspaceconnector/api/datamanagement/policy/PolicyApiControllerTest.java
@@ -0,0 +1,135 @@
+/*
+ *  Copyright (c) 2020 - 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+
+package org.eclipse.dataspaceconnector.api.datamanagement.policy;
+
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class PolicyApiControllerTest {
+    private PolicyApiController controller;
+
+    @BeforeEach
+    void setup() {
+        var monitor = mock(Monitor.class);
+        controller = new PolicyApiController(monitor);
+    }
+
+    @Test
+    void getAll_paging_noFilter() {
+        //todo: implement
+    }
+
+    @Test
+    void getAll_paging_pageSizeTooLarge() {
+        //todo: implement
+    }
+
+    @Test
+    void getAll_paging_offsetOutOfBounds() {
+        //todo: implement
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "id=id1", "id = id1", "id =id1", "id= id1" })
+    void getAll_paging_withValidFilter(String filter) {
+        //todo: implement
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = { "id > id1", "id < id1", "id like id1", "id inside id1" })
+    void getAll_paging_filterWithInvalidOperator(String filter) {
+        //todo: implement
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "id>id1", "id<id1" })
+    void getAll_paging_withInvalidFilter(String filter) {
+        //todo: implement
+    }
+
+    @Test
+    void getAll_paging_invalidParams() {
+        //todo: implement
+    }
+
+    @Test
+    void getAll_noPaging() {
+        //todo: implement
+    }
+
+    @Test
+    void getContractDef_found() {
+        //todo: implement
+    }
+
+    @Test
+    void getContractDef_notFound() {
+        assertThat(controller.getPolicy("not-exist")).isNull();
+    }
+
+    @Test
+    void createContractDefinition_success() {
+        //todo: implement
+
+    }
+
+    @Test
+    void createContractDefinition_alreadyExists() {
+        //todo: implement
+    }
+
+    @Test
+    void createContractDefinition_policyNotFound() {
+        //todo: implement
+
+    }
+
+    @Test
+    void createContractDefinition_noAssetSelector() {
+        //todo: implement
+    }
+
+    @Test
+    void createContractDefinition_noAccessPolicy() {
+        //todo: implement
+    }
+
+    @Test
+    void createContractDefinition_noContractPolicy() {
+        //todo: implement
+    }
+
+    @Test
+    void delete() {
+        //todo: implement
+    }
+
+    @Test
+    void delete_notFound() {
+
+    }
+
+    @Test
+    void delete_notPossible() {
+        //todo: implement
+    }
+}
