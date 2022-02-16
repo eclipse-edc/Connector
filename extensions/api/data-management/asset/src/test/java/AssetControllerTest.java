8eb020eb3d79ef84d6245de8b366d16b230e183b
Diego Gomez <diego.gomez.external@zf.com>
2022-02-16 14:17:21 +0100

    Management-api: Create Unit Tests Methods in class AssetControllerTest #587

---
+++ extensions/api/data-management/asset/src/test/java/AssetControllerTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022 Diego Gomez
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Contributors:
+ *   Diego Gomez - Initial API and Implementation
+ */
+
+import org.eclipse.dataspaceconnector.api.datamanagement.asset.AssetController;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class AssetControllerTest {
+
+    private AssetController assetController;
+
+    @BeforeEach
+    void setUp(){
+        var monitor = mock(Monitor.class);
+        assetController = new AssetController(monitor);
+    }
+
+    @Test
+    void createAsset(){
+        //Todo: implement
+    }
+
+    @Test
+    void createAssetAlreadyExists(){
+        //Todo: implement
+    }
+
+    @Test
+    void createAssetPathNotExists(){
+        //Todo: implement
+    }
+
+    @Test
+    void listAllAssets(){
+        //Todo: implement
+    }
+
+    @Test
+    void getAssetFromID(){
+        //Todo: implement
+    }
+
+    @Test
+    void getAssetFromIDNotExists(){
+        //Todo: implement
+    }
+
+    @Test
+    void removeAssetfromID(){
+        //Todo: implement
+    }
+
+    @Test
+    void removeAssetFromIDNotExists(){
+        //Todo: implement
+    }
+
+}
