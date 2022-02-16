81a2c66ec070c6ac9f89399b4557daee61c6af61
Diego Gomez <diego.gomez.external@zf.com>
2022-02-16 14:17:20 +0100

    Management-api: Create Unit tests in Model classes #587

---
+++ extensions/api/data-management/asset/src/test/java/org/eclipse/dataspaceconnector/api/datamanagement/asset/model/DataAddressTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Diego Gomez
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Contributors:
+ *   Diego Gomez - Initial API and Implementation
+ */
+
+package org.eclipse.dataspaceconnector.api.datamanagement.asset.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DataAddressTest {
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp(){
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void verifySerialization() throws JsonProcessingException {
+        var dataAddress = DataAddress.Builder.newInstance().properties(Collections.singletonMap("asset-1","/localhost")).build();
+
+        var str = objectMapper.writeValueAsString(dataAddress);
+
+        assertThat(str).isNotNull();
+
+        var deserialized = objectMapper.readValue(str, DataAddress.class);
+        assertThat(deserialized).usingRecursiveComparison().isEqualTo(dataAddress);
+    }
+}
