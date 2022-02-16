20f0b5cf8a8f138d0807e062bd5e34fcf20eae36
Diego Gomez <diego.gomez.external@zf.com>
2022-02-16 14:17:21 +0100

    Management-api: Create Copyright in classes #587

--- extensions/api/data-management/asset/build.gradle.kts
+++ extensions/api/data-management/asset/build.gradle.kts
@@ -1,17 +1,15 @@
 /*
- *  Copyright (c) 2021 Daimler TSS GmbH
+ * Copyright (c) 2022 Diego Gomez
  *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
- *  SPDX-License-Identifier: Apache-2.0
+ * SPDX-License-Identifier: Apache-2.0
  *
- *  Contributors:
- *       Daimler TSS GmbH - Initial API and Implementation
- *
+ * Contributors:
+ *   Diego Gomez - Initial API and Implementation
  */
-
 
 val infoModelVersion: String by project
 val rsApi: String by project
@@ -20,7 +18,7 @@ val jerseyVersion: String by project
 plugins {
     `java-library`
 }
-Perfekt
+
 dependencies {
     api(project(":spi"))
     api(project(":common:util"))
--- extensions/api/data-management/asset/src/main/java/org/eclipse/dataspaceconnector/api/datamanagement/asset/AssetControllerServiceExtension.java
+++ extensions/api/data-management/asset/src/main/java/org/eclipse/dataspaceconnector/api/datamanagement/asset/AssetControllerServiceExtension.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2022 Diego Gomez
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Contributors:
+ *   Diego Gomez - Initial API and Implementation
+ */
+
 package org.eclipse.dataspaceconnector.api.datamanagement.asset;
 
 import org.eclipse.dataspaceconnector.spi.WebService;
