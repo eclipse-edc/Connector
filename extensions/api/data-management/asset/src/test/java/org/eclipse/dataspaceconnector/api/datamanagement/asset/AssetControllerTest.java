b9280a020bbc5ac84dc5c63639559951d7948f5c
Diego Gomez <diego.gomez.external@zf.com>
2022-02-16 14:26:08 +0100

    Management-api: Correct Copyright in Module asset-management-api

--- extensions/api/data-management/asset/src/test/java/org/eclipse/dataspaceconnector/api/datamanagement/asset/AssetControllerTest.java
+++ extensions/api/data-management/asset/src/test/java/org/eclipse/dataspaceconnector/api/datamanagement/asset/AssetControllerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Diego Gomez
+ * Copyright (c) 2022 ZF Friedrichshafen AG
  *
  * This program and the accompanying materials are made available under the
  * terms of the Apache License, Version 2.0 which is available at
@@ -8,7 +8,7 @@
  * SPDX-License-Identifier: Apache-2.0
  *
  * Contributors:
- *   Diego Gomez - Initial API and Implementation
+ *    ZF Friedrichshafen AG - Initial API and Implementation
  */
 
 package org.eclipse.dataspaceconnector.api.datamanagement.asset;
