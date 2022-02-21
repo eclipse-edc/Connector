6502963474024ca43b138e27b55ad566ea00a0cc
Alexandre Gattiker <algattik@users.noreply.github.com>
2022-02-21 15:36:49 +0100

    Feature/618/23 azure dpf (#85)

---
+++ extensions/azure/data-plane-azure-storage/README.md
@@ -0,0 +1,54 @@
+## Azure storage Data Plane module
+
+### About this module
+
+This module contains a Data Plane extension to copy data to and from Azure Blob storage.
+
+When used as a source, it currently only supports copying a single blob.
+
+### Example usage
+
+Create two Azure storage accounts with arbitrary names, here called ACCOUNT1 and ACCOUNT2.
+
+In ACCOUNT1, create a storage container named `src`.
+
+In ACCOUNT2, create a storage container named `dest`.
+
+In ACCOUNT1, under storage container `src`, upload a file named `file.txt` with arbitrary content.
+
+Run the Data Plane server:
+
+```sh
+env web.http.public.port=9191 web.http.control.path=/control ./gradlew :launchers:data-plane-server:run
+```
+
+In another terminal, send a data flow request, replacing ACCOUNT1 and ACCOUNT2 with the storage account names, and KEY1 and KEY2 with their respective access keys.
+
+```sh
+curl 'http://localhost:8181/control/transfer' \
+--header 'Content-Type: application/json' \
+--data '{
+    "id": "B4819DE5-8B9F-4B44-8227-F37CF94744E9",
+    "edctype": "dataspaceconnector:dataflowrequest",
+    "processId": "6593E90E-DD13-4132-A6D0-ADEB02C32ECB",
+    "sourceDataAddress": {
+        "properties": {
+            "type": "AzureStorageBlobData",
+            "account": "ACCOUNT1",
+            "container": "src",
+            "blob": "file.txt",
+            "sharedKey": "KEY1"
+        }
+    },
+    "destinationDataAddress": {
+        "properties": {
+            "type": "AzureStorageBlobData",
+            "account": "ACCOUNT2",
+            "container": "dest",
+            "sharedKey": "KEY2"
+        }
+    }
+}'
+```
+
+The file `file.txt` is now copied into ACCOUNT2 under the container `dest`.
---
+++ extensions/azure/data-plane-azure-storage/src/test/java/org/eclipse/dataspaceconnector/azure/dataplane/azurestorage/pipeline/AzureDataSourceToDataSinkTest.java
@@ -0,0 +1,197 @@
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
+package org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.pipeline;
+
+import com.github.javafaker.Faker;
+import net.jodah.failsafe.RetryPolicy;
+import org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.adapter.BlobAdapter;
+import org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.adapter.BlobAdapterFactory;
+import org.eclipse.dataspaceconnector.azure.dataplane.azurestorage.pipeline.AzureStorageTestFixtures.FakeBlobAdapter;
+import org.eclipse.dataspaceconnector.dataplane.spi.result.TransferResult;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AzureDataSourceToDataSinkTest {
+    static Faker faker = new Faker();
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+    Monitor monitor = mock(Monitor.class);
+    FakeBlobAdapter fakeSource = new FakeBlobAdapter();
+    FakeBlobAdapter fakeSink = new FakeBlobAdapter();
+    String sourceAccountName = AzureStorageTestFixtures.createAccountName();
+    String sourceContainerName = AzureStorageTestFixtures.createContainerName();
+    String sourceSharedKey = AzureStorageTestFixtures.createSharedKey();
+    String sinkAccountName = AzureStorageTestFixtures.createAccountName();
+    String sinkContainerName = AzureStorageTestFixtures.createContainerName();
+    String sinkSharedKey = AzureStorageTestFixtures.createSharedKey();
+    String requestId = UUID.randomUUID().toString();
+    String errorMessage = faker.lorem().sentence();
+
+    /**
+     * Verifies a sink is able to pull data from the source without exceptions if both endpoints are functioning.
+     */
+    @Test
+    void transfer_success() {
+        var fakeSourceFactory = mock(BlobAdapterFactory.class);
+        when(fakeSourceFactory.getBlobAdapter(
+                sourceAccountName,
+                sourceContainerName,
+                fakeSource.name,
+                sourceSharedKey
+        )).thenReturn(fakeSource);
+
+        var dataSource = AzureStorageDataSource.Builder.newInstance()
+                .accountName(sourceAccountName)
+                .containerName(sourceContainerName)
+                .sharedKey(sourceSharedKey)
+                .blobName(fakeSource.name)
+                .requestId(requestId)
+                .retryPolicy(new RetryPolicy<>())
+                .blobAdapterFactory(fakeSourceFactory)
+                .monitor(monitor)
+                .build();
+
+        var fakeSinkFactory = mock(BlobAdapterFactory.class);
+        when(fakeSinkFactory.getBlobAdapter(
+                sinkAccountName,
+                sinkContainerName,
+                fakeSource.name,
+                sinkSharedKey
+        )).thenReturn(fakeSink);
+
+        var dataSink = AzureStorageDataSink.Builder.newInstance()
+                .accountName(sinkAccountName)
+                .containerName(sinkContainerName)
+                .sharedKey(sinkSharedKey)
+                .requestId(requestId)
+                .blobAdapterFactory(fakeSinkFactory)
+                .executorService(executor)
+                .monitor(monitor)
+                .build();
+
+        assertThat(dataSink.transfer(dataSource)).succeedsWithin(500, TimeUnit.MILLISECONDS)
+                .satisfies(transferResult -> assertThat(transferResult.succeeded()).isTrue());
+
+        assertThat(fakeSink.out.toString()).isEqualTo(fakeSource.content);
+    }
+
+    /**
+     * Verifies an exception thrown by the source endpoint is handled correctly.
+     */
+    @Test
+    void transfer_WhenSourceFails_fails() throws Exception {
+
+        // simulate source error
+        var blobAdapter = mock(BlobAdapter.class);
+        when(blobAdapter.getBlobName()).thenReturn(fakeSource.name);
+        when(blobAdapter.openInputStream()).thenThrow(new RuntimeException(errorMessage));
+        var fakeSourceFactory = mock(BlobAdapterFactory.class);
+        when(fakeSourceFactory.getBlobAdapter(
+                sourceAccountName,
+                sourceContainerName,
+                fakeSource.name,
+                sourceSharedKey
+        )).thenReturn(blobAdapter);
+
+        var dataSource = AzureStorageDataSource.Builder.newInstance()
+                .accountName(sourceAccountName)
+                .containerName(sourceContainerName)
+                .sharedKey(sourceSharedKey)
+                .blobName(fakeSource.name)
+                .requestId(requestId)
+                .retryPolicy(new RetryPolicy<>())
+                .blobAdapterFactory(fakeSourceFactory)
+                .monitor(monitor)
+                .build();
+
+        var fakeSinkFactory = mock(BlobAdapterFactory.class);
+        when(fakeSinkFactory.getBlobAdapter(
+                sinkAccountName,
+                sinkContainerName,
+                fakeSource.name,
+                sinkSharedKey
+        )).thenReturn(fakeSink);
+
+        var dataSink = AzureStorageDataSink.Builder.newInstance()
+                .accountName(sinkAccountName)
+                .containerName(sinkContainerName)
+                .sharedKey(sinkSharedKey)
+                .requestId(requestId)
+                .blobAdapterFactory(fakeSinkFactory)
+                .executorService(executor)
+                .monitor(monitor)
+                .build();
+
+        TransferResult transferResult = dataSink.transfer(dataSource).get();
+        assertThat(transferResult.failed()).isTrue();
+        assertThat(transferResult.getFailureMessages()).containsExactly(format("Error reading blob %s", fakeSource.name));
+    }
+
+    /**
+     * Verifies an exception thrown by the sink endpoint is handled correctly.
+     */
+    @Test
+    void transfer_whenSinkFails_fails() throws Exception {
+
+        // source completes normally
+        var fakeSourceFactory = mock(BlobAdapterFactory.class);
+        when(fakeSourceFactory.getBlobAdapter(
+                sourceAccountName,
+                sourceContainerName,
+                fakeSource.name,
+                sourceSharedKey
+        )).thenReturn(fakeSource);
+
+        var dataSource = AzureStorageDataSource.Builder.newInstance()
+                .accountName(sourceAccountName)
+                .containerName(sourceContainerName)
+                .sharedKey(sourceSharedKey)
+                .blobName(fakeSource.name)
+                .requestId(requestId)
+                .retryPolicy(new RetryPolicy<>())
+                .blobAdapterFactory(fakeSourceFactory)
+                .monitor(monitor)
+                .build();
+
+        // sink endpoint raises an exception
+        var blobAdapter = mock(BlobAdapter.class);
+        when(blobAdapter.getOutputStream()).thenThrow(new RuntimeException());
+        var fakeSinkFactory = mock(BlobAdapterFactory.class);
+        when(fakeSinkFactory.getBlobAdapter(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(blobAdapter);
+
+        var dataSink = AzureStorageDataSink.Builder.newInstance()
+                .accountName(sinkAccountName)
+                .containerName(sinkContainerName)
+                .sharedKey(sinkSharedKey)
+                .requestId(requestId)
+                .blobAdapterFactory(fakeSinkFactory)
+                .executorService(executor)
+                .monitor(monitor)
+                .build();
+
+        assertThat(dataSink.transfer(dataSource).get().failed()).isTrue();
+    }
+
+}
