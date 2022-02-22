0e33e08730582e952b97bb3656489827270ebbbc
Marc Gomez <marcgomez@microsoft.com>
2022-02-22 16:18:07 +0100

    Unit test for ParallelSink

---
+++ extensions/data-plane/data-plane-spi/src/test/java/org/eclipse/dataspaceconnector/dataplane/spi/pipeline/ParallelSinkTest.java
@@ -0,0 +1,103 @@
+package org.eclipse.dataspaceconnector.dataplane.spi.pipeline;
+
+import com.github.javafaker.Faker;
+import org.eclipse.dataspaceconnector.dataplane.spi.result.TransferResult;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
+import org.eclipse.dataspaceconnector.spi.response.ResponseStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ParallelSinkTest {
+
+    Faker faker = new Faker();
+    Monitor monitor = mock(Monitor.class);
+    ExecutorService executor = Executors.newFixedThreadPool(2);
+    String dataSourceName = faker.lorem().word();
+    String dataSourceContent = faker.lorem().characters();
+    String errorMessage = faker.lorem().sentence();
+
+    FakeParallelSink fakeSink;
+
+    @BeforeEach
+    void setup() {
+        fakeSink = new FakeParallelSink();
+        fakeSink.monitor = monitor;
+        fakeSink.executorService = executor;
+        fakeSink.requestId = UUID.randomUUID().toString();
+    }
+
+    @Test
+    void transfer_succeeds() {
+        var dataSource = new InputStreamDataSource(dataSourceName, new ByteArrayInputStream(dataSourceContent.getBytes()));
+
+        assertThat(fakeSink.transfer(dataSource)).succeedsWithin(500, TimeUnit.MILLISECONDS)
+                .satisfies(transferResult -> assertThat(transferResult.succeeded()).isTrue());
+
+        assertThat(fakeSink.parts).containsExactly(dataSource);
+    }
+
+    @Test
+    void transfer_whenExceptionOpeningPartStream_fails() {
+        var dataSourceMock = mock(DataSource.class);
+
+        when(dataSourceMock.openPartStream()).thenThrow(new RuntimeException(errorMessage));
+
+        assertThat(fakeSink.transfer(dataSourceMock)).succeedsWithin(500, TimeUnit.MILLISECONDS)
+            .satisfies(transferResult -> assertThat(transferResult.failed()).isTrue())
+            .satisfies(transferResult -> assertThat(transferResult.getFailureMessages()).containsExactly("Error processing data transfer request"));
+    }
+
+    @Test
+    void transfer_whenFailureDuringTransfer_fails() {
+        fakeSink.transferResultSupplier = () -> TransferResult.failure(ResponseStatus.FATAL_ERROR, errorMessage);
+
+        var dataSource = new InputStreamDataSource(dataSourceName, new ByteArrayInputStream(dataSourceContent.getBytes()));
+
+        assertThat(fakeSink.transfer(dataSource)).succeedsWithin(500, TimeUnit.MILLISECONDS)
+            .satisfies(transferResult -> assertThat(transferResult.failed()).isTrue())
+                .satisfies(transferResult -> assertThat(transferResult.getFailure().status()).isEqualTo(ResponseStatus.ERROR_RETRY))
+            .satisfies(transferResult -> assertThat(transferResult.getFailureMessages()).containsExactly(errorMessage));
+
+        assertThat(fakeSink.parts).containsExactly(dataSource);
+    }
+
+    @Test
+    void transfer_whenExceptionDuringTransfer_fails() {
+        fakeSink.transferResultSupplier = () -> {
+            throw new RuntimeException(errorMessage);
+        };
+        var dataSource = new InputStreamDataSource(dataSourceName, new ByteArrayInputStream(dataSourceContent.getBytes()));
+
+        assertThat(fakeSink.transfer(dataSource)).succeedsWithin(500, TimeUnit.MILLISECONDS)
+                .satisfies(transferResult -> assertThat(transferResult.failed()).isTrue())
+                .satisfies(transferResult -> assertThat(transferResult.getFailure().status()).isEqualTo(ResponseStatus.ERROR_RETRY))
+                .satisfies(transferResult -> assertThat(transferResult.getFailureMessages())
+                        .containsExactly("Unhandled exception raised when transferring data: java.lang.RuntimeException: " + errorMessage));
+
+        assertThat(fakeSink.parts).containsExactly(dataSource);
+    }
+
+    private static class FakeParallelSink extends ParallelSink {
+
+        List<DataSource.Part> parts;
+        Supplier<TransferResult> transferResultSupplier = TransferResult::success;
+
+        @Override
+        protected TransferResult transferParts(List<DataSource.Part> parts) {
+            this.parts = parts;
+            return transferResultSupplier.get();
+        }
+    }
+}
