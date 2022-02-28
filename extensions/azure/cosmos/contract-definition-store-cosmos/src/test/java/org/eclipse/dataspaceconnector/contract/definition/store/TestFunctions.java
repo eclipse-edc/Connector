38b76f6a31aa3238e16dad507367fc20b08b146a
Alexandre Gattiker <algattik@users.noreply.github.com>
2022-02-28 13:07:24 +0100

    CI: Use Cosmos DB Emulator for integration tests

--- extensions/azure/cosmos/contract-definition-store-cosmos/src/test/java/org/eclipse/dataspaceconnector/contract/definition/store/TestFunctions.java
+++ extensions/azure/cosmos/contract-definition-store-cosmos/src/test/java/org/eclipse/dataspaceconnector/contract/definition/store/TestFunctions.java
@@ -9,11 +9,13 @@ import java.util.UUID;
 
 public class TestFunctions {
 
+    public static final String PARTITION_KEY = "test-ap-id1";
+
     public static ContractDefinition generateDefinition() {
         return ContractDefinition.Builder.newInstance()
                 .id(UUID.randomUUID().toString())
                 .contractPolicy(Policy.Builder.newInstance().id("test-cp-id1").build())
-                .accessPolicy(Policy.Builder.newInstance().id("test-ap-id1").build())
+                .accessPolicy(Policy.Builder.newInstance().id(PARTITION_KEY).build())
                 .selectorExpression(AssetSelectorExpression.Builder.newInstance().whenEquals("somekey", "someval").build())
                 .build();
     }
