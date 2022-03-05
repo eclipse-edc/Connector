44abbbb551f7b9ffa0c090311b9443fefaaabb13
bscholtes1A <88310985+bscholtes1A@users.noreply.github.com>
2022-03-05 12:39:59 +0100

    Improve unit test for HttpDataSource and HttpDataSourceFactory

--- extensions/data-plane/data-plane-http/src/test/java/org/eclipse/dataspaceconnector/dataplane/http/pipeline/HttpDataSourceTest.java
+++ extensions/data-plane/data-plane-http/src/test/java/org/eclipse/dataspaceconnector/dataplane/http/pipeline/HttpDataSourceTest.java
@@ -4,16 +4,14 @@ import net.jodah.failsafe.RetryPolicy;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import okio.Okio;
 import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.io.ByteArrayOutputStream;
@@ -23,11 +21,9 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static okhttp3.Protocol.HTTP_1_1;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.eclipse.dataspaceconnector.common.testfixtures.TestUtils.testOkHttpClient;
 import static org.eclipse.dataspaceconnector.dataplane.http.pipeline.HttpDataSourceTest.CustomInterceptor.JSON_RESPONSE;
 import static org.mockito.Mockito.mock;
@@ -36,122 +32,84 @@ class HttpDataSourceTest {
 
     private static final String TEST_ENDPOINT = "http://example.com";
 
-    @ParameterizedTest(name = "{index} {0}")
-    @MethodSource("providesInvalidSourceBuilders")
-    void verifyBuilderThrowsIfInvalidInput(String name, HttpDataSource.Builder builder) {
-        assertThatExceptionOfType(NullPointerException.class).isThrownBy(builder::build);
+    private CustomInterceptor interceptor;
+
+    @BeforeEach
+    public void setUp() {
+        interceptor = new CustomInterceptor();
     }
 
     @ParameterizedTest
     @NullAndEmptySource
     void verifyRequest_nameBlankOrNull(String name) {
-        var interceptor = new CustomInterceptor();
-        var source = defaultBuilder(interceptor).method("GET").name(name).build();
+        var source = defaultBuilder().method("GET").name(name).build();
 
         source.openPartStream();
 
-        var requests = interceptor.getRequests();
-        assertThat(requests)
-                .hasSize(1)
-                .allSatisfy(request -> {
-                    assertThat(request.url()).hasToString(TEST_ENDPOINT + "/");
-                    assertThat(request.method()).isEqualTo("GET");
-                });
+        var request = interceptor.getInterceptedRequest();
+        assertThat(request.url()).hasToString(TEST_ENDPOINT + "/");
+        assertThat(request.method()).isEqualTo("GET");
     }
 
 
     @ParameterizedTest
     @NullAndEmptySource
     void verifyRequest_queryParamsBlankOrNull(String queryParams) {
-        var interceptor = new CustomInterceptor();
-        var source = defaultBuilder(interceptor).method("GET").name("testfile").queryParams(queryParams).build();
+        var source = defaultBuilder().method("GET").name("testfile").queryParams(queryParams).build();
 
         source.openPartStream();
 
-        var requests = interceptor.getRequests();
-        assertThat(requests)
-                .hasSize(1)
-                .allSatisfy(request -> {
-                    assertThat(request.url()).hasToString(TEST_ENDPOINT + "/testfile");
-                    assertThat(request.method()).isEqualTo("GET");
-                });
+        var request = interceptor.getInterceptedRequest();
+        assertThat(request.url()).hasToString(TEST_ENDPOINT + "/testfile");
+        assertThat(request.method()).isEqualTo("GET");
     }
 
     @Test
     void verifyRequest_withNameAndQueryParams() {
-        var interceptor = new CustomInterceptor();
-        var source = defaultBuilder(interceptor).method("GET").name("testfile").queryParams("foo=bar&hello=world").build();
+        var source = defaultBuilder().method("GET").name("testfile").queryParams("foo=bar&hello=world").build();
 
         source.openPartStream();
 
-        var requests = interceptor.getRequests();
-        assertThat(requests).hasSize(1);
-        assertThat(requests.get(0)).satisfies(request -> {
-            assertThat(request.url()).hasToString(TEST_ENDPOINT + "/testfile?foo=bar&hello=world");
-            assertThat(request.method()).isEqualTo("GET");
-        });
+        var request = interceptor.getInterceptedRequest();
+        assertThat(request.url()).hasToString(TEST_ENDPOINT + "/testfile?foo=bar&hello=world");
+        assertThat(request.method()).isEqualTo("GET");
     }
 
     @Test
     void verifyRequest_body() {
-        var interceptor = new CustomInterceptor();
         var json = "{ \"foo\" : \"bar\" }";
-        var requestBody = RequestBody.create(json, MediaType.get("application/json"));
-        var source = defaultBuilder(interceptor).method("POST").requestBody(requestBody).build();
+        var source = defaultBuilder().method("POST").requestBody(MediaType.get("application/json"), json).build();
 
         source.openPartStream();
 
-        var requests = interceptor.getRequests();
-        assertThat(requests).hasSize(1);
-        assertThat(requests.get(0)).satisfies(request -> {
-            assertThat(request.url()).hasToString(TEST_ENDPOINT + "/");
-            assertThat(request.method()).isEqualTo("POST");
-            try {
-                // check request body
-                var sink = Okio.sink(new ByteArrayOutputStream());
-                var bufferedSink = Okio.buffer(sink);
-                Objects.requireNonNull(request.body()).writeTo(bufferedSink);
-                assertThat(bufferedSink.getBuffer().readUtf8()).isEqualTo(json);
-            } catch (IOException e) {
-                throw new AssertionError("Failed to write body");
-            }
-        });
+        var request = interceptor.getInterceptedRequest();
+        assertThat(request.url()).hasToString(TEST_ENDPOINT + "/");
+        assertThat(request.method()).isEqualTo("POST");
+        assertThat(extractRequestBody(request)).isEqualTo(json);
     }
 
     @Test
     void verifyOpenPartStream() {
-        var source = defaultBuilder(new CustomInterceptor()).method("GET").build();
+        var source = defaultBuilder().method("GET").build();
 
         var parts = source.openPartStream().collect(Collectors.toList());
 
-        assertThat(parts).hasSize(1)
-                .allSatisfy(part -> {
-                    try {
-                        assertThat(part.size()).isEqualTo(JSON_RESPONSE.length());
-                        assertThat(part.openStream().readAllBytes()).isEqualTo(JSON_RESPONSE.getBytes());
-                    } catch (IOException e) {
-                        throw new AssertionError("Failed to open Part stream");
-                    }
-                });
+        assertThat(parts).hasSize(1);
+        assertThat(parts.get(0).openStream()).hasContent(JSON_RESPONSE);
     }
 
-    /**
-     * Serves some invalid {@link HttpDataSource.Builder} which are expected to throw exception when calling build() method.
-     */
-    private static Stream<Arguments> providesInvalidSourceBuilders() {
-        var nullHeaderKey = defaultBuilder().header(null, "bar");
-        var nullHeaderValue = defaultBuilder().header("foo", null);
-        return Stream.of(
-                Arguments.of("NULL HEADER KEY", nullHeaderKey),
-                Arguments.of("NULL HEADER VALUE", nullHeaderValue)
-        );
+    private static String extractRequestBody(Request request) {
+        try {
+            var sink = Okio.sink(new ByteArrayOutputStream());
+            var bufferedSink = Okio.buffer(sink);
+            Objects.requireNonNull(request.body()).writeTo(bufferedSink);
+            return bufferedSink.getBuffer().readUtf8();
+        } catch (IOException e) {
+            throw new AssertionError(e);
+        }
     }
 
-    private static HttpDataSource.Builder defaultBuilder() {
-        return defaultBuilder(new CustomInterceptor());
-    }
-
-    private static HttpDataSource.Builder defaultBuilder(Interceptor interceptor) {
+    private HttpDataSource.Builder defaultBuilder() {
         var monitor = mock(Monitor.class);
         var retryPolicy = new RetryPolicy<>().withMaxAttempts(1);
         var httpClient = testOkHttpClient().newBuilder().addInterceptor(interceptor).build();
@@ -180,8 +138,10 @@ class HttpDataSourceTest {
                     .build();
         }
 
-        public List<Request> getRequests() {
-            return requests;
+        public Request getInterceptedRequest() {
+            return requests.stream()
+                    .findFirst()
+                    .orElseThrow(() -> new AssertionError("No request intercepted"));
         }
     }
 }
