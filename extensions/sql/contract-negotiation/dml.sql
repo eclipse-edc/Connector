0d84279434a5f01fda7b828195952913667d73de
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-03-23 07:05:59 +0100

    Update dml.sql

--- extensions/sql/contract-negotiation/dml.sql
+++ extensions/sql/contract-negotiation/dml.sql
@@ -73,22 +73,6 @@ SET state=200,
     trace_context='{}'
 WHERE id = 'test-cn1';
 
-
-/*
- *  Copyright (c) 2020 - 2022 Microsoft Corporation
- *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
- *
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Contributors:
- *       Microsoft Corporation - initial API and implementation
- *
- */
-
-
 -- select next for state
 SELECT *
 FROM contract_negotiation
