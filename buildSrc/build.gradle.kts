a172046af185a2dac32d350f4ed10b8e6ed33003
Marc Gomez <marcgomez@microsoft.com>
2022-03-24 10:21:21 +0100

    Initial test setup

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `kotlin-dsl`
+    `java-library`
 }
 
 repositories {
@@ -8,6 +9,12 @@ repositories {
 
 dependencies {
     api(gradleApi())
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.mockito:mockito-core:4.2.0")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("com.github.javafaker:javafaker:1.0.2")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
---
+++ buildSrc/src/test/java/org/eclipse/dataspaceconnector/gradle/DependencyRulesTaskTest.java
@@ -0,0 +1,16 @@
+package org.eclipse.dataspaceconnector.gradle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DependencyRulesTaskTest {
+
+    @Test
+    void test() {
+        // TBD
+    }
+}
