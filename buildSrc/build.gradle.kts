21c59feb2f29eaaeba89b067001a4b9160484232
Alexandre Gattiker <algattik@microsoft.com>
2022-03-24 10:45:16 +0100

    junit

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,3 +15,13 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         jvmTarget = "11"
     }
 }
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ buildSrc/src/test/java/MyTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+class MyTest {
+    @Test
+    void annoyEveryone() {
+        throw new RuntimeException();
+    }
+}
