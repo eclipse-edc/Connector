390d786f706ae6f8dc4da5eac790a452f4ba47f1
Diego Gomez <diego.gomez.external@zf.com>
2022-03-25 11:38:17 +0100

    Create PolicyService interface

---
+++ extensions/api/data-management/policydefinition/src/main/java/org/eclipse/dataspaceconnector/api/datamanagement/policy/service/PolicyService.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022 ZF Friedrichshafen AG
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Contributors:
+ *    ZF Friedrichshafen AG - Initial API and Implementation
+ */
+
+package org.eclipse.dataspaceconnector.api.datamanagement.policy.service;
+
+import org.eclipse.dataspaceconnector.policy.model.Policy;
+import org.eclipse.dataspaceconnector.spi.query.QuerySpec;
+
+import java.util.Collection;
+
+public interface PolicyService {
+
+    /**
+     * Returns a policy by its id
+     *
+     * @param policyId id of the policy
+     * @return the policy, null if it's not found
+     */
+    Policy findbyId(String policyId);
+
+    /**
+     * Query policies
+     *
+     * @param query request
+     * @return the collection of policies that match the query
+     */
+    Collection<Policy> query(QuerySpec query);
+
+    /**
+     * Delete a policy
+     *
+     * @param policyId the id of the policy to be deleted
+     * @return Policy deleted if the policy is deleted correctly, failure otherwise
+     */
+    Policy delete(String policyId);
+
+    /**
+     * Create an policy
+     *
+     * @param policy the policy
+     */
+    void create(Policy policy);
+}
