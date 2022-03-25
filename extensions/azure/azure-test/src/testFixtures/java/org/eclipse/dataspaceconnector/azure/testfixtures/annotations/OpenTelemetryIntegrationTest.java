e8e54e132932890240b995b944caf5cce651e3b9
Ophelie Le Mentec <olementec@microsoft.com>
2022-03-25 13:05:22 +0100

    Added back annotation.

--- extensions/azure/azure-test/src/testFixtures/java/org/eclipse/dataspaceconnector/azure/testfixtures/annotations/OpenTelemetryIntegrationTest.java
+++ extensions/azure/azure-test/src/testFixtures/java/org/eclipse/dataspaceconnector/azure/testfixtures/annotations/OpenTelemetryIntegrationTest.java
@@ -28,5 +28,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Tag("OpenTelemetryIntegrationTest")
+@IntegrationTest
 public @interface OpenTelemetryIntegrationTest {
 }
