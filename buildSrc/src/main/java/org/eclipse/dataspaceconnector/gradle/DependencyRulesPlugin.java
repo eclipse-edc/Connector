ac5f31321eeeeddef0120fadc35f841afd9e13f6
Marc Gomez <marcgomez@microsoft.com>
2022-03-25 11:00:59 +0100

    Add copyright headers

--- buildSrc/src/main/java/org/eclipse/dataspaceconnector/gradle/DependencyRulesPlugin.java
+++ buildSrc/src/main/java/org/eclipse/dataspaceconnector/gradle/DependencyRulesPlugin.java
@@ -3,6 +3,19 @@ package org.eclipse.dataspaceconnector.gradle;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
 public class DependencyRulesPlugin implements Plugin<Project> {
 
     @Override
--- buildSrc/src/main/java/org/eclipse/dataspaceconnector/gradle/DependencyRulesTask.java
+++ buildSrc/src/main/java/org/eclipse/dataspaceconnector/gradle/DependencyRulesTask.java
@@ -12,6 +12,19 @@ import java.util.regex.Pattern;
 
 import static java.lang.String.format;
 
+/*
+ *  Copyright (c) 2022 Microsoft Corporation
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Microsoft Corporation - initial API and implementation
+ *
+ */
 public abstract class DependencyRulesTask extends DefaultTask {
 
     private static final Pattern CROSS_MODULE_DEPENDENCY_PATTERN = Pattern.compile("\\.\\./[^.].*");
