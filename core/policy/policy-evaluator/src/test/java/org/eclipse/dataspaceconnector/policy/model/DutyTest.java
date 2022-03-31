a2723a3d0a5c315161d75703829ae6e4333c217b
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2022-03-31 09:26:28 +0200

    policy-model: add tests for copying with target

--- core/policy/policy-evaluator/src/test/java/org/eclipse/dataspaceconnector/policy/model/DutyTest.java
+++ core/policy/policy-evaluator/src/test/java/org/eclipse/dataspaceconnector/policy/model/DutyTest.java
@@ -9,6 +9,7 @@
  *
  *  Contributors:
  *       Microsoft Corporation - initial API and implementation
+ *       Fraunhofer Institute for Software and Systems Engineering - added test
  *
  */
 package org.eclipse.dataspaceconnector.policy.model;
@@ -26,5 +27,32 @@ class DutyTest {
         var mapper = new ObjectMapper();
         var serialized = mapper.writeValueAsString(Duty.Builder.newInstance().action(Action.Builder.newInstance().type("USE").build()).build());
         assertThat(mapper.readValue(serialized, Duty.class).getAction()).isNotNull();
+    }
+    
+    @Test
+    void copyWithTarget() {
+        var target = "target-id";
+        var duty = Duty.Builder.newInstance()
+                .uid("id")
+                .assigner("assigner")
+                .assignee("assignee")
+                .action(Action.Builder.newInstance().type("DELETE").build())
+                .constraint(AtomicConstraint.Builder.newInstance()
+                        .leftExpression(new LiteralExpression("left"))
+                        .operator(Operator.EQ)
+                        .rightExpression(new LiteralExpression("right"))
+                        .build())
+                .consequence(Duty.Builder.newInstance().build())
+                .build();
+        
+        var copy = duty.copy(target);
+    
+        assertThat(copy.getUid()).isEqualTo(duty.getUid());
+        assertThat(copy.getAssigner()).isEqualTo(duty.getAssigner());
+        assertThat(copy.getAssignee()).isEqualTo(duty.getAssignee());
+        assertThat(copy.getAction()).isEqualTo(duty.getAction());
+        assertThat(copy.getConstraints()).isEqualTo(duty.getConstraints());
+        assertThat(copy.getConsequence().getTarget()).isEqualTo(target);
+        assertThat(copy.getTarget()).isEqualTo(target);
     }
 }
