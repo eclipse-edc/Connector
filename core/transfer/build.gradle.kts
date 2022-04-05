5591d436c6592c5f8808463f1734bfb487f35ffc
Marc Gomez <marcgomez@microsoft.com>
2022-04-05 22:05:12 +0200

    Dependency checks (#1000)
    Co-authored-by: Alexandre Gattiker <algattik@microsoft.com>
    
    Co-authored-by: Alexandre Gattiker <algattik@microsoft.com>
    Co-authored-by: Jim Marino <jim.marino@gmail.com>

--- core/transfer/build.gradle.kts
+++ core/transfer/build.gradle.kts
@@ -20,9 +20,9 @@ plugins {
 
 dependencies {
     api(project(":spi:transfer-spi"))
-    api(project(":common:state-machine-lib"))
-    api(project(":common:util"))
 
+    implementation(project(":common:state-machine-lib"))
+    implementation(project(":common:util"))
     implementation("io.opentelemetry:opentelemetry-extension-annotations:${openTelemetryVersion}")
 
     testImplementation(project(":extensions:in-memory:transfer-store-memory"))
