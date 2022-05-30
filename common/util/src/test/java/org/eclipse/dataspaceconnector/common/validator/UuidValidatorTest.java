d59ed1d7d974ee274b187a2d0ddd4768484033e4
Tuncay Tunc (ZF Friedrichshafen AG) <100704677+tuncaytunc-zf@users.noreply.github.com>
2022-05-30 16:06:40 +0200

    Fix Checkstyle findings.

--- common/util/src/test/java/org/eclipse/dataspaceconnector/common/validator/UuidValidatorTest.java
+++ common/util/src/test/java/org/eclipse/dataspaceconnector/common/validator/UuidValidatorTest.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2022 ZF Friedrichshafen AG
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       ZF Friedrichshafen AG - Initial Implementation
+ *
+ */
+
 package org.eclipse.dataspaceconnector.common.validator;
 
 import org.junit.jupiter.api.Test;
@@ -11,13 +25,13 @@ public class UuidValidatorTest {
     UuidValidator uuidValidator = new UuidValidator();
 
     @Test
-    void isValid_valid_UUID() {
+    void isValid_valid_uuid() {
         String uuid = UUID.randomUUID().toString();
         assertThat(uuidValidator.isValid(uuid, null)).isTrue();
     }
 
     @Test
-    void isValid_invalid_UUID() {
+    void isValid_invalid_uuid() {
         String uuid = "invalid-uuid-string";
         assertThat(uuidValidator.isValid(uuid, null)).isFalse();
     }
