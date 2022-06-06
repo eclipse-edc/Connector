e232f525dfcd05184b432e1d59c45f221695fc55
Andrei-Laurentiu Coman <andrei.coman@siemens.com>
2022-06-06 12:02:46 +0300

    fix start-up issues with connectors

--- samples/other/file-transfer-http-to-http/provider/build.gradle.kts
+++ samples/other/file-transfer-http-to-http/provider/build.gradle.kts
@@ -20,7 +20,9 @@ dependencies {
     implementation(project(":extensions:api:auth-tokenbased"))
     implementation(project(":extensions:api:data-management"))
 
-    implementation(project(":data-protocols:ids"))
+    implementation(project(":data-protocols:ids")) {
+        exclude("org.eclipse.dataspaceconnector","ids-token-validation")
+    }
 
     implementation(project(":samples:other:file-transfer-http-to-http:transfer-file"))
 
