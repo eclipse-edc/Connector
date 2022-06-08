57f1b12d0de358b50ff32ce1f567f532f61ae80a
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-06-08 17:44:20 +0200

    removed old and unused registration service (#1440)

--- extensions/iam/decentralized-identity/build.gradle.kts
+++ extensions/iam/decentralized-identity/build.gradle.kts
@@ -22,8 +22,6 @@ dependencies {
     api(project(":extensions:iam:decentralized-identity:identity-did-spi"))
     api(project(":extensions:iam:decentralized-identity:identity-did-core"))
     api(project(":extensions:iam:decentralized-identity:identity-did-service"))
-//    api(project(":extensions:iam:decentralized-identity:registration-service"))
-//    api(project(":extensions:iam:decentralized-identity:registration-service-api"))
     api(project(":extensions:iam:decentralized-identity:identity-common-test"))
 }
 
