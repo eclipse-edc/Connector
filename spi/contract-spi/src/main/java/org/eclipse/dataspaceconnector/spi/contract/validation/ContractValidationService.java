47f67f30dd4e6c1860f076c93893299bc93f57ca
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2022-06-20 16:02:39 +0200

    feat: add policy scope and evaluation to ContractValidationService

--- spi/contract-spi/src/main/java/org/eclipse/dataspaceconnector/spi/contract/validation/ContractValidationService.java
+++ spi/contract-spi/src/main/java/org/eclipse/dataspaceconnector/spi/contract/validation/ContractValidationService.java
@@ -9,12 +9,14 @@
  *
  *  Contributors:
  *       Microsoft Corporation - initial API and implementation
+ *       Fraunhofer Institute for Software and Systems Engineering - add policy scope
  *
  */
 
 package org.eclipse.dataspaceconnector.spi.contract.validation;
 
 import org.eclipse.dataspaceconnector.spi.iam.ClaimToken;
+import org.eclipse.dataspaceconnector.spi.policy.PolicyScope;
 import org.eclipse.dataspaceconnector.spi.result.Result;
 import org.eclipse.dataspaceconnector.spi.types.domain.contract.agreement.ContractAgreement;
 import org.eclipse.dataspaceconnector.spi.types.domain.contract.offer.ContractOffer;
@@ -24,6 +26,9 @@ import org.jetbrains.annotations.NotNull;
  * Validates {@link ContractOffer}s and {@link ContractAgreement}s.
  */
 public interface ContractValidationService {
+    
+    @PolicyScope
+    String NEGOTIATION_SCOPE = "contract.negotiation";
 
     /**
      * Validates and sanitizes the contract offer for the consumer represented by the given claims.
