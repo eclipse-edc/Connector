010c06dc16a5e52492d800b8b98cc758d39598c9
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-06-22 10:53:59 +0200

    docs: improve Swagger documentation for APIs (#1448)
    * fixed description
    
    * added docs to asset api
    
    * documented contract agreement api
    
    * documented contract definition and negotiation
    
    * documented policy and transfer process
    
    * documented observability API
    
    * updated yamls

--- extensions/api/data-management/contractagreement/src/main/java/org/eclipse/dataspaceconnector/api/datamanagement/contractagreement/ContractAgreementApi.java
+++ extensions/api/data-management/contractagreement/src/main/java/org/eclipse/dataspaceconnector/api/datamanagement/contractagreement/ContractAgreementApi.java
@@ -15,6 +15,11 @@
 package org.eclipse.dataspaceconnector.api.datamanagement.contractagreement;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.eclipse.dataspaceconnector.api.datamanagement.contractagreement.model.ContractAgreementDto;
@@ -26,8 +31,21 @@ import java.util.List;
 @Tag(name = "Contract Agreement")
 public interface ContractAgreementApi {
 
+    @Operation(description = "Gets all contract agreements according to a particular query",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = ContractAgreementDto.class)))),
+                    @ApiResponse(responseCode = "400", description = "Request body was malformed")
+            }
+    )
     List<ContractAgreementDto> getAllAgreements(@Valid QuerySpecDto querySpecDto);
 
+    @Operation(description = "Gets an contract agreement with the given ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "The contract agreement", content = @Content(schema = @Schema(implementation = ContractAgreementDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Request was malformed, e.g. id was null"),
+                    @ApiResponse(responseCode = "404", description = "An contract agreement with the given ID does not exist")
+            }
+    )
     ContractAgreementDto getContractAgreement(String id);
 
 }
--- extensions/api/observability/src/main/java/org/eclipse/dataspaceconnector/api/observability/ObservabilityApi.java
+++ extensions/api/observability/src/main/java/org/eclipse/dataspaceconnector/api/observability/ObservabilityApi.java
@@ -15,19 +15,41 @@
 package org.eclipse.dataspaceconnector.api.observability;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.dataspaceconnector.spi.system.health.HealthStatus;
 
 @OpenAPIDefinition
 @Tag(name = "Application Observability")
 public interface ObservabilityApi {
 
+    @Operation(description = "Performs a liveness probe to determine whether the runtime is working properly.",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = HealthStatus.class)))) }
+    )
     Response checkHealth();
 
+    @Operation(description = "Performs a liveness probe to determine whether the runtime is working properly.",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = HealthStatus.class)))) }
+    )
     Response getLiveness();
 
+    @Operation(description = "Performs a readiness probe to determine whether the runtime is able to accept requests.",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = HealthStatus.class)))) }
+    )
     Response getReadiness();
 
+    @Operation(description = "Performs a startup probe to determine whether the runtime has completed startup.",
+            responses = {
+                    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = HealthStatus.class)))) }
+    )
     Response getStartup();
 
 }
