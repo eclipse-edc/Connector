ea5f1e703a08b85bd8e7ef24c77c1505aed8766e
Lucian Torje <lucian.torje@siemens.com>
2022-06-23 08:41:26 +0300

    fix missing header

--- spi/core-spi/src/test/java/org/eclipse/dataspaceconnector/spi/types/domain/HttpDataAddressTest.java
+++ spi/core-spi/src/test/java/org/eclipse/dataspaceconnector/spi/types/domain/HttpDataAddressTest.java
@@ -1,3 +1,17 @@
+/*
+ *  Copyright (c) 2022 Siemens AG
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Siemens AG - initial implementation
+ *
+ */
+
 package org.eclipse.dataspaceconnector.spi.types.domain;
 
 import org.junit.jupiter.api.Test;
