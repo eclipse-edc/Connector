f5bde54a2c3e6f9464659bb5b1fc5522e0c72dca
Julia Pampus <72392527+juliapampus@users.noreply.github.com>
2022-06-24 15:53:05 +0200

    build(deps): bump junit-pioneer from 1.6.2 to 1.7.1

--- common/util/build.gradle.kts
+++ common/util/build.gradle.kts
@@ -27,7 +27,7 @@ dependencies {
     testFixturesImplementation("org.junit.jupiter:junit-jupiter-api:${jupiterVersion}")
     testFixturesRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${jupiterVersion}")
     testFixturesImplementation("com.squareup.okhttp3:okhttp:${okHttpVersion}")
-    testImplementation("org.junit-pioneer:junit-pioneer:1.6.2")
+    testImplementation("org.junit-pioneer:junit-pioneer:1.7.1")
 }
 
 publishing {
