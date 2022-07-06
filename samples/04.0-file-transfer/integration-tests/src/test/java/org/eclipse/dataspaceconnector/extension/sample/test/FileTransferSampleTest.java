c81eb1f60d17370741e01b40c15c6be64b449169
Peter Kirchner <peter.kirchner@microsoft.com>
2022-07-06 18:05:43 +0200

    samples: use static import

--- samples/04.0-file-transfer/integration-tests/src/test/java/org/eclipse/dataspaceconnector/extension/sample/test/FileTransferSampleTest.java
+++ samples/04.0-file-transfer/integration-tests/src/test/java/org/eclipse/dataspaceconnector/extension/sample/test/FileTransferSampleTest.java
@@ -22,6 +22,8 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.util.Map;
 
+import static org.eclipse.dataspaceconnector.extension.sample.test.FileTransferSampleTestUtils.getFileFromRelativePath;
+
 @EndToEndTest
 public class FileTransferSampleTest {
 
@@ -34,8 +36,8 @@ public class FileTransferSampleTest {
             "provider",
             Map.of(
                     // Override 'edc.samples.04.asset.path' implicitly set via property 'edc.fs.config'.
-                    "edc.samples.04.asset.path", FileTransferSampleTestUtils.getFileFromRelativePath(SAMPLE_ASSET_FILE_PATH).getAbsolutePath(),
-                    "edc.fs.config", FileTransferSampleTestUtils.getFileFromRelativePath(PROVIDER_CONFIG_PROPERTIES_FILE_PATH).getAbsolutePath()
+                    "edc.samples.04.asset.path", getFileFromRelativePath(SAMPLE_ASSET_FILE_PATH).getAbsolutePath(),
+                    "edc.fs.config", getFileFromRelativePath(PROVIDER_CONFIG_PROPERTIES_FILE_PATH).getAbsolutePath()
             )
     );
     @RegisterExtension
@@ -43,7 +45,7 @@ public class FileTransferSampleTest {
             ":samples:04.0-file-transfer:consumer",
             "consumer",
             Map.of(
-                    "edc.fs.config", FileTransferSampleTestUtils.getFileFromRelativePath(CONSUMER_CONFIG_PROPERTIES_FILE_PATH).getAbsolutePath()
+                    "edc.fs.config", getFileFromRelativePath(CONSUMER_CONFIG_PROPERTIES_FILE_PATH).getAbsolutePath()
             )
     );
 
