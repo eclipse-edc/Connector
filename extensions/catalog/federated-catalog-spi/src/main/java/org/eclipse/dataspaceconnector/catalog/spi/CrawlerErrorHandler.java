da69370ef30c8e7861b1d986428a0f646f05a8d2
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2022-08-03 21:27:16 +0200

    fix: javadoc (#1777)

--- extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/CrawlerErrorHandler.java
+++ extensions/catalog/federated-catalog-spi/src/main/java/org/eclipse/dataspaceconnector/catalog/spi/CrawlerErrorHandler.java
@@ -18,7 +18,7 @@ package org.eclipse.dataspaceconnector.catalog.spi;
 import java.util.function.Consumer;
 
 /**
- * Whenever a {@link Crawler} encounters an error, e.g. when a node is not reachable, a node does not respond within a given time frame,
+ * Whenever a crawler encounters an error, e.g. when a node is not reachable, a node does not respond within a given time frame,
  * or the response is invalid, it calls out to the {@code CrawlerErrorHandler} to signal that error.
  */
 @FunctionalInterface
