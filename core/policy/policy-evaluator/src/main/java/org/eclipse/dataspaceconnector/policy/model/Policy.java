7b36ed4aacda81a65f72c2824af467abc25dd839
Benjamin Scholtes <88310985+bscholtes1A@users.noreply.github.com>
2022-08-09 10:15:08 +0200

    fix: Fix broken link in Policy javadoc (#1799)
    Fix broken link in Policy javadoc

--- core/policy/policy-evaluator/src/main/java/org/eclipse/dataspaceconnector/policy/model/Policy.java
+++ core/policy/policy-evaluator/src/main/java/org/eclipse/dataspaceconnector/policy/model/Policy.java
@@ -29,8 +29,7 @@ import java.util.stream.Collectors;
 /**
  * A collection of permissions, prohibitions, and obligations. Subtypes are defined by
  * {@link PolicyType}.
- * This is a value object. In order to have it identifiable and individually addressable, consider the use of
- * {@link PolicyDefinition}.
+ * This is a value object. In order to have it identifiable and individually addressable, consider the use of PolicyDefinition.
  */
 @JsonDeserialize(builder = Policy.Builder.class)
 public class Policy {
