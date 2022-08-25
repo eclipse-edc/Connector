1d8c418770171065f047f312e7e4c09408266db7
maciejkizlich-zf <102966398+maciejkizlich-zf@users.noreply.github.com>
2022-08-25 16:26:27 +0200

    feat: enable s3 multipart upload (#1876)
    * feat: enable multipart upload via S3 Extension
    
    * feat: enable multipart upload via S3 extension - code review fixes - code style

--- extensions/data-plane/data-plane-s3/src/main/java/org/eclipse/dataspaceconnector/aws/dataplane/s3/S3DataSink.java
+++ extensions/data-plane/data-plane-s3/src/main/java/org/eclipse/dataspaceconnector/aws/dataplane/s3/S3DataSink.java
@@ -20,9 +20,15 @@ import org.eclipse.dataspaceconnector.spi.response.ResponseStatus;
 import org.eclipse.dataspaceconnector.spi.response.StatusResult;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.CompleteMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.CompletedMultipartUpload;
+import software.amazon.awssdk.services.s3.model.CompletedPart;
+import software.amazon.awssdk.services.s3.model.CreateMultipartUploadRequest;
+import software.amazon.awssdk.services.s3.model.UploadPartRequest;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.List;
 
 class S3DataSink extends ParallelSink {
@@ -30,18 +36,49 @@ class S3DataSink extends ParallelSink {
     private S3Client client;
     private String bucketName;
     private String keyName;
+    private int chunkSize;
 
-    private S3DataSink() { }
+    private S3DataSink() {}
 
     @Override
     protected StatusResult<Void> transferParts(List<DataSource.Part> parts) {
-        for (DataSource.Part part : parts) {
+        for (var part : parts) {
             try (var input = part.openStream()) {
-                PutObjectRequest request = PutObjectRequest.builder()
+
+                var partNumber = 1;
+                List<CompletedPart> completedParts = new ArrayList<>();
+
+                var uploadId = client.createMultipartUpload(CreateMultipartUploadRequest.builder()
                         .bucket(bucketName)
                         .key(keyName)
-                        .build();
-                client.putObject(request, RequestBody.fromBytes(input.readAllBytes()));
+                        .build()).uploadId();
+
+                while (true) {
+                    var bytesChunk = input.readNBytes(chunkSize);
+
+                    if (bytesChunk.length < 1) {
+                        break;
+                    }
+
+                    completedParts.add(CompletedPart.builder().partNumber(partNumber)
+                            .eTag(client.uploadPart(UploadPartRequest.builder()
+                                .bucket(bucketName)
+                                .key(keyName)
+                                .uploadId(uploadId)
+                                .partNumber(partNumber)
+                                .build(), RequestBody.fromByteBuffer(ByteBuffer.wrap(bytesChunk))).eTag()).build());
+                    partNumber++;
+                }
+
+                client.completeMultipartUpload(CompleteMultipartUploadRequest.builder()
+                        .bucket(bucketName)
+                        .key(keyName)
+                        .uploadId(uploadId)
+                        .multipartUpload(CompletedMultipartUpload.builder()
+                            .parts(completedParts)
+                            .build())
+                        .build());
+
             } catch (IOException e) {
                 monitor.severe("Cannot open the input part");
                 return StatusResult.failure(ResponseStatus.FATAL_ERROR, "An error");
@@ -50,6 +87,7 @@ class S3DataSink extends ParallelSink {
                 return StatusResult.failure(ResponseStatus.FATAL_ERROR, "An error");
             }
         }
+
         return StatusResult.success();
     }
 
@@ -78,9 +116,12 @@ class S3DataSink extends ParallelSink {
             return this;
         }
 
-        @Override
-        protected void validate() {
-
+        public Builder chunkSizeBytes(int chunkSize) {
+            sink.chunkSize = chunkSize;
+            return this;
         }
+
+        @Override
+        protected void validate() {}
     }
 }
