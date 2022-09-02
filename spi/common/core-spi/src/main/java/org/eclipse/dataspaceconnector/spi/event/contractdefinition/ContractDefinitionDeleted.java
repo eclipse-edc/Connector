84585585a659af90c74b78990cac0b9d794fcf9e
Jan Peter Meyer <jan.peter.meyer@isst.fraunhofer.de>
2022-09-02 08:58:16 +0200

    doc: correction license header

--- spi/common/core-spi/src/main/java/org/eclipse/dataspaceconnector/spi/event/contractdefinition/ContractDefinitionDeleted.java
+++ spi/common/core-spi/src/main/java/org/eclipse/dataspaceconnector/spi/event/contractdefinition/ContractDefinitionDeleted.java
@@ -9,6 +9,7 @@
  *
  *  Contributors:
  *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *       Fraunhofer Institute for Software and Systems Engineering - expending Event classes
  *
  */
 
