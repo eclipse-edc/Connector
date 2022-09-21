21f9e798ade898e20250883633dc3857bda636aa
ndr_brt <andrea.bertagnolli@gmail.com>
2022-09-21 19:27:22 +0200

    chore: bump swagger version to 2.2.2 (#1996)
    * chore: bump swagger version to 2.2.2
    
    * pr remark

--- build.gradle.kts
+++ build.gradle.kts
@@ -114,7 +114,8 @@ subprojects {
 
 buildscript {
     dependencies {
-        classpath("io.swagger.core.v3:swagger-gradle-plugin:2.1.13")
+        val swagger: String by project
+        classpath("io.swagger.core.v3:swagger-gradle-plugin:${swagger}")
     }
 }
 
