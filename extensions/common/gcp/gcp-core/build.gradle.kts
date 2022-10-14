41e5827c072d229d139be98a895c2aebe78c6f20
masoud allahyari <masoud.allahyari@t-systems.com>
2022-10-14 17:33:07 +0200

    chore(gcs-core): change implementation to api for google-cloud-storage. Ignore case in storage location comparison

--- extensions/common/gcp/gcp-core/build.gradle.kts
+++ extensions/common/gcp/gcp-core/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     api(project(":spi:control-plane:transfer-spi"))
 
     implementation("com.google.cloud:google-iam-admin:${googleCloudIamAdminVersion}")
-    implementation("com.google.cloud:google-cloud-storage:${googleCloudStorageVersion}")
+    api("com.google.cloud:google-cloud-storage:${googleCloudStorageVersion}")
     implementation("com.google.cloud:google-cloud-iamcredentials:${googleCloudIamCredentialsVersion}")
 }
 publishing {
