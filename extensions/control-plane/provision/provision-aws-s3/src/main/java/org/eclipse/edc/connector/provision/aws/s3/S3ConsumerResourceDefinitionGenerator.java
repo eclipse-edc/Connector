48a9eb2e6f7d31c67e36495464e642f4bba49da4
Diego Gomez <diego.gomez.external@zf.com>
2022-11-04 14:56:41 +0100

    Refactor method generate

--- extensions/control-plane/provision/provision-aws-s3/src/main/java/org/eclipse/edc/connector/provision/aws/s3/S3ConsumerResourceDefinitionGenerator.java
+++ extensions/control-plane/provision/provision-aws-s3/src/main/java/org/eclipse/edc/connector/provision/aws/s3/S3ConsumerResourceDefinitionGenerator.java
@@ -19,7 +19,6 @@ import org.eclipse.edc.connector.transfer.spi.provision.ConsumerResourceDefiniti
 import org.eclipse.edc.connector.transfer.spi.types.DataRequest;
 import org.eclipse.edc.connector.transfer.spi.types.ResourceDefinition;
 import org.eclipse.edc.policy.model.Policy;
-import software.amazon.awssdk.regions.Region;
 
 import static java.util.UUID.randomUUID;
 
@@ -30,11 +29,7 @@ public class S3ConsumerResourceDefinitionGenerator implements ConsumerResourceDe
 
     @Override
     public ResourceDefinition generate(DataRequest dataRequest, Policy policy) {
-        if (dataRequest.getDestinationType() != null) {
-            // FIXME generate region from policy engine
 
-            return S3BucketResourceDefinition.Builder.newInstance().id(randomUUID().toString()).bucketName(dataRequest.getProcessId()).regionId(Region.US_EAST_1.id()).build();
-        }
         var destination = dataRequest.getDataDestination();
         var id = randomUUID().toString();
         return S3BucketResourceDefinition.Builder.newInstance().id(id).bucketName(destination.getProperty(S3BucketSchema.BUCKET_NAME)).regionId(destination.getProperty(S3BucketSchema.REGION)).build();
