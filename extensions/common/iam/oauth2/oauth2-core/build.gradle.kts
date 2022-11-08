65f1e196b0c38b354408dd01aa84bb3bf6a1cac8
ndr_brt <andrea.bertagnolli@gmail.com>
2022-11-08 11:38:10 +0100

    feat: add retry mechanism to oauth2 service

--- extensions/common/iam/oauth2/oauth2-core/build.gradle.kts
+++ extensions/common/iam/oauth2/oauth2-core/build.gradle.kts
@@ -16,6 +16,7 @@ plugins {
     `java-library`
 }
 
+val failsafeVersion: String by project
 val httpMockServer: String by project
 val nimbusVersion: String by project
 val okHttpVersion: String by project
@@ -23,6 +24,7 @@ val okHttpVersion: String by project
 dependencies {
     api(project(":spi:common:oauth2-spi"))
     implementation(project(":core:common:jwt-core"))
+    implementation("dev.failsafe:failsafe-okhttp:${failsafeVersion}")
 
     implementation("com.nimbusds:nimbus-jose-jwt:${nimbusVersion}")
     implementation("com.squareup.okhttp3:okhttp:${okHttpVersion}")
