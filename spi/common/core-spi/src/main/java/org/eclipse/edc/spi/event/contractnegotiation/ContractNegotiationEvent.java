45a84396df1b0303c0a437d2b0241339c351579e
ndr_brt <andrea.bertagnolli@gmail.com>
2022-12-22 15:33:19 +0100

    Improve event hierarchy for ContractNegotiation

---
+++ spi/common/core-spi/src/main/java/org/eclipse/edc/spi/event/contractnegotiation/ContractNegotiationEvent.java
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2022 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.spi.event.contractnegotiation;
+
+import org.eclipse.edc.spi.event.Event;
+import org.eclipse.edc.spi.event.EventPayload;
+
+import java.util.Objects;
+
+/**
+ *  Class as organizational between level to catch events of type TransferProcess to catch them together in an Event Subscriber
+ *  Contains data related to assets
+ */
+public abstract class ContractNegotiationEvent<P extends ContractNegotiationEvent.Payload> extends Event<P> {
+
+    public static abstract class Payload extends EventPayload {
+        protected String contractNegotiationId;
+
+        public String getContractNegotiationId() {
+            return contractNegotiationId;
+        }
+    }
+
+    public static class Builder<E extends ContractNegotiationEvent<P>, P extends ContractNegotiationEvent.Payload, B extends Builder<E, P, B>> extends Event.Builder<E, P, B> {
+
+        protected Builder(E event, P payload) {
+            super(event, payload);
+        }
+
+        @SuppressWarnings("unchecked")
+        public B contractNegotiationId(String contractNegotiationId) {
+            event.payload.contractNegotiationId = contractNegotiationId;
+            return (B) this;
+        }
+
+        @Override
+        protected void validate() {
+            Objects.requireNonNull(event.payload.contractNegotiationId);
+        }
+
+    }
+}
--- spi/common/core-spi/src/main/java/org/eclipse/edc/spi/event/contractnegotiation/ContractNegotiationFailed.java
+++ spi/common/core-spi/src/main/java/org/eclipse/edc/spi/event/contractnegotiation/ContractNegotiationFailed.java
@@ -15,19 +15,22 @@
 
 package org.eclipse.edc.spi.event.contractnegotiation;
 
-import org.eclipse.edc.spi.event.Event;
-
-import java.util.Objects;
-
 /**
  * This event is raised when the ContractNegotiation has failed.
  */
-public class ContractNegotiationFailed extends Event<ContractNegotiationFailed.Payload> {
+public class ContractNegotiationFailed extends ContractNegotiationEvent<ContractNegotiationFailed.Payload> {
 
     private ContractNegotiationFailed() {
     }
 
-    public static class Builder extends Event.Builder<ContractNegotiationFailed, Payload, Builder> {
+    /**
+     * This class contains all event specific attributes of a ContractNegotiation Failed Event
+     *
+     */
+    public static class Payload extends ContractNegotiationEvent.Payload {
+    }
+
+    public static class Builder extends ContractNegotiationEvent.Builder<ContractNegotiationFailed, Payload, Builder> {
 
         public static Builder newInstance() {
             return new Builder();
@@ -36,22 +39,6 @@ public class ContractNegotiationFailed extends Event<ContractNegotiationFailed.P
         private Builder() {
             super(new ContractNegotiationFailed(), new Payload());
         }
-
-        public Builder contractNegotiationId(String contractNegotiationId) {
-            event.payload.contractNegotiationId = contractNegotiationId;
-            return this;
-        }
-
-        @Override
-        protected void validate() {
-            Objects.requireNonNull(event.payload.contractNegotiationId);
-        }
     }
 
-    /**
-     * This class contains all event specific attributes of a ContractNegotiation Failed Event
-     *
-     */
-    public static class Payload extends ContractNegotiationEventPayload {
-    }
 }
