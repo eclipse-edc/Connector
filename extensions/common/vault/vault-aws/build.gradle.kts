696ee007027840cdc9bf24feb0052cd65d13c43f
flseidel1 <flseidel@amazon.com>
2023-02-06 17:03:12 +0100

    Refactoring: Replace api dependency to secretsmanager lib with implementation dependency.

--- extensions/common/vault/vault-aws/build.gradle.kts
+++ extensions/common/vault/vault-aws/build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 
 dependencies {
     api(project(":spi:common:core-spi"))
-    api(libs.aws.secretsmanager)
+    implementation(libs.aws.secretsmanager)
     implementation(project(":core:common:util"))
     testImplementation(libs.mockito.inline)
 }
