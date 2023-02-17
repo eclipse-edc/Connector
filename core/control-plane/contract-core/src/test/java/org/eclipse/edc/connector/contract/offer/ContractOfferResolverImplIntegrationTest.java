6f3d24004ccbfbf28c737fb100b6131efd846242
yurimssilva <yuri.silva@ctw.bmwgroup.com>
2023-02-17 13:33:04 +0000

    fix: tests

--- core/control-plane/contract-core/src/test/java/org/eclipse/edc/connector/contract/offer/ContractOfferResolverImplIntegrationTest.java
+++ core/control-plane/contract-core/src/test/java/org/eclipse/edc/connector/contract/offer/ContractOfferResolverImplIntegrationTest.java
@@ -36,6 +36,9 @@ import org.eclipse.edc.spi.types.domain.asset.Asset;
 import org.eclipse.edc.spi.types.domain.asset.AssetEntry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -44,6 +47,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 import static java.time.ZoneOffset.UTC;
 import static java.util.Collections.emptyMap;
 import static java.util.stream.IntStream.range;
@@ -105,6 +110,42 @@ class ContractOfferResolverImplIntegrationTest {
         verify(agentService).createFor(isA(ClaimToken.class));
         verify(contractDefinitionService, times(1)).definitionsFor(isA(ParticipantAgent.class));
         verify(policyStore).findById("contract");
+    }
+
+    private static Stream<Arguments> ranges() {
+        return Stream.of(
+                Arguments.of(0, 12),
+                Arguments.of(8, 14),
+                Arguments.of(0, 999),
+                Arguments.of(4, 888),
+                Arguments.of(3, 20),
+                Arguments.of(23, 25)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ranges")
+    void shouldReturnOfferSubsetAcrossMultipleContractDefinitions(int from, int to) {
+
+        var assets1 = range(0, 10).mapToObj(i -> createAsset("asset-" + i).build()).collect(Collectors.toList());
+        var assets2 = range(10, 20).mapToObj(i -> createAsset("asset-" + i).build()).collect(Collectors.toList());
+        var maximumRange = max(0, (assets1.size() + assets2.size()) - from);
+        var requestedRange = to - from;
+
+        store(assets1);
+        store(assets2);
+
+        var contractDefinition1 = getContractDefBuilder("contract-definition-")
+                .selectorExpression(selectorFrom(assets1)).build();
+        var contractDefinition2 = getContractDefBuilder("contract-definition-")
+                .selectorExpression(selectorFrom(assets2)).build();
+
+        when(agentService.createFor(isA(ClaimToken.class))).thenReturn(new ParticipantAgent(emptyMap(), emptyMap()));
+        when(contractDefinitionService.definitionsFor(isA(ParticipantAgent.class))).thenAnswer(i -> Stream.of(contractDefinition1, contractDefinition2));
+        when(policyStore.findById(any())).thenReturn(PolicyDefinition.Builder.newInstance().policy(Policy.Builder.newInstance().build()).build());
+
+        var query = ContractOfferQuery.builder().range(new Range(from, to)).claimToken(ClaimToken.Builder.newInstance().build()).build();
+        assertThat(contractOfferResolver.queryContractOffers(query)).hasSize(min(requestedRange, maximumRange));
     }
 
     @Test
