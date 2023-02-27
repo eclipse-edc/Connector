32be32e33eedd3e39754489c0ca5e8aac0a7f467
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2023-02-27 10:24:37 +0100

    chore: upgrade to gradle 8, upgrade the shadow plugin

--- system-tests/runtimes/azure-storage-transfer-consumer/build.gradle.kts
+++ system-tests/runtimes/azure-storage-transfer-consumer/build.gradle.kts
@@ -16,7 +16,7 @@
 plugins {
     `java-library`
     id("application")
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.0.0"
 }
 
 dependencies {
