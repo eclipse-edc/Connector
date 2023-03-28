14a31c11ea187a10a70212c7c1d72eec950799da
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2023-03-28 17:11:52 +0200

    chore(dsp-api-config): add dsp api config class

---
+++ data-protocols/dsp/dsp-api-configuration/src/main/java/org/eclipse/edc/protocol/dsp/api/configuration/DspApiConfiguration.java
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2023 Fraunhofer Institute for Software and Systems Engineering
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Fraunhofer Institute for Software and Systems Engineering - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.protocol.dsp.api.configuration;
+
+public class DspApiConfiguration {
+    
+    private final String contextAlias;
+    private final String dspWebhookAddress;
+    
+    public DspApiConfiguration(String contextAlias, String dspWebhookAddress) {
+        this.contextAlias = contextAlias;
+        this.dspWebhookAddress = dspWebhookAddress;
+    }
+    
+    public String getContextAlias() {
+        return contextAlias;
+    }
+    
+    public String getDspWebhookAddress() {
+        return dspWebhookAddress;
+    }
+    
+}
