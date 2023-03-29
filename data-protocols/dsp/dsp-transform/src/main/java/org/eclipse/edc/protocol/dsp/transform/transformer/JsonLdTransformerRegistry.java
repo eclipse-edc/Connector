a628f58e042413d9752923120ad27e7029f4bf6e
Ronja Quensel <ronja.quensel@isst.fraunhofer.de>
2023-03-29 16:15:07 +0200

    chore(dsp-transform): add base structure for transformers

---
+++ data-protocols/dsp/dsp-transform/src/main/java/org/eclipse/edc/protocol/dsp/transform/transformer/JsonLdTransformerRegistry.java
@@ -0,0 +1,6 @@
+package org.eclipse.edc.protocol.dsp.transform.transformer;
+
+import org.eclipse.edc.transform.spi.TypeTransformerRegistry;
+
+public interface JsonLdTransformerRegistry extends TypeTransformerRegistry<JsonLdTransformer<?, ?>> {
+}
