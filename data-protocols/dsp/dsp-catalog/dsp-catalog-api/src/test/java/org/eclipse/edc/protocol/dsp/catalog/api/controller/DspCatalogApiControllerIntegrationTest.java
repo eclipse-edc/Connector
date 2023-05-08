742160570723683f23467f6025336c108346aa67
Jan Peter Meyer <jan.peter.meyer@isst.fraunhofer.de>
2023-05-08 15:15:58 +0200

    chore(dsp-catalog-error): adapt test

--- data-protocols/dsp/dsp-catalog/dsp-catalog-api/src/test/java/org/eclipse/edc/protocol/dsp/catalog/api/controller/DspCatalogApiControllerIntegrationTest.java
+++ data-protocols/dsp/dsp-catalog/dsp-catalog-api/src/test/java/org/eclipse/edc/protocol/dsp/catalog/api/controller/DspCatalogApiControllerIntegrationTest.java
@@ -22,6 +22,7 @@ import jakarta.ws.rs.core.MediaType;
 import org.eclipse.edc.catalog.spi.Catalog;
 import org.eclipse.edc.catalog.spi.DataServiceRegistry;
 import org.eclipse.edc.connector.spi.catalog.CatalogProtocolService;
+import org.eclipse.edc.jsonld.spi.JsonLdKeywords;
 import org.eclipse.edc.junit.annotations.ApiTest;
 import org.eclipse.edc.junit.extensions.EdcExtension;
 import org.eclipse.edc.service.spi.result.ServiceResult;
@@ -36,6 +37,7 @@ import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static java.util.Collections.emptyList;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.CONTEXT;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.TYPE;
 import static org.eclipse.edc.jsonld.spi.Namespaces.DCAT_PREFIX;
@@ -44,6 +46,7 @@ import static org.eclipse.edc.jsonld.spi.Namespaces.DSPACE_SCHEMA;
 import static org.eclipse.edc.junit.testfixtures.TestUtils.getFreePort;
 import static org.eclipse.edc.protocol.dsp.catalog.api.CatalogApiPaths.BASE_PATH;
 import static org.eclipse.edc.protocol.dsp.catalog.api.CatalogApiPaths.CATALOG_REQUEST;
+import static org.eclipse.edc.protocol.dsp.catalog.api.DspCatalogTypeNames.DSPACE_CATALOG_ERROR;
 import static org.eclipse.edc.protocol.dsp.catalog.transform.DspCatalogPropertyAndTypeNames.DSPACE_CATALOG_REQUEST_TYPE;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -106,12 +109,17 @@ class DspCatalogApiControllerIntegrationTest {
     void catalogRequest_authenticationFailed_returnUnauthorized() {
         when(identityService.verifyJwtToken(any(), any())).thenReturn(Result.failure("error"));
 
-        baseRequest()
+        var result = baseRequest()
                 .body(request)
                 .contentType(MediaType.APPLICATION_JSON)
                 .post(BASE_PATH + CATALOG_REQUEST)
                 .then()
-                .statusCode(401);
+                .statusCode(401)
+                .extract().as(Map.class);
+
+        assertThat(result.get(JsonLdKeywords.TYPE)).isEqualTo(DSPACE_CATALOG_ERROR);
+        assertThat(result.get(DSPACE_SCHEMA + "code")).isEqualTo("401");
+        assertThat(result.get(DSPACE_SCHEMA + "reason")).isNotNull();
     }
 
     @Test
@@ -126,12 +134,17 @@ class DspCatalogApiControllerIntegrationTest {
                 .add(TYPE, "not-a-catalog-request-message")
                 .build();
 
-        baseRequest()
+        var result = baseRequest()
                 .body(invalidRequest)
                 .contentType(MediaType.APPLICATION_JSON)
                 .post(BASE_PATH + CATALOG_REQUEST)
                 .then()
-                .statusCode(400);
+                .statusCode(400)
+                .extract().as(Map.class);
+
+        assertThat(result.get(JsonLdKeywords.TYPE)).isEqualTo(DSPACE_CATALOG_ERROR);
+        assertThat(result.get(DSPACE_SCHEMA + "code")).isEqualTo("400");
+        assertThat(result.get(DSPACE_SCHEMA + "reason")).isNotNull();
     }
 
     private RequestSpecification baseRequest() {
