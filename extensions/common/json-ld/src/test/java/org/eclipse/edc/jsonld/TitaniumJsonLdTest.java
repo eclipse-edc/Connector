5535fd116047e7e0d62209035c00a8f65e5f2509
ndr_brt <andrea.bertagnolli@gmail.com>
2023-05-10 18:52:33 +0200

    feat: implement json-ld file document resolution

--- extensions/common/json-ld/src/test/java/org/eclipse/edc/jsonld/TitaniumJsonLdTest.java
+++ extensions/common/json-ld/src/test/java/org/eclipse/edc/jsonld/TitaniumJsonLdTest.java
@@ -15,8 +15,14 @@
 package org.eclipse.edc.jsonld;
 
 import jakarta.json.Json;
+import jakarta.json.JsonValue;
+import org.eclipse.edc.jsonld.spi.JsonLd;
 import org.eclipse.edc.spi.monitor.Monitor;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.mockserver.integration.ClientAndServer;
+
+import java.io.File;
 
 import static jakarta.json.Json.createArrayBuilder;
 import static jakarta.json.Json.createObjectBuilder;
@@ -24,11 +30,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.CONTEXT;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.TYPE;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.VALUE;
+import static org.eclipse.edc.junit.assertions.AbstractResultAssert.assertThat;
+import static org.eclipse.edc.junit.testfixtures.TestUtils.getFileFromResourceName;
+import static org.eclipse.edc.junit.testfixtures.TestUtils.getFreePort;
+import static org.eclipse.edc.junit.testfixtures.TestUtils.getResourceFileContentAsString;
 import static org.mockito.Mockito.mock;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 class TitaniumJsonLdTest {
 
-    private final TitaniumJsonLd service = new TitaniumJsonLd(mock(Monitor.class));
+    private final int port = getFreePort();
+    private final ClientAndServer server = startClientAndServer(port);
+
+    private final Monitor monitor = mock(Monitor.class);
+
+    @AfterEach
+    void tearDown() {
+        server.stop();
+    }
 
     @Test
     void expand() {
@@ -40,7 +61,8 @@ class TitaniumJsonLdTest {
                         .build())
                 .build();
 
-        var expanded = service.expand(jsonObject);
+        var expanded = defaultService().expand(jsonObject);
+
         assertThat(expanded.succeeded()).isTrue();
         assertThat(expanded.getContent().toString())
                 .contains("test:item")
@@ -52,7 +74,8 @@ class TitaniumJsonLdTest {
 
     @Test
     void expand_withEmptyArray() {
-        var expanded = service.expand(createObjectBuilder().build());
+        var expanded = defaultService().expand(createObjectBuilder().build());
+
         assertThat(expanded.succeeded()).isTrue();
         assertThat(expanded.getContent()).isEmpty();
     }
@@ -68,7 +91,8 @@ class TitaniumJsonLdTest {
                         .build())
                 .build();
 
-        var expanded = service.expand(jsonObject);
+        var expanded = defaultService().expand(jsonObject);
+
         assertThat(expanded.succeeded()).isTrue();
         assertThat(expanded.getContent().toString())
                 .contains("test:item")
@@ -88,7 +112,7 @@ class TitaniumJsonLdTest {
                         .add(ns + "key2", createArrayBuilder().add(createObjectBuilder().add(VALUE, "value2").build()).build()))
                 .build();
 
-        var compacted = service.compact(expanded);
+        var compacted = defaultService().compact(expanded);
 
         assertThat(compacted.succeeded()).isTrue();
         assertThat(compacted.getContent().getJsonObject(ns + "item")).isNotNull();
@@ -107,6 +131,7 @@ class TitaniumJsonLdTest {
                         .add(ns + "key2", createArrayBuilder().add(createObjectBuilder().add(VALUE, "value2").build()).build()))
                 .build();
 
+        var service = defaultService();
         service.registerNamespace(prefix, ns);
         var compacted = service.compact(expanded);
 
@@ -114,5 +139,71 @@ class TitaniumJsonLdTest {
         assertThat(compacted.getContent().getJsonObject(prefix + ":item")).isNotNull();
         assertThat(compacted.getContent().getJsonObject(prefix + ":item").getJsonString(prefix + ":key1").getString()).isEqualTo("value1");
         assertThat(compacted.getContent().getJsonObject(prefix + ":item").getJsonString(prefix + ":key2").getString()).isEqualTo("value2");
+    }
+
+    @Test
+    void documentResolution_shouldNotCallHttpEndpoint_whenFileContextIsRegistered() {
+        var contextUrl = "http://localhost:" + port;
+        var jsonObject = createObjectBuilder()
+                .add(CONTEXT, contextUrl)
+                .add("test:key", "value")
+                .build();
+        var service = defaultService();
+        service.registerCachedDocument(contextUrl, getFileFromResourceName("test-context.jsonld"));
+
+        var expanded = service.expand(jsonObject);
+
+        server.verifyZeroInteractions();
+        assertThat(expanded).isSucceeded().satisfies(json -> {
+            assertThat(json.getJsonArray("http://test.org/context/key")).hasSize(1).first()
+                    .extracting(JsonValue::asJsonObject)
+                    .extracting(it -> it.getString(VALUE))
+                    .isEqualTo("value");
+        });
+    }
+
+    @Test
+    void documentResolution_shouldFailByDefault_whenContextIsNotRegisteredAndHttpIsNotEnabled() {
+        server.when(request()).respond(response(getResourceFileContentAsString("test-context.jsonld")));
+        var contextUrl = "http://localhost:" + port;
+        var jsonObject = createObjectBuilder()
+                .add(CONTEXT, contextUrl)
+                .add("test:key", "value")
+                .build();
+        var service = defaultService();
+        service.registerCachedDocument("http//any.other/url", new File("any"));
+
+        var expanded = service.expand(jsonObject);
+
+        assertThat(expanded).isFailed();
+    }
+
+    @Test
+    void documentResolution_shouldCallHttpEndpoint_whenContextIsNotRegistered_andHttpIsEnabled() {
+        server.when(request()).respond(response(getResourceFileContentAsString("test-context.jsonld")));
+        var contextUrl = "http://localhost:" + port;
+        var jsonObject = createObjectBuilder()
+                .add(CONTEXT, contextUrl)
+                .add("test:key", "value")
+                .build();
+        var service = httpEnabledService();
+        service.registerCachedDocument("http//any.other/url", new File("any"));
+
+        var expanded = service.expand(jsonObject);
+
+        assertThat(expanded).isSucceeded().satisfies(json -> {
+            assertThat(json.getJsonArray("http://test.org/context/key")).hasSize(1).first()
+                    .extracting(JsonValue::asJsonObject)
+                    .extracting(it -> it.getString(VALUE))
+                    .isEqualTo("value");
+        });
+    }
+
+    private JsonLd httpEnabledService() {
+        return new TitaniumJsonLd(monitor, JsonLdConfiguration.Builder.newInstance().httpEnabled(true).build());
+    }
+
+    private JsonLd defaultService() {
+        return new TitaniumJsonLd(monitor);
     }
 }
