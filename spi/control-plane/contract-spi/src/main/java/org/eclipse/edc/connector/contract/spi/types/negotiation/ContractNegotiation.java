414f0793577ada117e18f99336aa7efdff2627ed
Julia Pampus <julia.pampus@isst.fraunhofer.de>
2023-05-12 12:19:55 +0200

    chore: remove checksum from ContractNegotiation

--- spi/control-plane/contract-spi/src/main/java/org/eclipse/edc/connector/contract/spi/types/negotiation/ContractNegotiation.java
+++ spi/control-plane/contract-spi/src/main/java/org/eclipse/edc/connector/contract/spi/types/negotiation/ContractNegotiation.java
@@ -66,7 +66,6 @@ public class ContractNegotiation extends StatefulEntity<ContractNegotiation> {
     private String correlationId;
     private String counterPartyId;
     private String counterPartyAddress;
-    private String checksum;
     private String protocol;
     private Type type = CONSUMER;
     private ContractAgreement contractAgreement;
@@ -82,10 +81,6 @@ public class ContractNegotiation extends StatefulEntity<ContractNegotiation> {
 
     public String getCounterPartyAddress() {
         return counterPartyAddress;
-    }
-
-    public String getChecksum() {
-        return checksum;
     }
 
     /**
@@ -418,11 +413,6 @@ public class ContractNegotiation extends StatefulEntity<ContractNegotiation> {
 
         public Builder counterPartyAddress(String address) {
             entity.counterPartyAddress = address;
-            return this;
-        }
-
-        public Builder checksum(String checksum) {
-            entity.checksum = checksum;
             return this;
         }
 
