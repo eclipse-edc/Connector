7e6089c9ac61310a05f08d6037bf877920095d9f
ndr_brt <andrea.bertagnolli@gmail.com>
2023-07-06 14:25:44 +0200

    feat: make http client timeouts configurable (#3277)
    * feat: make http client timeouts configurable
    
    * pr remarks

--- core/common/connector-core/src/main/java/org/eclipse/edc/connector/core/base/OkHttpClientFactory.java
+++ core/common/connector-core/src/main/java/org/eclipse/edc/connector/core/base/OkHttpClientFactory.java
@@ -24,15 +24,29 @@ import org.eclipse.edc.spi.system.ServiceExtensionContext;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
+import static java.lang.Integer.parseInt;
 import static java.lang.String.format;
 import static java.util.Optional.ofNullable;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class OkHttpClientFactory {
 
-    @Setting(value = "If true, enable HTTPS call enforcement. Default value is 'false'", type = "boolean")
+    private static final String DEFAULT_TIMEOUT = "30";
+    private static final String DEFAULT_HTTPS_ENFORCE = "false";
+
+    @Setting(value = "DEPRECATED. If true, enable HTTPS call enforcement. Default value is 'false'", type = "boolean")
+    @Deprecated(since = "0.1.3")
     public static final String EDC_HTTP_ENFORCE_HTTPS = "edc.http.enforce-https";
+
+    @Setting(value = "If true, enable HTTPS call enforcement.", defaultValue = DEFAULT_HTTPS_ENFORCE, type = "boolean")
+    public static final String EDC_HTTP_CLIENT_HTTPS_ENFORCE = "edc.http.client.https.enforce";
+
+    @Setting(value = "HTTP Client connect timeout, in seconds", defaultValue = DEFAULT_TIMEOUT, type = "int")
+    public static final String EDC_HTTP_CLIENT_TIMEOUT_CONNECT = "edc.http.client.timeout.connect";
+
+    @Setting(value = "HTTP Client read timeout, in seconds", defaultValue = DEFAULT_TIMEOUT, type = "int")
+    public static final String EDC_HTTP_CLIENT_TIMEOUT_READ = "edc.http.client.timeout.read";
 
     /**
      * Create an OkHttpClient instance
@@ -43,13 +57,20 @@ public class OkHttpClientFactory {
      */
     @NotNull
     public static OkHttpClient create(ServiceExtensionContext context, EventListener okHttpEventListener) {
+        var connectTimeout = context.getSetting(EDC_HTTP_CLIENT_TIMEOUT_CONNECT, parseInt(DEFAULT_TIMEOUT));
+        var readTimeout = context.getSetting(EDC_HTTP_CLIENT_TIMEOUT_READ, parseInt(DEFAULT_TIMEOUT));
+
         var builder = new OkHttpClient.Builder()
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS);
+                .connectTimeout(connectTimeout, SECONDS)
+                .readTimeout(readTimeout, SECONDS);
 
         ofNullable(okHttpEventListener).ifPresent(builder::eventListener);
 
-        var enforceHttps = context.getSetting(EDC_HTTP_ENFORCE_HTTPS, false);
+        if (context.getSetting(EDC_HTTP_ENFORCE_HTTPS, null) != null) {
+            context.getMonitor().warning(format("Configuration setting %s has been deprecated, please use %s instead", EDC_HTTP_ENFORCE_HTTPS, EDC_HTTP_CLIENT_HTTPS_ENFORCE));
+        }
+
+        var enforceHttps = context.getSetting(EDC_HTTP_CLIENT_HTTPS_ENFORCE, context.getSetting(EDC_HTTP_ENFORCE_HTTPS, Boolean.parseBoolean(DEFAULT_HTTPS_ENFORCE)));
         if (enforceHttps) {
             builder.addInterceptor(new EnforceHttps());
         } else {
