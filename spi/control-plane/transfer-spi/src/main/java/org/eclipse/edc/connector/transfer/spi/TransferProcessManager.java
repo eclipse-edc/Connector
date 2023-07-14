7ab1e4e03eee9b0bdca39eb8729f44a1d54583a0
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2023-07-14 09:20:51 +0200

    chore: remove unneeded method (#3300)

--- spi/control-plane/transfer-spi/src/main/java/org/eclipse/edc/connector/transfer/spi/TransferProcessManager.java
+++ spi/control-plane/transfer-spi/src/main/java/org/eclipse/edc/connector/transfer/spi/TransferProcessManager.java
@@ -19,7 +19,6 @@ import org.eclipse.edc.connector.transfer.spi.types.TransferRequest;
 import org.eclipse.edc.connector.transfer.spi.types.command.TransferProcessCommand;
 import org.eclipse.edc.runtime.metamodel.annotation.ExtensionPoint;
 import org.eclipse.edc.spi.response.StatusResult;
-import org.eclipse.edc.spi.result.Result;
 
 /**
  * Manages data transfer processes. Currently synchronous and asynchronous data transfers are supported.
@@ -36,10 +35,5 @@ public interface TransferProcessManager {
      * Adds a single Command to the queue which is processed sequentially.
      */
     void enqueueCommand(TransferProcessCommand command);
-
-    /**
-     * Run a command syncronously
-     */
-    Result<Void> runCommand(TransferProcessCommand command);
 
 }
