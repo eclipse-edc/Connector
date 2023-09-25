f2618098bbabd5038cb590b7718c56559b1aae9b
Mohannad Ezzo <mohannad.ezzo@sap.com>
2023-09-25 14:58:12 +0200

    feat: adjust JsonObjectToContractDefinition transformer

--- extensions/control-plane/api/management-api/contract-definition-api/src/main/java/org/eclipse/edc/connector/api/management/contractdefinition/transform/JsonObjectToContractDefinitionTransformer.java
+++ extensions/control-plane/api/management-api/contract-definition-api/src/main/java/org/eclipse/edc/connector/api/management/contractdefinition/transform/JsonObjectToContractDefinitionTransformer.java
@@ -9,12 +9,15 @@
  *
  *  Contributors:
  *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *       SAP SE - add private properties to contract definition
  *
  */
 
 package org.eclipse.edc.connector.api.management.contractdefinition.transform;
 
+import jakarta.json.JsonArray;
 import jakarta.json.JsonObject;
+import jakarta.json.JsonValue;
 import org.eclipse.edc.connector.contract.spi.types.offer.ContractDefinition;
 import org.eclipse.edc.jsonld.spi.transformer.AbstractJsonLdTransformer;
 import org.eclipse.edc.spi.query.Criterion;
@@ -22,9 +25,7 @@ import org.eclipse.edc.transform.spi.TransformerContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import static org.eclipse.edc.connector.contract.spi.types.offer.ContractDefinition.CONTRACT_DEFINITION_ACCESSPOLICY_ID;
-import static org.eclipse.edc.connector.contract.spi.types.offer.ContractDefinition.CONTRACT_DEFINITION_ASSETS_SELECTOR;
-import static org.eclipse.edc.connector.contract.spi.types.offer.ContractDefinition.CONTRACT_DEFINITION_CONTRACTPOLICY_ID;
+import static org.eclipse.edc.connector.contract.spi.types.offer.ContractDefinition.*;
 
 
 public class JsonObjectToContractDefinitionTransformer extends AbstractJsonLdTransformer<JsonObject, ContractDefinition> {
@@ -36,19 +37,23 @@ public class JsonObjectToContractDefinitionTransformer extends AbstractJsonLdTra
     @Override
     public @Nullable ContractDefinition transform(@NotNull JsonObject object, @NotNull TransformerContext context) {
         var builder = ContractDefinition.Builder.newInstance();
-
         builder.id(nodeId(object));
-
-        visitProperties(object, (key, value) -> {
-            switch (key) {
-                case CONTRACT_DEFINITION_ACCESSPOLICY_ID -> builder.accessPolicyId(transformString(value, context));
-                case CONTRACT_DEFINITION_CONTRACTPOLICY_ID -> builder.contractPolicyId(transformString(value, context));
-                case CONTRACT_DEFINITION_ASSETS_SELECTOR -> builder.assetsSelector(transformArray(value, Criterion.class, context));
-                default -> { }
-            }
-        });
-
+        visitProperties(object, (s, jsonValue) -> transformProperties(s, jsonValue, builder, context));
         return builderResult(builder::build, context);
     }
 
+    private void transformProperties(String key, JsonValue jsonValue, ContractDefinition.Builder builder, TransformerContext context) {
+        if (CONTRACT_DEFINITION_ACCESSPOLICY_ID.equals(key) ) {
+            builder.accessPolicyId(transformString(jsonValue, context));
+        } else if (CONTRACT_DEFINITION_CONTRACTPOLICY_ID.equals(key) ) {
+            builder.contractPolicyId(transformString(jsonValue, context));
+        } else if (CONTRACT_DEFINITION_ASSETS_SELECTOR.equals(key) ) {
+            builder.assetsSelector(transformArray(jsonValue, Criterion.class, context));
+        } else if (CONTRACT_DEFINITION_PRIVATE_PROPERTIES.equals(key) && jsonValue instanceof JsonArray) {
+            var props = jsonValue.asJsonArray().getJsonObject(0);
+            visitProperties(props, (k, val) -> transformProperties(k, val, builder, context));
+        } else {
+                builder.privateProperty(key, transformGenericProperty(jsonValue, context));
+            }
+        }
 }
