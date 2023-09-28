b1edd63f1eb553af042f81228c1bfb8e7d06f691
Mohannad Ezzo <mohannad.ezzo@sap.com>
2023-09-28 11:31:10 +0200

    adjust sql schemam and ER diagram

--- extensions/control-plane/store/sql/contract-definition-store-sql/docs/schema.sql
+++ extensions/control-plane/store/sql/contract-definition-store-sql/docs/schema.sql
@@ -10,6 +10,7 @@
 --  Contributors:
 --       Daimler TSS GmbH - Initial SQL Query
 --       Microsoft Corporation - refactoring
+--       SAP SE - add private properties to contract definition
 --
 
 -- table: edc_contract_definitions
@@ -23,3 +24,30 @@ CREATE TABLE IF NOT EXISTS edc_contract_definitions
     assets_selector        JSON    NOT NULL,
     PRIMARY KEY (contract_definition_id)
 );
+
+
+-- table: edc_contract_definition_property
+CREATE TABLE IF NOT EXISTS edc_contract_definition_property
+(
+    contract_definition_id_fk       VARCHAR(255) NOT NULL,
+    property_name  VARCHAR(255) NOT NULL,
+    property_value TEXT         NOT NULL,
+    property_type  VARCHAR(255) NOT NULL,
+    property_is_private BOOLEAN,
+    PRIMARY KEY (contract_definition_id_fk, property_name),
+    FOREIGN KEY (contract_definition_id_fk) REFERENCES edc_contract_definitions (contract_definition_id) ON DELETE CASCADE
+);
+
+
+CREATE INDEX IF NOT EXISTS idx_edc_contract_definition_property_value
+    ON edc_contract_definition_property (property_name, property_value);
+
+
+COMMENT ON COLUMN edc_contract_definition_property.property_name IS
+    'Contract definition property key';
+COMMENT ON COLUMN edc_contract_definition_property.property_value IS
+    'Contract definition property value';
+COMMENT ON COLUMN edc_contract_definition_property.property_type IS
+    'Contract definition property class name';
+COMMENT ON COLUMN edc_contract_definition_property.property_is_private IS
+    'Contract definition property private flag';
