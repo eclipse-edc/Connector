5bf493ebb2729f9182123861ae44d890e87b332d
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2023-10-12 13:41:26 +0200

    feat: add trusted issuer registry

---
+++ spi/common/identity-trust-spi/src/main/java/org/eclipse/edc/identitytrust/model/Issuer.java
@@ -0,0 +1,20 @@
+/*
+ *  Copyright (c) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.identitytrust.model;
+
+import java.util.Map;
+
+public record Issuer(String id, Map<String, Object> additionalProperties) {
+}
