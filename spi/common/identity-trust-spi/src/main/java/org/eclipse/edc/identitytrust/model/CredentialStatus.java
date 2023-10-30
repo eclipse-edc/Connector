1dd9370a68cdbba7ef13f1c224b5edbfab08d213
Jim Marino <jim.marino@gmail.com>
2023-10-30 17:27:09 +0100

    Remove typo

--- spi/common/identity-trust-spi/src/main/java/org/eclipse/edc/identitytrust/model/CredentialStatus.java
+++ spi/common/identity-trust-spi/src/main/java/org/eclipse/edc/identitytrust/model/CredentialStatus.java
@@ -17,6 +17,6 @@ package org.eclipse.edc.identitytrust.model;
 import java.util.Map;
 
 public record CredentialStatus(String id, String type, Map<String, Object> additionalProperties) {
-    public static final String CREDENTIAL_STATUS_ID_PROPERTY = "@id";;
+    public static final String CREDENTIAL_STATUS_ID_PROPERTY = "@id";
     public static final String CREDENTIAL_STATUS_TYPE_PROPERTY = "@type";
 }
