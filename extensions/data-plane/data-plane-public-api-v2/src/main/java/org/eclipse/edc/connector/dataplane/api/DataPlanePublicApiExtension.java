20bb0a46e5b6642412796fff85c619fabe122139
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-03-12 07:23:20 +0100

    feat: register default PublicEndpointGenerator fct (#3991)

--- extensions/data-plane/data-plane-public-api-v2/src/main/java/org/eclipse/edc/connector/dataplane/api/DataPlanePublicApiExtension.java
+++ extensions/data-plane/data-plane-public-api-v2/src/main/java/org/eclipse/edc/connector/dataplane/api/DataPlanePublicApiExtension.java
@@ -15,11 +15,15 @@
 package org.eclipse.edc.connector.dataplane.api;
 
 import org.eclipse.edc.connector.dataplane.api.controller.DataPlanePublicApiV2Controller;
+import org.eclipse.edc.connector.dataplane.spi.Endpoint;
 import org.eclipse.edc.connector.dataplane.spi.iam.DataPlaneAuthorizationService;
+import org.eclipse.edc.connector.dataplane.spi.iam.PublicEndpointGeneratorService;
 import org.eclipse.edc.connector.dataplane.spi.pipeline.PipelineService;
 import org.eclipse.edc.runtime.metamodel.annotation.Extension;
 import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.runtime.metamodel.annotation.Setting;
 import org.eclipse.edc.spi.system.ExecutorInstrumentation;
+import org.eclipse.edc.spi.system.Hostname;
 import org.eclipse.edc.spi.system.ServiceExtension;
 import org.eclipse.edc.spi.system.ServiceExtensionContext;
 import org.eclipse.edc.web.spi.WebServer;
@@ -36,13 +40,17 @@ import java.util.concurrent.Executors;
 @Extension(value = DataPlanePublicApiExtension.NAME)
 public class DataPlanePublicApiExtension implements ServiceExtension {
     public static final String NAME = "Data Plane Public API";
-    private static final int DEFAULT_PUBLIC_PORT = 8185;
-    private static final String PUBLIC_API_CONFIG = "web.http.public";
-    private static final String PUBLIC_CONTEXT_ALIAS = "public";
-    private static final String PUBLIC_CONTEXT_PATH = "/api/v2/public";
+
+
+    public static final int DEFAULT_PUBLIC_PORT = 8185;
+    public static final String PUBLIC_API_CONFIG = "web.http.public";
+    public static final String PUBLIC_CONTEXT_ALIAS = "public";
+    public static final String PUBLIC_CONTEXT_PATH = "/api/v2/public";
+    @Setting(value = "Base url of the public API endpoint without the trailing slash. This should correspond to the values configured " +
+            "in '" + DEFAULT_PUBLIC_PORT + "' and '" + PUBLIC_CONTEXT_PATH + "'.", defaultValue = "http://<HOST>:" + DEFAULT_PUBLIC_PORT + PUBLIC_CONTEXT_PATH)
+    public static final String PUBLIC_ENDPOINT = "edc.dataplane.api.public.baseurl";
 
     private static final int DEFAULT_THREAD_POOL = 10;
-
     private static final WebServiceSettings PUBLIC_SETTINGS = WebServiceSettings.Builder.newInstance()
             .apiConfigKey(PUBLIC_API_CONFIG)
             .contextAlias(PUBLIC_CONTEXT_ALIAS)
@@ -65,8 +73,15 @@ public class DataPlanePublicApiExtension implements ServiceExtension {
 
     @Inject
     private ExecutorInstrumentation executorInstrumentation;
+
     @Inject
     private DataPlaneAuthorizationService authorizationService;
+
+    @Inject
+    private PublicEndpointGeneratorService generatorService;
+
+    @Inject
+    private Hostname hostname;
 
     @Override
     public String name() {
@@ -80,6 +95,15 @@ public class DataPlanePublicApiExtension implements ServiceExtension {
                 Executors.newFixedThreadPool(DEFAULT_THREAD_POOL),
                 "Data plane proxy transfers"
         );
+
+        var publicEndpoint = context.getSetting(PUBLIC_ENDPOINT, null);
+        if (publicEndpoint == null) {
+            publicEndpoint = "http://%s:%d%s".formatted(hostname.get(), configuration.getPort(), configuration.getPath());
+            context.getMonitor().warning("Config property '%s' was not specified, the default '%s' will be used.".formatted(PUBLIC_ENDPOINT, publicEndpoint));
+        }
+        var endpoint = Endpoint.url(publicEndpoint);
+        generatorService.addGeneratorFunction("HttpData", dataAddress -> endpoint);
+
         var publicApiController = new DataPlanePublicApiV2Controller(pipelineService, executorService, authorizationService);
         webService.registerResource(configuration.getContextAlias(), publicApiController);
     }
