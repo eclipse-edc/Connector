659078986e58755f20efb074e2eb9a7b19eda9a4
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-03-29 10:05:40 +0100

    chore: issue SEVERE message when no DID value provided (#4063)
    * chore: issue SEVERE message when no DID value provided
    
    * DEPENDENCIES

--- extensions/common/iam/identity-trust/identity-trust-core/src/test/java/org/eclipse/edc/iam/identitytrust/core/IdentityAndTrustExtensionTest.java
+++ extensions/common/iam/identity-trust/identity-trust-core/src/test/java/org/eclipse/edc/iam/identitytrust/core/IdentityAndTrustExtensionTest.java
@@ -27,6 +27,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.eclipse.edc.spi.CoreConstants.JSON_LD;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -46,12 +47,12 @@ class IdentityAndTrustExtensionTest {
     @Test
     void verifyCorrectService(IdentityAndTrustExtension extension, ServiceExtensionContext context) {
         var configMock = mock(Config.class);
-        when(configMock.getString(eq(IdentityAndTrustExtension.CONNECTOR_DID_PROPERTY))).thenReturn("did:web:test");
+        when(configMock.getString(eq(IdentityAndTrustExtension.CONNECTOR_DID_PROPERTY), isNull())).thenReturn("did:web:test");
         when(context.getConfig()).thenReturn(configMock);
 
         var is = extension.createIdentityService(context);
 
         assertThat(is).isInstanceOf(IdentityAndTrustService.class);
-        verify(configMock, atLeastOnce()).getString(eq(IdentityAndTrustExtension.CONNECTOR_DID_PROPERTY));
+        verify(configMock, atLeastOnce()).getString(eq(IdentityAndTrustExtension.CONNECTOR_DID_PROPERTY), isNull());
     }
 }
