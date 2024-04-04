5aeed7193a2233536427a24ca8ca9aaea7dbe6bc
ndr_brt <andrea.bertagnolli@gmail.com>
2024-04-04 15:40:50 +0200

    fix: accessService serialization

--- data-protocols/dsp/dsp-catalog/dsp-catalog-transform/src/test/java/org/eclipse/edc/protocol/dsp/catalog/transform/from/JsonObjectFromDistributionTransformerTest.java
+++ data-protocols/dsp/dsp-catalog/dsp-catalog-transform/src/test/java/org/eclipse/edc/protocol/dsp/catalog/transform/from/JsonObjectFromDistributionTransformerTest.java
@@ -16,10 +16,10 @@ package org.eclipse.edc.protocol.dsp.catalog.transform.from;
 
 import jakarta.json.Json;
 import jakarta.json.JsonBuilderFactory;
+import jakarta.json.JsonObject;
 import org.eclipse.edc.connector.controlplane.catalog.spi.DataService;
 import org.eclipse.edc.connector.controlplane.catalog.spi.Distribution;
 import org.eclipse.edc.transform.spi.TransformerContext;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -30,37 +30,37 @@ import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.TYPE;
 import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.DCAT_ACCESS_SERVICE_ATTRIBUTE;
 import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.DCAT_DISTRIBUTION_TYPE;
 import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.DCT_FORMAT_ATTRIBUTE;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class JsonObjectFromDistributionTransformerTest {
 
-    private JsonBuilderFactory jsonFactory = Json.createBuilderFactory(Map.of());
-    private TransformerContext context = mock(TransformerContext.class);
+    private final JsonBuilderFactory jsonFactory = Json.createBuilderFactory(Map.of());
+    private final TransformerContext context = mock();
 
-    private JsonObjectFromDistributionTransformer transformer;
-
-    @BeforeEach
-    void setUp() {
-        transformer = new JsonObjectFromDistributionTransformer(jsonFactory);
-    }
+    private final JsonObjectFromDistributionTransformer transformer = new JsonObjectFromDistributionTransformer(jsonFactory);
 
     @Test
     void transform_returnJsonObject() {
-        var distribution = getDistribution();
+        var accessServiceJson = jsonFactory.createObjectBuilder().add(ID, "dataServiceId").build();
+        when(context.transform(any(), eq(JsonObject.class))).thenReturn(accessServiceJson);
+        var distribution = Distribution.Builder.newInstance()
+                .format("format")
+                .dataService(DataService.Builder.newInstance().id("dataServiceId").build())
+                .build();
+
         var result = transformer.transform(distribution, context);
 
         assertThat(result).isNotNull();
         assertThat(result.getJsonString(TYPE).getString()).isEqualTo(DCAT_DISTRIBUTION_TYPE);
         assertThat(result.getJsonObject(DCT_FORMAT_ATTRIBUTE).getJsonString(ID).getString())
                 .isEqualTo(distribution.getFormat());
-        assertThat(result.getJsonString(DCAT_ACCESS_SERVICE_ATTRIBUTE).getString())
-                .isEqualTo(distribution.getDataService().getId());
+        assertThat(result.getJsonObject(DCAT_ACCESS_SERVICE_ATTRIBUTE).getString(ID)).isEqualTo("dataServiceId");
+        verify(context).transform(isA(DataService.class), eq(JsonObject.class));
     }
 
-    private Distribution getDistribution() {
-        return Distribution.Builder.newInstance()
-                .format("format")
-                .dataService(DataService.Builder.newInstance().id("dataServiceId").build())
-                .build();
-    }
 }
