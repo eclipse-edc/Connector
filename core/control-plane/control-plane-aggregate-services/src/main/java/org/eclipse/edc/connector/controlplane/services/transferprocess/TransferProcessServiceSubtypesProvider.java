896ff37f562bed3db7f72c23afb5348f147bba75
Encho Belezirev <encho.belezirev@gmail.com>
2024-04-05 11:41:28 +0300

    fix:Add documentation on new classes

--- core/control-plane/control-plane-aggregate-services/src/main/java/org/eclipse/edc/connector/controlplane/services/transferprocess/TransferProcessServiceSubtypesProvider.java
+++ core/control-plane/control-plane-aggregate-services/src/main/java/org/eclipse/edc/connector/controlplane/services/transferprocess/TransferProcessServiceSubtypesProvider.java
@@ -1,3 +1,16 @@
+/*
+ *  Copyright (c) 2024 Encho Belezirev (Digital Lights)
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Encho Belezirev (Digital Lights) - refactor(test): improve QueryValidator testing strategy
+ *
+ */
 
 package org.eclipse.edc.connector.controlplane.services.transferprocess;
 
