6d1fd445bb2ac19698a82489a4a48b2cb8e97dde
ndr_brt <andrea.bertagnolli@gmail.com>
2024-04-17 16:37:32 +0200

    feat: add data map on TransformContext

--- core/common/lib/transform-lib/src/test/java/org/eclipse/edc/transform/TransformerContextImplTest.java
+++ core/common/lib/transform-lib/src/test/java/org/eclipse/edc/transform/TransformerContextImplTest.java
@@ -14,7 +14,9 @@
 
 package org.eclipse.edc.transform;
 
+import org.eclipse.edc.transform.spi.TransformerContext;
 import org.eclipse.edc.transform.spi.TypeTransformerRegistry;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,8 +28,8 @@ import static org.mockito.Mockito.when;
 
 class TransformerContextImplTest {
 
-    private final TypeTransformerRegistry registry = mock(TypeTransformerRegistry.class);
-    private final TransformerContextImpl context = new TransformerContextImpl(registry);
+    private final TypeTransformerRegistry registry = mock();
+    private final TransformerContext context = new TransformerContextImpl(registry);
 
     @Test
     void shouldReturnTransformedInput() {
@@ -54,5 +56,44 @@ class TransformerContextImplTest {
 
         assertThat(result).isNull();
         verifyNoInteractions(registry);
+    }
+
+    @Nested
+    class Data {
+        @Test
+        void shouldClearData_whenConsumed() {
+            context.setData(Integer.class, "key", "value");
+
+            var data = context.consumeData(Integer.class, "key");
+
+            assertThat(data).isEqualTo("value");
+            assertThat(context.consumeData(Integer.class, "key")).isNull();
+        }
+
+        @Test
+        void shouldReturnNull_whenKeyDoesNotExist() {
+            context.setData(Integer.class, "key", "value");
+
+            var data = context.consumeData(Integer.class, "unexistent-key");
+
+            assertThat(data).isNull();
+        }
+
+        @Test
+        void shouldReturnNull_whenTypeDoesNotExist() {
+            var data = context.consumeData(Double.class, "any");
+
+            assertThat(data).isNull();
+        }
+
+        @Test
+        void shouldPermitMultipleDataPerType() {
+            context.setData(Integer.class, "key", "value");
+            context.setData(Integer.class, "another-key", "another-value");
+
+            var data = context.consumeData(Integer.class, "key");
+
+            assertThat(data).isEqualTo("value");
+        }
     }
 }
