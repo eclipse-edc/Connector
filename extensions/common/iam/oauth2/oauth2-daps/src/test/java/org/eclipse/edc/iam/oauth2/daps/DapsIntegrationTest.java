9b435a7b23dde7127f8c3e6f3d3d9825d2fa194f
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-05-21 17:18:57 +0200

    fix daps tests

--- extensions/common/iam/oauth2/oauth2-daps/src/test/java/org/eclipse/edc/iam/oauth2/daps/DapsIntegrationTest.java
+++ extensions/common/iam/oauth2/oauth2-daps/src/test/java/org/eclipse/edc/iam/oauth2/daps/DapsIntegrationTest.java
@@ -22,7 +22,6 @@ import org.eclipse.edc.policy.model.Policy;
 import org.eclipse.edc.spi.iam.IdentityService;
 import org.eclipse.edc.spi.iam.TokenParameters;
 import org.eclipse.edc.spi.iam.VerificationContext;
-import org.eclipse.edc.spi.security.Vault;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,9 +39,10 @@ import static org.eclipse.edc.junit.testfixtures.TestUtils.findBuildRoot;
 @ComponentTest
 class DapsIntegrationTest {
 
+    public static final String CLIENT_CERTIFICATE_ALIAS = "1";
+    public static final String CLIENT_PRIVATE_KEY_ALIAS = "2";
     private static final String AUDIENCE_IDS_CONNECTORS_ALL = "idsc:IDS_CONNECTORS_ALL";
     private static final String CLIENT_ID = "68:99:2E:D4:13:2D:FD:3A:66:6B:85:DE:FB:98:2E:2D:FD:E7:83:D7";
-    private static final String CLIENT_KEYSTORE_KEY_ALIAS = "1";
     private static final String CLIENT_KEYSTORE_PASSWORD = "1234";
 
     private final Path resourceFolder = findBuildRoot().toPath().resolve("extensions/common/iam/oauth2/oauth2-daps/src/test/resources");
@@ -54,7 +54,7 @@ class DapsIntegrationTest {
             .withFileSystemBind(resourceFolder.resolve("keys").toString(), "/opt/keys");
 
     @Test
-    void retrieveTokenAndValidate(IdentityService identityService, Vault vault) {
+    void retrieveTokenAndValidate(IdentityService identityService) {
         var tokenParameters = TokenParameters.Builder.newInstance()
                 .claims(JwtRegisteredClaimNames.SCOPE, "idsc:IDS_CONNECTOR_ATTRIBUTES_ALL")
                 .claims(JwtRegisteredClaimNames.AUDIENCE, "audience")
@@ -89,8 +89,8 @@ class DapsIntegrationTest {
                 "edc.oauth.provider.audience", AUDIENCE_IDS_CONNECTORS_ALL,
                 "edc.oauth.endpoint.audience", AUDIENCE_IDS_CONNECTORS_ALL,
                 "edc.oauth.provider.jwks.url", dapsUrl + jwksPath,
-                "edc.oauth.certificate.alias", CLIENT_KEYSTORE_KEY_ALIAS,
-                "edc.oauth.private.key.alias", CLIENT_KEYSTORE_KEY_ALIAS,
+                "edc.oauth.certificate.alias", CLIENT_CERTIFICATE_ALIAS,
+                "edc.oauth.private.key.alias", CLIENT_PRIVATE_KEY_ALIAS,
                 "edc.iam.token.scope", "idsc:IDS_CONNECTOR_ATTRIBUTES_ALL"
         ));
     }
