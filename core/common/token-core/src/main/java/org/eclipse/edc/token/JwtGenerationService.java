b6f1db0006666b8bd68da12d4d2f54558b5286be
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-08-07 11:42:02 +0200

    Update core/common/token-core/src/main/java/org/eclipse/edc/token/JwtGenerationService.java
    Co-authored-by: ndr_brt <andrea.bertagnolli@gmail.com>

--- core/common/token-core/src/main/java/org/eclipse/edc/token/JwtGenerationService.java
+++ core/common/token-core/src/main/java/org/eclipse/edc/token/JwtGenerationService.java
@@ -39,7 +39,7 @@ public class JwtGenerationService implements TokenGenerationService {
 
     private final JwsSignerProvider jwsGeneratorFunction;
 
-    public JwtGenerationService(JwsSignerProvider jwsGeneratorFunction) {
+    public JwtGenerationService(JwsSignerProvider jwsSignerProvider) {
 
         this.jwsGeneratorFunction = jwsGeneratorFunction;
     }
