339e45192e5af2fc1bce67dfd97522095b68511b
Paul Latzelsperger <paul.latzelsperger@beardyinc.com>
2024-08-20 15:47:11 +0200

    improved tests

--- spi/common/verifiable-credentials-spi/src/test/java/org/eclipse/edc/iam/verifiablecredentials/spi/model/bitstringstatuslist/BitstringStatusListCredentialTest.java
+++ spi/common/verifiable-credentials-spi/src/test/java/org/eclipse/edc/iam/verifiablecredentials/spi/model/bitstringstatuslist/BitstringStatusListCredentialTest.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.eclipse.edc.iam.verifiablecredentials.spi.model.bitstringstatuslist.BitstringStatusListCredential.STATUS_LIST_ENCODED_LIST;
 import static org.eclipse.edc.iam.verifiablecredentials.spi.model.bitstringstatuslist.BitstringStatusListCredential.STATUS_LIST_TTL;
 import static org.eclipse.edc.iam.verifiablecredentials.spi.model.bitstringstatuslist.BitstringStatusListStatus.STATUS_LIST_PURPOSE;
@@ -55,4 +56,67 @@ class BitstringStatusListCredentialTest {
         assertThat(statusListCred.validUntil()).isNull();
         assertThat(statusListCred.ttl()).hasMillis(123_456);
     }
+
+    @Test
+    void parse_useDefaultValue_ifTtlNotPresent() {
+        // values taken from https://www.w3.org/TR/vc-bitstring-status-list/#example-example-bitstringstatuslistcredential
+        //noinspection unchecked
+        var credential = VerifiableCredential.Builder.newInstance()
+                .id("https://example.com/credentials/status/3")
+                .types(List.of("VerifiableCredential", "BitstringStatusListCredential"))
+                .issuer(new Issuer("did:example:12345", Map.of()))
+                .issuanceDate(Instant.parse("2021-04-05T14:27:40Z"))
+                .credentialSubject(CredentialSubject.Builder.newInstance()
+                        .id("https://example.com/status/3#list")
+                        .claim("type", "BitstringStatusList")
+                        .claim(STATUS_LIST_PURPOSE, "revocation")
+                        .claim(STATUS_LIST_ENCODED_LIST, "uH4sIAAAAAAAAA-3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAIC3AYbSVKsAQAAA")
+                        .build())
+                .build();
+
+        var statusListCred = BitstringStatusListCredential.parse(credential);
+
+        assertThat(statusListCred.ttl()).hasMillis(300_000);
+    }
+
+    @Test
+    void parse_noPurpose_expectException() {
+        //noinspection unchecked
+        var credential = VerifiableCredential.Builder.newInstance()
+                .id("https://example.com/credentials/status/3")
+                .types(List.of("VerifiableCredential", "BitstringStatusListCredential"))
+                .issuer(new Issuer("did:example:12345", Map.of()))
+                .issuanceDate(Instant.parse("2021-04-05T14:27:40Z"))
+                .credentialSubject(CredentialSubject.Builder.newInstance()
+                        .id("https://example.com/status/3#list")
+                        .claim("type", "BitstringStatusList")
+                        // .claim(STATUS_LIST_PURPOSE, "revocation")
+                        .claim(STATUS_LIST_ENCODED_LIST, "uH4sIAAAAAAAAA-3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAIC3AYbSVKsAQAAA")
+                        .claim(STATUS_LIST_TTL, 123_456)
+                        .build())
+                .build();
+
+        assertThatThrownBy(() -> BitstringStatusListCredential.parse(credential)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void parse_noEncodedList_expectException() {
+        //noinspection unchecked
+        var credential = VerifiableCredential.Builder.newInstance()
+                .id("https://example.com/credentials/status/3")
+                .types(List.of("VerifiableCredential", "BitstringStatusListCredential"))
+                .issuer(new Issuer("did:example:12345", Map.of()))
+                .issuanceDate(Instant.parse("2021-04-05T14:27:40Z"))
+                .credentialSubject(CredentialSubject.Builder.newInstance()
+                        .id("https://example.com/status/3#list")
+                        .claim("type", "BitstringStatusList")
+                        .claim(STATUS_LIST_PURPOSE, "revocation")
+                        //.claim(STATUS_LIST_ENCODED_LIST, "uH4sIAAAAAAAAA-3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAIC3AYbSVKsAQAAA")
+                        .claim(STATUS_LIST_TTL, 123_456)
+                        .build())
+                .build();
+
+        assertThatThrownBy(() -> BitstringStatusListCredential.parse(credential)).isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
--- spi/common/verifiable-credentials-spi/src/test/java/org/eclipse/edc/iam/verifiablecredentials/spi/model/statuslist2021/StatusList2021CredentialTest.java
+++ spi/common/verifiable-credentials-spi/src/test/java/org/eclipse/edc/iam/verifiablecredentials/spi/model/statuslist2021/StatusList2021CredentialTest.java
@@ -17,6 +17,8 @@ package org.eclipse.edc.iam.verifiablecredentials.spi.model.statuslist2021;
 import org.eclipse.edc.iam.verifiablecredentials.spi.model.CredentialSubject;
 import org.eclipse.edc.iam.verifiablecredentials.spi.model.Issuer;
 import org.eclipse.edc.iam.verifiablecredentials.spi.model.VerifiableCredential;
+import org.eclipse.edc.iam.verifiablecredentials.spi.model.bitstringstatuslist.BitstringStatusListCredential;
+import org.eclipse.edc.iam.verifiablecredentials.spi.model.bitstringstatuslist.BitstringStatusListStatus;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
@@ -24,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.eclipse.edc.iam.verifiablecredentials.spi.model.statuslist2021.StatusList2021Credential.STATUS_LIST_ENCODED_LIST;
 import static org.eclipse.edc.iam.verifiablecredentials.spi.model.statuslist2021.StatusList2021Status.STATUS_LIST_PURPOSE;
 
@@ -52,5 +55,42 @@ public class StatusList2021CredentialTest {
         assertThat(statusListCred.statusPurpose()).isEqualTo("revocation");
     }
 
+    @Test
+    void parse_noPurpose_expectException() {
+        //noinspection unchecked
+        var credential = VerifiableCredential.Builder.newInstance()
+                .id("https://example.com/credentials/status/3")
+                .types(List.of("VerifiableCredential", "StatusList2021Credential"))
+                .issuer(new Issuer("did:example:12345", Map.of()))
+                .issuanceDate(Instant.parse("2021-04-05T14:27:40Z"))
+                .credentialSubject(CredentialSubject.Builder.newInstance()
+                        .id("https://example.com/status/3#list")
+                        .claim("type", "BitstringStatusList")
+                        // .claim(STATUS_LIST_PURPOSE, "revocation")
+                        .claim(BitstringStatusListCredential.STATUS_LIST_ENCODED_LIST, "uH4sIAAAAAAAAA-3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAIC3AYbSVKsAQAAA")
+                        .build())
+                .build();
+
+        assertThatThrownBy(() -> StatusList2021Credential.parse(credential)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void parse_noEncodedList_expectException() {
+        //noinspection unchecked
+        var credential = VerifiableCredential.Builder.newInstance()
+                .id("https://example.com/credentials/status/3")
+                .types(List.of("VerifiableCredential", "StatusList2021Credential"))
+                .issuer(new Issuer("did:example:12345", Map.of()))
+                .issuanceDate(Instant.parse("2021-04-05T14:27:40Z"))
+                .credentialSubject(CredentialSubject.Builder.newInstance()
+                        .id("https://example.com/status/3#list")
+                        .claim("type", "BitstringStatusList")
+                        .claim(BitstringStatusListStatus.STATUS_LIST_PURPOSE, "revocation")
+                        //.claim(STATUS_LIST_ENCODED_LIST, "uH4sIAAAAAAAAA-3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAIC3AYbSVKsAQAAA")
+                        .build())
+                .build();
+
+        assertThatThrownBy(() -> StatusList2021Credential.parse(credential)).isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
