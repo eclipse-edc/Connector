44592f5438fe9d6ee86d6d47f5b00d677b133519
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2024-10-04 11:07:52 +0200

    fix: stay with static container/test initialization not to risk multiple vault instances. Courtesy of @paullatzelsperger.

--- extensions/common/vault/vault-hashicorp/src/test/java/org/eclipse/edc/vault/hashicorp/client/HashicorpVaultClientIntegrationTest.java
+++ extensions/common/vault/vault-hashicorp/src/test/java/org/eclipse/edc/vault/hashicorp/client/HashicorpVaultClientIntegrationTest.java
@@ -9,7 +9,6 @@
  *
  *  Contributors:
  *       Mercedes-Benz Tech Innovation GmbH - Implement automatic Hashicorp Vault token renewal
- *       T-Systems International GmbH - Allow to subclass for exchanging the vault implementation to test against
  *
  */
 
@@ -20,8 +19,8 @@ import jakarta.json.Json;
 import org.eclipse.edc.junit.annotations.ComponentTest;
 import org.eclipse.edc.spi.monitor.ConsoleMonitor;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.vault.VaultContainer;
@@ -37,107 +36,158 @@ import static org.eclipse.edc.http.client.testfixtures.HttpTestUtils.testHttpCli
 import static org.eclipse.edc.junit.assertions.AbstractResultAssert.assertThat;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
-@ComponentTest
-@Testcontainers
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 class HashicorpVaultClientIntegrationTest {
-    @Container
-    final VaultContainer<?> VAULT_CONTAINER = new VaultContainer<>(getVaultImage())
-            .withVaultToken(UUID.randomUUID().toString());
 
-    private final String HTTP_URL_FORMAT = "http://%s:%s";
-    private final String HEALTH_CHECK_PATH = "/health/path";
-    private final String CLIENT_TOKEN_KEY = "client_token";
-    private final String AUTH_KEY = "auth";
-    private final long CREATION_TTL = 6L;
-    private final long TTL = 5L;
-    private final long RENEW_BUFFER = 4L;
-    private final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-    private final ConsoleMonitor MONITOR = new ConsoleMonitor();
+    @ComponentTest
+    @Testcontainers
+    @Nested
+    abstract static class Tests {
 
-    private HashicorpVaultClient client;
+        protected static final String HTTP_URL_FORMAT = "http://%s:%s";
+        protected static final String HEALTH_CHECK_PATH = "/health/path";
+        protected static final String CLIENT_TOKEN_KEY = "client_token";
+        protected static final String AUTH_KEY = "auth";
+        protected static final long CREATION_TTL = 6L;
+        protected static final long TTL = 5L;
+        protected static final long RENEW_BUFFER = 4L;
+        protected HashicorpVaultClient client;
+        protected final ObjectMapper mapper = new ObjectMapper();
+        protected final ConsoleMonitor monitor = new ConsoleMonitor();
 
-    /**
-     * Determines the vault container to test against
-     * @return coordinates of the FOSS vault implementation
-     */
-    protected String getVaultImage() {
-        return "vault:1.9.6";
+        @BeforeEach
+        void beforeEach() throws IOException, InterruptedException {
+            assertThat(CREATION_TTL).isGreaterThan(TTL);
+        }
+
+        @Test
+        void lookUpToken_whenTokenNotExpired_shouldSucceed() {
+            var tokenLookUpResult = client.isTokenRenewable();
+
+            assertThat(tokenLookUpResult).isSucceeded().isEqualTo(true);
+        }
+
+        @Test
+        void lookUpToken_whenTokenExpired_shouldFail() {
+            await()
+                    .pollDelay(CREATION_TTL, TimeUnit.SECONDS)
+                    .atMost(CREATION_TTL + 1, TimeUnit.SECONDS)
+                    .untilAsserted(() -> {
+                        var tokenLookUpResult = client.isTokenRenewable();
+                        assertThat(tokenLookUpResult).isFailed();
+                        assertThat(tokenLookUpResult.getFailureDetail()).isEqualTo("Token look up failed with status 403");
+                    });
+        }
+
+        @Test
+        void renewToken_whenTokenNotExpired_shouldSucceed() {
+            var tokenRenewResult = client.renewToken();
+
+            assertThat(tokenRenewResult).isSucceeded().satisfies(ttl -> assertThat(ttl).isEqualTo(TTL));
+        }
+
+        @Test
+        void renewToken_whenTokenExpired_shouldFail() {
+            await()
+                    .pollDelay(CREATION_TTL, TimeUnit.SECONDS)
+                    .atMost(CREATION_TTL + 1, TimeUnit.SECONDS)
+                    .untilAsserted(() -> {
+                        var tokenRenewResult = client.renewToken();
+                        assertThat(tokenRenewResult).isFailed();
+                        assertThat(tokenRenewResult.getFailureDetail()).isEqualTo("Token renew failed with status: 403");
+                    });
+        }
     }
 
-    @BeforeEach
-    void beforeEach() throws IOException, InterruptedException {
-        assertThat(CREATION_TTL).isGreaterThan(TTL);
-        client = new HashicorpVaultClient(
-                testHttpClient(),
-                OBJECT_MAPPER,
-                MONITOR,
-                getSettings()
-        );
+    @ComponentTest
+    @Testcontainers
+    @Nested
+    class LastKnownFoss extends Tests {
+        @Container
+        static final VaultContainer<?> VAULT_CONTAINER = new VaultContainer<>("vault:1.9.6")
+                .withVaultToken(UUID.randomUUID().toString());
+
+        public static HashicorpVaultSettings getSettings() throws IOException, InterruptedException {
+            var execResult = VAULT_CONTAINER.execInContainer(
+                    "vault",
+                    "token",
+                    "create",
+                    "-policy=root",
+                    "-ttl=%d".formatted(CREATION_TTL),
+                    "-format=json");
+
+            var jsonParser = Json.createParser(new StringReader(execResult.getStdout()));
+            jsonParser.next();
+            var auth = jsonParser.getObjectStream().filter(e -> e.getKey().equals(AUTH_KEY))
+                    .map(Map.Entry::getValue)
+                    .findFirst()
+                    .orElseThrow()
+                    .asJsonObject();
+            var clientToken = auth.getString(CLIENT_TOKEN_KEY);
+
+            return HashicorpVaultSettings.Builder.newInstance()
+                    .url(HTTP_URL_FORMAT.formatted(VAULT_CONTAINER.getHost(), VAULT_CONTAINER.getFirstMappedPort()))
+                    .healthCheckPath(HEALTH_CHECK_PATH)
+                    .token(clientToken)
+                    .ttl(TTL)
+                    .renewBuffer(RENEW_BUFFER)
+                    .build();
+        }
+
+        @BeforeEach
+        void beforeEach() throws IOException, InterruptedException {
+            client = new HashicorpVaultClient(
+                    testHttpClient(),
+                    mapper,
+                    monitor,
+                    getSettings()
+            );
+        }
     }
 
-    @Test
-    void lookUpToken_whenTokenNotExpired_shouldSucceed() {
-        var tokenLookUpResult = client.isTokenRenewable();
+    @ComponentTest
+    @Testcontainers
+    @Nested
+    class Latest extends Tests {
+        @Container
+        static final VaultContainer<?> VAULT_CONTAINER = new VaultContainer<>("hashicorp/vault:1.17.3")
+                .withVaultToken(UUID.randomUUID().toString());
 
-        assertThat(tokenLookUpResult).isSucceeded().isEqualTo(true);
-    }
+        public static HashicorpVaultSettings getSettings() throws IOException, InterruptedException {
+            var execResult = VAULT_CONTAINER.execInContainer(
+                    "vault",
+                    "token",
+                    "create",
+                    "-policy=root",
+                    "-ttl=%d".formatted(CREATION_TTL),
+                    "-format=json");
 
-    @Test
-    void lookUpToken_whenTokenExpired_shouldFail() {
-        await()
-                .pollDelay(CREATION_TTL, TimeUnit.SECONDS)
-                .atMost(CREATION_TTL + 1, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    var tokenLookUpResult = client.isTokenRenewable();
-                    assertThat(tokenLookUpResult).isFailed();
-                    assertThat(tokenLookUpResult.getFailureDetail()).isEqualTo("Token look up failed with status 403");
-                });
-    }
+            var jsonParser = Json.createParser(new StringReader(execResult.getStdout()));
+            jsonParser.next();
+            var auth = jsonParser.getObjectStream().filter(e -> e.getKey().equals(AUTH_KEY))
+                    .map(Map.Entry::getValue)
+                    .findFirst()
+                    .orElseThrow()
+                    .asJsonObject();
+            var clientToken = auth.getString(CLIENT_TOKEN_KEY);
 
-    @Test
-    void renewToken_whenTokenNotExpired_shouldSucceed() {
-        var tokenRenewResult = client.renewToken();
+            return HashicorpVaultSettings.Builder.newInstance()
+                    .url(HTTP_URL_FORMAT.formatted(VAULT_CONTAINER.getHost(), VAULT_CONTAINER.getFirstMappedPort()))
+                    .healthCheckPath(HEALTH_CHECK_PATH)
+                    .token(clientToken)
+                    .ttl(TTL)
+                    .renewBuffer(RENEW_BUFFER)
+                    .build();
+        }
 
-        assertThat(tokenRenewResult).isSucceeded().satisfies(ttl -> assertThat(ttl).isEqualTo(TTL));
-    }
-
-    @Test
-    void renewToken_whenTokenExpired_shouldFail() {
-        await()
-                .pollDelay(CREATION_TTL, TimeUnit.SECONDS)
-                .atMost(CREATION_TTL + 1, TimeUnit.SECONDS)
-                .untilAsserted(() -> {
-                    var tokenRenewResult = client.renewToken();
-                    assertThat(tokenRenewResult).isFailed();
-                    assertThat(tokenRenewResult.getFailureDetail()).isEqualTo("Token renew failed with status: 403");
-                });
-    }
-
-    public HashicorpVaultSettings getSettings() throws IOException, InterruptedException {
-        var execResult = VAULT_CONTAINER.execInContainer(
-                "vault",
-                "token",
-                "create",
-                "-policy=root",
-                "-ttl=%d".formatted(CREATION_TTL),
-                "-format=json");
-
-        var jsonParser = Json.createParser(new StringReader(execResult.getStdout()));
-        jsonParser.next();
-        var auth = jsonParser.getObjectStream().filter(e -> e.getKey().equals(AUTH_KEY))
-                .map(Map.Entry::getValue)
-                .findFirst()
-                .orElseThrow()
-                .asJsonObject();
-        var clientToken = auth.getString(CLIENT_TOKEN_KEY);
-
-        return HashicorpVaultSettings.Builder.newInstance()
-                .url(HTTP_URL_FORMAT.formatted(VAULT_CONTAINER.getHost(), VAULT_CONTAINER.getFirstMappedPort()))
-                .healthCheckPath(HEALTH_CHECK_PATH)
-                .token(clientToken)
-                .ttl(TTL)
-                .renewBuffer(RENEW_BUFFER)
-                .build();
+        @BeforeEach
+        void beforeEach() throws IOException, InterruptedException {
+            client = new HashicorpVaultClient(
+                    testHttpClient(),
+                    mapper,
+                    monitor,
+                    getSettings()
+            );
+        }
     }
 }
