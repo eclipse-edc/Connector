146a231ec3b5b23cfc2ae5a6474801032a29baab
ndr_brt <andrea.bertagnolli@gmail.com>
2024-10-15 09:17:47 +0200

    pr remark

--- core/control-plane/control-plane-aggregate-services/build.gradle.kts
+++ core/control-plane/control-plane-aggregate-services/build.gradle.kts
@@ -17,14 +17,13 @@ plugins {
 }
 
 dependencies {
-    api(project(":spi:common:boot-spi"))
-    api(project(":spi:common:policy:request-policy-context-spi"))
-    api(project(":spi:common:transaction-spi"))
-    api(project(":spi:common:validator-spi"))
-    api(project(":spi:control-plane:asset-spi"))
-    api(project(":spi:control-plane:control-plane-spi"))
-    api(project(":spi:control-plane:secrets-spi"))
-    api(project(":spi:control-plane:transfer-data-plane-spi"))
+    implementation(project(":spi:common:policy:request-policy-context-spi"))
+    implementation(project(":spi:common:transaction-spi"))
+    implementation(project(":spi:common:validator-spi"))
+    implementation(project(":spi:control-plane:asset-spi"))
+    implementation(project(":spi:control-plane:control-plane-spi"))
+    implementation(project(":spi:control-plane:secrets-spi"))
+    implementation(project(":spi:control-plane:transfer-data-plane-spi"))
     implementation(project(":core:common:lib:util-lib"))
 
     implementation(libs.opentelemetry.instrumentation.annotations)
