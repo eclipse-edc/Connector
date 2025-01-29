bc3fe6c5d276cd1668bb5e4e83e13fb4ff5c1faa
Enrico Risa <enrico.risa@gmail.com>
2025-01-29 15:16:03 +0100

    chore: pr suggestions

--- spi/common/core-spi/src/main/java/org/eclipse/edc/spi/protocol/ProtocolWebhookRegistry.java
+++ spi/common/core-spi/src/main/java/org/eclipse/edc/spi/protocol/ProtocolWebhookRegistry.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2023 Bayerische Motoren Werke Aktiengesellschaft (BMW AG)
+ *  Copyright (c) 2025 Cofinity-X
  *
  *  This program and the accompanying materials are made available under the
  *  terms of the Apache License, Version 2.0 which is available at
@@ -8,7 +8,7 @@
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Contributors:
- *       Bayerische Motoren Werke Aktiengesellschaft (BMW AG) - initial API and implementation
+ *       Cofinity-X - initial API and implementation
  *
  */
 
