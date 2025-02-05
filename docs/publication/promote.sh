d950478e86f92f64c526a72e5e72e474c589cc50
Christophe Loiseau <christophe.loiseau@sovity.de>
2025-02-05 10:55:04 +0100

    Update the promote.sh script

--- docs/publication/promote.sh
+++ docs/publication/promote.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-curl  -H "Authorization: Basic $AZURE_PAT" \
-"https://feeds.dev.azure.com/sovity/41799556-91c8-4df6-8ddb-4471d6f15953/_apis/packaging/Feeds/core-edc/packages?protocolType=maven&includeUrls=false&includeAllVersions=true&includeDeleted=false&api-version=7.0" \
+# Core EDC https://dev.azure.com/sovity/Core%20EDC
+PROJECT="41799556-91c8-4df6-8ddb-4471d6f15953"
+#core-edc feed https://dev.azure.com/sovity/Core%20EDC/_artifacts/feed/core-edc
+FEED="core-edc"
+
+curl -u ":$AZURE_PAT" \
+"https://feeds.dev.azure.com/sovity/$PROJECT/_apis/packaging/Feeds/$FEED/packages?protocolType=maven&includeUrls=false&includeAllVersions=true&includeDeleted=false&api-version=7.0" \
 | jq --raw-output '.value[].name' | cut -d ':' -f 2 | while read -r arti
 do
+  echo "Promoting $arti ..."
   curl \
     -X PATCH \
-    -H "Authorization: Basic $AZURE_PAT" \
-    "https://pkgs.dev.azure.com/sovity/41799556-91c8-4df6-8ddb-4471d6f15953/_apis/packaging/feeds/core-edc/maven/groups/org.eclipse.edc/artifacts/${arti}/versions/${VERSION}?api-version=7.1-preview.1" \
+    -u ":$AZURE_PAT" \
+    "https://pkgs.dev.azure.com/sovity/$PROJECT/_apis/packaging/feeds/core-edc/maven/groups/org.eclipse.edc/artifacts/${arti}/versions/${VERSION}?api-version=7.1-preview.1" \
     --json @- <<"EOF"
 {
   "views": {
@@ -23,6 +29,8 @@ EOF
   then
     echo "Failed to promote $arti"
     exit 1
+  else
+    echo "Promoted $arti"
   fi
 
 done
