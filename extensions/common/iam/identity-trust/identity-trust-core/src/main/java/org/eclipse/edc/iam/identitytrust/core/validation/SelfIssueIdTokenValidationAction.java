a38935d1e41f34e65e2dd1c1c670846d44029d76
Enrico Risa <enrico.risa@gmail.com>
2025-03-06 17:10:00 +0100

    fix: adds IssuerKeyIdValidationRule to SI token validation

---
+++ extensions/common/iam/identity-trust/identity-trust-core/src/main/java/org/eclipse/edc/iam/identitytrust/core/validation/SelfIssueIdTokenValidationAction.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (c) 2025 Cofinity-X
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Cofinity-X - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.iam.identitytrust.core.validation;
+
+import com.nimbusds.jwt.SignedJWT;
+import org.eclipse.edc.iam.did.spi.resolution.DidPublicKeyResolver;
+import org.eclipse.edc.iam.identitytrust.spi.validation.TokenValidationAction;
+import org.eclipse.edc.spi.EdcException;
+import org.eclipse.edc.spi.iam.ClaimToken;
+import org.eclipse.edc.spi.iam.TokenRepresentation;
+import org.eclipse.edc.spi.result.Result;
+import org.eclipse.edc.token.spi.TokenValidationRulesRegistry;
+import org.eclipse.edc.token.spi.TokenValidationService;
+import org.eclipse.edc.verifiablecredentials.jwt.rules.IssuerKeyIdValidationRule;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import static org.eclipse.edc.iam.identitytrust.core.IdentityAndTrustExtension.DCP_SELF_ISSUED_TOKEN_CONTEXT;
+
+public class SelfIssueIdTokenValidationAction implements TokenValidationAction {
+
+
+    private final TokenValidationService tokenValidationService;
+    private final TokenValidationRulesRegistry rulesRegistry;
+    private final DidPublicKeyResolver didPublicKeyResolver;
+
+    public SelfIssueIdTokenValidationAction(TokenValidationService tokenValidationService, TokenValidationRulesRegistry rulesRegistry, DidPublicKeyResolver didPublicKeyResolver) {
+        this.tokenValidationService = tokenValidationService;
+        this.rulesRegistry = rulesRegistry;
+        this.didPublicKeyResolver = didPublicKeyResolver;
+    }
+
+    @Override
+    public Result<ClaimToken> apply(TokenRepresentation tokenRepresentation) {
+        try {
+            var signedJwt = SignedJWT.parse(tokenRepresentation.getToken());
+            var keyId = signedJwt.getHeader().getKeyID();
+            var rules = new ArrayList<>(rulesRegistry.getRules(DCP_SELF_ISSUED_TOKEN_CONTEXT));
+            rules.add(new IssuerKeyIdValidationRule(keyId));
+            return tokenValidationService.validate(tokenRepresentation, didPublicKeyResolver, rules);
+        } catch (ParseException e) {
+            throw new EdcException(e);
+        }
+    }
+}
