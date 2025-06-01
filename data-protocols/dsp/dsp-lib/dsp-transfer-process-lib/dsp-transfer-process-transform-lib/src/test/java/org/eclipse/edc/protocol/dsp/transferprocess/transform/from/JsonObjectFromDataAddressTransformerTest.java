4d1fc59db2db50277125e482df0ac7a902aa9561
Rafael Magalhaes <rafaelmagftw@gmail.com>
2025-06-01 21:35:52 +0100

    adapt transform test with more cases

--- data-protocols/dsp/dsp-lib/dsp-transfer-process-lib/dsp-transfer-process-transform-lib/src/test/java/org/eclipse/edc/protocol/dsp/transferprocess/transform/from/JsonObjectFromDataAddressTransformerTest.java
+++ data-protocols/dsp/dsp-lib/dsp-transfer-process-lib/dsp-transfer-process-transform-lib/src/test/java/org/eclipse/edc/protocol/dsp/transferprocess/transform/from/JsonObjectFromDataAddressTransformerTest.java
@@ -15,7 +15,9 @@
 package org.eclipse.edc.protocol.dsp.transferprocess.transform.from;
 
 import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonBuilderFactory;
+import jakarta.json.JsonObjectBuilder;
 import org.eclipse.edc.jsonld.util.JacksonJsonLd;
 import org.eclipse.edc.spi.types.TypeManager;
 import org.eclipse.edc.spi.types.domain.DataAddress;
@@ -58,49 +60,56 @@ class JsonObjectFromDataAddressTransformerTest {
 
     @Test
     void transform() {
+        var innerDataAddress = DataAddress.Builder.newInstance()
+                .property(EDC_DATA_ADDRESS_TYPE_PROPERTY, "type")
+                .build();
         var message = DataAddress.Builder.newInstance()
                 .type(type)
                 .keyName(key)
-                .property(TEST_KEY, TEST_VALUE)
+                .property("string", "test")
+                .property("integer", 123)
+                .property("whatAboutDouble", 123.456)
+                .property("nestedDataAddress", innerDataAddress)
+                .property("nestedJsonObject", Map.of("key", Map.of("testKey", "testValue")))
+                .property("jsonArray", List.of("string1", "string2"))
+                .property("arrayOfObjects", List.of(
+                        Map.of("key1", "value1"),
+                        Map.of("key2", "value2")))
+                .property("nestedJsonObjectWithArray", Map.of(
+                        "key", List.of("value1", "value2"),
+                        "anotherKey", List.of("value3", "value4")))
                 .build();
 
-        var result = transformer.transform(message, context);
-
-        assertThat(result).isNotNull();
-        assertThat(result.getJsonString(TEST_KEY).getString()).isEqualTo(TEST_VALUE);
-        assertThat(result.getJsonString(DataAddress.EDC_DATA_ADDRESS_TYPE_PROPERTY).getString()).isEqualTo(type);
-        assertThat(result.getJsonString(DataAddress.EDC_DATA_ADDRESS_KEY_NAME).getString()).isEqualTo(key);
-
-        verify(context, never()).reportProblem(anyString());
-    }
-
-    @Test
-    void transform_withCustomComplexProperty() {
-        var innerDataAddress = DataAddress.Builder.newInstance().property(EDC_DATA_ADDRESS_TYPE_PROPERTY, "type").build();
-        var message = DataAddress.Builder.newInstance()
-                .type(type)
-                .keyName(key)
-                .property("complexJsonObject", innerDataAddress)
-                .property("complexJsonArray", List.of("string1", "string2"))
-                .build();
-
-        var expectedJson = jsonFactory.createObjectBuilder()
+        var expectedJson = jsonObject()
                 .add(TYPE, EDC_NAMESPACE + "DataAddress")
                 .add(EDC_DATA_ADDRESS_TYPE_PROPERTY, type)
                 .add(EDC_DATA_ADDRESS_KEY_NAME, key)
-                .add("complexJsonObject", jsonFactory.createObjectBuilder()
-                        .add("properties", jsonFactory.createObjectBuilder()
-                                .add(EDC_DATA_ADDRESS_TYPE_PROPERTY, "type")))
-                .add("complexJsonArray", jsonFactory.createArrayBuilder()
-                        .add("string1")
-                        .add("string2"))
+                .add("string", "test")
+                .add("integer", 123)
+                .add("whatAboutDouble", 123.456)
+                .add("nestedDataAddress",
+                        jsonObject()
+                                .add("properties", jsonObject().add(EDC_DATA_ADDRESS_TYPE_PROPERTY, "type")))
+                .add("nestedJsonObject",
+                        jsonObject()
+                                .add("key", jsonObject().add("testKey", "testValue")))
+                .add("jsonArray",
+                        jsonArray()
+                                .add("string1").add("string2"))
+                .add("arrayOfObjects",
+                        jsonArray()
+                                .add(jsonObject().add("key1", "value1"))
+                                .add(jsonObject().add("key2", "value2")))
+                .add("nestedJsonObjectWithArray",
+                        jsonObject()
+                                .add("key", jsonArray().add("value1").add("value2"))
+                                .add("anotherKey", jsonArray().add("value3").add("value4")))
                 .build();
 
         var result = transformer.transform(message, context);
 
         assertThat(result).usingRecursiveAssertion().isEqualTo(expectedJson);
-
-
+        verify(context, never()).reportProblem(anyString());
     }
 
     @Test
@@ -116,5 +125,14 @@ class JsonObjectFromDataAddressTransformerTest {
         assertThat(result).isNotNull();
         assertThat(result.getJsonString(schema + TEST_KEY).getString()).isEqualTo(TEST_VALUE);
         verify(context, never()).reportProblem(anyString());
+    }
+
+    private JsonObjectBuilder jsonObject() {
+        return jsonFactory.createObjectBuilder();
+
+    }
+
+    private JsonArrayBuilder jsonArray() {
+        return jsonFactory.createArrayBuilder();
     }
 }
