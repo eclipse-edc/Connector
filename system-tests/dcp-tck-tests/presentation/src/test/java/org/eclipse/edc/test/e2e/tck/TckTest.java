b7137d14f9a8faf7036e6a956b5866407cb8b922
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2025-06-30 10:51:56 +0200

    feat: separate out TCK tests (#5072)
    * feat: separate out TCK tests
    
    * run ci

---
+++ system-tests/dcp-tck-tests/presentation/src/test/java/org/eclipse/edc/test/e2e/tck/TckTest.java
@@ -0,0 +1,31 @@
+/*
+ *  Copyright (c) 2025 Cofinity-X
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Apache License, Version 2.0 which is available at
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Contributors:
+ *       Cofinity-X - initial API and implementation
+ *
+ */
+
+package org.eclipse.edc.test.e2e.tck;
+
+import org.eclipse.edc.junit.annotations.IntegrationTest;
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@IntegrationTest
+@Tag("TckTest")
+public @interface TckTest {
+}
+
