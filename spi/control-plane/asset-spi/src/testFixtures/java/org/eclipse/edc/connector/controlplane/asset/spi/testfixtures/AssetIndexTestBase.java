ac8bfd1566bd81c07a794a9132727215750f9d56
Rafael Magalhães <rafaelmagftw@gmail.com>
2025-07-15 10:55:06 +0100

    fix: fixes DataAddress updates on InMemoryAssetIndex (#5118)
    * fix: fixes DataAddress updates on InMemoryAssetIndex
    
    * ignoring createdAt

--- spi/control-plane/asset-spi/src/testFixtures/java/org/eclipse/edc/connector/controlplane/asset/spi/testfixtures/AssetIndexTestBase.java
+++ spi/control-plane/asset-spi/src/testFixtures/java/org/eclipse/edc/connector/controlplane/asset/spi/testfixtures/AssetIndexTestBase.java
@@ -552,15 +552,21 @@ public abstract class AssetIndexTestBase {
 
             assertThat(assetIndex.countAssets(List.of())).isEqualTo(1);
 
-            asset.getProperties().put("newKey", "newValue");
-            var updated = assetIndex.updateAsset(asset);
+            var newAsset = getAsset(id);
+            newAsset.getProperties().put("newKey", "newValue");
+
+            var updated = assetIndex.updateAsset(newAsset);
 
             Assertions.assertThat(updated).isNotNull();
 
             var assetFound = getAssetIndex().findById("id1");
 
             assertThat(assetFound).isNotNull();
-            assertThat(assetFound).usingRecursiveComparison().isEqualTo(asset);
+            assertThat(assetFound)
+                    .usingRecursiveComparison()
+                    .ignoringFields("createdAt")
+                    .isEqualTo(newAsset);
+            assertThat(assetFound.getProperties()).containsEntry("newKey", "newValue");
         }
 
         @Test
@@ -574,15 +580,20 @@ public abstract class AssetIndexTestBase {
 
             assertThat(assetIndex.countAssets(List.of())).isEqualTo(1);
 
-            asset.getProperties().remove("newKey");
-            var updated = assetIndex.updateAsset(asset);
+            var newAsset = getAsset(id);
+            newAsset.getProperties().remove("newKey");
+
+            var updated = assetIndex.updateAsset(newAsset);
 
             Assertions.assertThat(updated).isNotNull();
 
             var assetFound = getAssetIndex().findById("id1");
 
             assertThat(assetFound).isNotNull();
-            assertThat(assetFound).usingRecursiveComparison().isEqualTo(asset);
+            assertThat(assetFound)
+                    .usingRecursiveComparison()
+                    .ignoringFields("createdAt")
+                    .isEqualTo(newAsset);
             assertThat(assetFound.getProperties().keySet()).doesNotContain("newKey");
         }
 
@@ -597,15 +608,19 @@ public abstract class AssetIndexTestBase {
 
             assertThat(assetIndex.countAssets(List.of())).isEqualTo(1);
 
-            asset.getProperties().put("newKey", "newValue");
-            var updated = assetIndex.updateAsset(asset);
+            var newAsset = getAsset(id);
+            newAsset.getProperties().put("newKey", "newValue");
+            var updated = assetIndex.updateAsset(newAsset);
 
             Assertions.assertThat(updated).isNotNull();
 
             var assetFound = getAssetIndex().findById("id1");
 
             assertThat(assetFound).isNotNull();
-            assertThat(assetFound).usingRecursiveComparison().isEqualTo(asset);
+            assertThat(assetFound)
+                    .usingRecursiveComparison()
+                    .ignoringFields("createdAt")
+                    .isEqualTo(newAsset);
             assertThat(assetFound.getProperties()).containsEntry("newKey", "newValue");
         }
 
@@ -618,15 +633,24 @@ public abstract class AssetIndexTestBase {
 
             assertThat(assetIndex.countAssets(List.of())).isEqualTo(1);
 
-            asset.getDataAddress().getProperties().put("newKey", "newValue");
-            var updated = assetIndex.updateAsset(asset);
+            var newAsset = getAsset(id);
+            newAsset.getDataAddress().getProperties().put("newKey", "newValue");
+            var updated = assetIndex.updateAsset(newAsset);
 
             Assertions.assertThat(updated).isNotNull();
 
-            var assetFound = getAssetIndex().findById("id1");
+            var assetFound = assetIndex.findById("id1");
+            var dataAddressFound = assetIndex.resolveForAsset("id1");
 
             assertThat(assetFound).isNotNull();
-            assertThat(assetFound).usingRecursiveComparison().isEqualTo(asset);
+            assertThat(dataAddressFound).isNotNull();
+            assertThat(assetFound)
+                    .usingRecursiveComparison()
+                    .ignoringFields("createdAt")
+                    .isEqualTo(newAsset);
+            assertThat(assetFound.getDataAddress())
+                    .usingRecursiveComparison()
+                    .isEqualTo(dataAddressFound);
         }
     }
 
