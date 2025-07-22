184627db6b56d3e7c2a621e5de9163b9937b4a70
Rafael Magalhaes <rafaelmagftw@gmail.com>
2025-07-22 18:04:17 +0100

    imports and null checks

--- extensions/data-plane/data-plane-iam/src/main/java/org/eclipse/edc/connector/dataplane/iam/service/DataPlaneAuthorizationServiceImpl.java
+++ extensions/data-plane/data-plane-iam/src/main/java/org/eclipse/edc/connector/dataplane/iam/service/DataPlaneAuthorizationServiceImpl.java
@@ -80,7 +80,7 @@ public class DataPlaneAuthorizationServiceImpl implements DataPlaneAuthorization
         // "decorate" the data address with response channel properties
         var responseChannelType = dataFlow.getTransferType().responseChannelType();
         if (responseChannelType != null) {
-            var responseChannel = dataFlow.getSource().getResponseChannel();
+            var responseChannel = sourceDataAddress.getResponseChannel();
             var responseChannelTokenParams = createTokenParams(dataFlow);
             dataAddressBuilder = dataAddressBuilder.compose(builder -> endpointGenerator.generateResponseFor(responseChannelType)
                     .compose(ep -> createSecureEndpoint(ep, responseChannelTokenParams, additionalProperties, responseChannel))
