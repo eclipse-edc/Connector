59b868323f35e6eb431a5eb82b7145a386f273d6
Rafael Magalhaes <rafaelmagftw@gmail.com>
2025-07-22 18:43:56 +0100

    logic change and missing test case

--- core/common/lib/transform-lib/src/test/java/org/eclipse/edc/transform/transformer/dspace/from/JsonObjectFromDataAddressDspaceTransformerTest.java
+++ core/common/lib/transform-lib/src/test/java/org/eclipse/edc/transform/transformer/dspace/from/JsonObjectFromDataAddressDspaceTransformerTest.java
@@ -15,6 +15,7 @@
 package org.eclipse.edc.transform.transformer.dspace.from;
 
 import jakarta.json.Json;
+import jakarta.json.JsonObject;
 import org.eclipse.edc.jsonld.util.JacksonJsonLd;
 import org.eclipse.edc.spi.types.TypeManager;
 import org.eclipse.edc.spi.types.domain.DataAddress;
@@ -25,6 +26,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.TYPE;
 import static org.eclipse.edc.transform.transformer.dspace.DataAddressDspaceSerialization.ENDPOINT_PROPERTIES_PROPERTY_IRI;
 import static org.eclipse.edc.transform.transformer.dspace.DataAddressDspaceSerialization.ENDPOINT_PROPERTY_IRI;
 import static org.eclipse.edc.transform.transformer.dspace.DataAddressDspaceSerialization.ENDPOINT_PROPERTY_NAME_PROPERTY_IRI;
@@ -93,4 +95,48 @@ class JsonObjectFromDataAddressDspaceTransformerTest {
                             .containsKey("complexObj");
                 });
     }
+
+    @Test
+    void transform_withResponseChannelDataAddress() {
+        var responseChannelDataAddress = DataAddress.Builder.newInstance()
+                .type("responseType")
+                .property("endpoint", "https://response.example.com")
+                .build();
+        var dataAddress = DataAddress.Builder.newInstance()
+                .type("someType")
+                .property("endpoint", "https://example.com")
+                .property(DataAddress.EDC_DATA_ADDRESS_RESPONSE_CHANNEL, responseChannelDataAddress)
+                .build();
+
+        when(context.transform(responseChannelDataAddress, JsonObject.class))
+                .thenReturn(Json.createObjectBuilder()
+                        .add(TYPE, "https://namespace/DataAddress")
+                        .add(ENDPOINT_TYPE_PROPERTY_IRI, "responseType")
+                        .add(ENDPOINT_PROPERTIES_PROPERTY_IRI, Json.createArrayBuilder()
+                                .add(Json.createObjectBuilder()
+                                        .add(ENDPOINT_PROPERTY_NAME_PROPERTY_IRI, "endpoint")
+                                        .add(ENDPOINT_PROPERTY_VALUE_PROPERTY_IRI, "https://response.example.com")))
+                        .build());
+
+        var jsonObject = transformer.transform(dataAddress, context);
+
+        assertThat(jsonObject).isNotNull();
+        assertThat(jsonObject.getJsonArray(ENDPOINT_PROPERTIES_PROPERTY_IRI))
+                .anySatisfy(jv -> {
+                    assertThat(jv.asJsonObject().getString(ENDPOINT_PROPERTY_NAME_PROPERTY_IRI))
+                            .isEqualTo(DataAddress.EDC_DATA_ADDRESS_RESPONSE_CHANNEL);
+                    var responseChannelJson = jv.asJsonObject().getJsonObject(ENDPOINT_PROPERTY_VALUE_PROPERTY_IRI);
+                    assertThat(responseChannelJson.getString(ENDPOINT_TYPE_PROPERTY_IRI))
+                            .isEqualTo("responseType");
+                    assertThat(responseChannelJson.getJsonArray(ENDPOINT_PROPERTIES_PROPERTY_IRI))
+                            .hasSize(1)
+                            .anySatisfy(prop -> {
+                                assertThat(prop.asJsonObject().getString(ENDPOINT_PROPERTY_NAME_PROPERTY_IRI))
+                                        .isEqualTo("endpoint");
+                                assertThat(prop.asJsonObject().getString(ENDPOINT_PROPERTY_VALUE_PROPERTY_IRI))
+                                        .isEqualTo("https://response.example.com");
+                            });
+                });
+    }
+
 }
