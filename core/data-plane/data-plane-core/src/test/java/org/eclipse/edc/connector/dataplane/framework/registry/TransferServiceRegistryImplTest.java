94ddf341b57247f2b429fba8681af39666689ce6
ndr_brt <andrea.bertagnolli@gmail.com>
2025-10-28 09:52:20 +0100

    refactor: removed deprecated TransferServiceSelectionStrategy

--- core/data-plane/data-plane-core/src/test/java/org/eclipse/edc/connector/dataplane/framework/registry/TransferServiceRegistryImplTest.java
+++ core/data-plane/data-plane-core/src/test/java/org/eclipse/edc/connector/dataplane/framework/registry/TransferServiceRegistryImplTest.java
@@ -19,26 +19,18 @@ import org.eclipse.edc.connector.dataplane.spi.pipeline.TransferService;
 import org.eclipse.edc.spi.types.domain.DataAddress;
 import org.eclipse.edc.spi.types.domain.transfer.DataFlowStartMessage;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class TransferServiceRegistryImplTest {
-    private TransferService transferService = mock(TransferService.class);
-    private TransferService transferService2 = mock(TransferService.class);
-    
-    private DataFlowStartMessage request = createRequest().build();
-    private TransferServiceSelectionStrategy transferServiceSelectionStrategy = mock(TransferServiceSelectionStrategy.class);
-    @SuppressWarnings("unchecked")
-    private ArgumentCaptor<Stream<TransferService>> streamCaptor = ArgumentCaptor.forClass(Stream.class);
-    
-    private TransferServiceRegistryImpl registry = new TransferServiceRegistryImpl(transferServiceSelectionStrategy);
+
+    private final TransferService transferService = mock();
+    private final TransferService transferService2 = mock();
+    private final DataFlowStartMessage request = createRequest().build();
+
+    private final TransferServiceRegistryImpl registry = new TransferServiceRegistryImpl();
     
     @Test
     void resolveTransferService_noServicesRegistered_shouldReturnNull() {
@@ -79,45 +71,6 @@ class TransferServiceRegistryImplTest {
         var service = registry.resolveTransferService(request);
         
         assertThat(service).isEqualTo(transferService);
-    }
-    
-    @Test
-    void resolveTransferService_noPriorityAndNoneCanHandle_shouldApplyStrategyWithEmptyStream() {
-        registry.registerTransferService(transferService);
-        registry.registerTransferService(transferService2);
-        when(transferService.canHandle(request)).thenReturn(false);
-        when(transferService2.canHandle(request)).thenReturn(false);
-        
-        registry.resolveTransferService(request);
-        
-        verify(transferServiceSelectionStrategy).chooseTransferService(eq(request), streamCaptor.capture());
-        assertThat(streamCaptor.getValue()).isEmpty();
-    }
-    
-    @Test
-    void resolveTransferService_noPriorityAndOneCanHandle_shouldApplyStrategyWithOneService() {
-        registry.registerTransferService(transferService);
-        registry.registerTransferService(transferService2);
-        when(transferService.canHandle(request)).thenReturn(true);
-        when(transferService2.canHandle(request)).thenReturn(false);
-        
-        registry.resolveTransferService(request);
-        
-        verify(transferServiceSelectionStrategy).chooseTransferService(eq(request), streamCaptor.capture());
-        assertThat(streamCaptor.getValue()).containsExactly(transferService);
-    }
-    
-    @Test
-    void resolveTransferService_noPriorityAndAllCanHandle_shouldApplyStrategyWithAllServices() {
-        registry.registerTransferService(transferService);
-        registry.registerTransferService(transferService2);
-        when(transferService.canHandle(request)).thenReturn(true);
-        when(transferService2.canHandle(request)).thenReturn(true);
-        
-        registry.resolveTransferService(request);
-        
-        verify(transferServiceSelectionStrategy).chooseTransferService(eq(request), streamCaptor.capture());
-        assertThat(streamCaptor.getValue()).containsExactly(transferService, transferService2);
     }
 
     private DataFlowStartMessage.Builder createRequest() {
