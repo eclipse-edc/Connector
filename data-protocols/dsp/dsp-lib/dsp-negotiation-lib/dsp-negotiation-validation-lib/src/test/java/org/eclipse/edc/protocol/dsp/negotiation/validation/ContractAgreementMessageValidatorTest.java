5d0f68d2a3d2ce561b611ccfd7f97f5116f338c1
ndr_brt <andrea.bertagnolli@gmail.com>
2026-04-01 16:27:50 +0200

    refactor: validate assignee and assigner in agreement

--- data-protocols/dsp/dsp-lib/dsp-negotiation-lib/dsp-negotiation-validation-lib/src/test/java/org/eclipse/edc/protocol/dsp/negotiation/validation/ContractAgreementMessageValidatorTest.java
+++ data-protocols/dsp/dsp-lib/dsp-negotiation-lib/dsp-negotiation-validation-lib/src/test/java/org/eclipse/edc/protocol/dsp/negotiation/validation/ContractAgreementMessageValidatorTest.java
@@ -15,6 +15,7 @@
 package org.eclipse.edc.protocol.dsp.negotiation.validation;
 
 import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonObject;
 import org.eclipse.edc.jsonld.spi.JsonLdNamespace;
 import org.eclipse.edc.validator.spi.ValidationFailure;
@@ -22,21 +23,33 @@ import org.eclipse.edc.validator.spi.Validator;
 import org.eclipse.edc.validator.spi.Violation;
 import org.junit.jupiter.api.Test;
 
+import static jakarta.json.Json.createArrayBuilder;
+import static jakarta.json.Json.createObjectBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.list;
+import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.ID;
 import static org.eclipse.edc.jsonld.spi.JsonLdKeywords.TYPE;
+import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.ODRL_ASSIGNEE_ATTRIBUTE;
+import static org.eclipse.edc.jsonld.spi.PropertyAndTypeNames.ODRL_ASSIGNER_ATTRIBUTE;
 import static org.eclipse.edc.junit.assertions.AbstractResultAssert.assertThat;
+import static org.eclipse.edc.protocol.dsp.spi.type.DspNegotiationPropertyAndTypeNames.DSPACE_PROPERTY_AGREEMENT_TERM;
 import static org.eclipse.edc.protocol.dsp.spi.type.DspNegotiationPropertyAndTypeNames.DSPACE_TYPE_CONTRACT_AGREEMENT_MESSAGE_TERM;
 
 class ContractAgreementMessageValidatorTest {
-    
+
     private static final JsonLdNamespace DSP_NAMESPACE = new JsonLdNamespace("http://www.w3.org/ns/dsp#");
     private final Validator<JsonObject> validator = ContractAgreementMessageValidator.instance(DSP_NAMESPACE);
 
     @Test
     void shouldSucceed_whenObjectIsValid() {
-        var input = Json.createObjectBuilder()
+        var input = createObjectBuilder()
                 .add(TYPE, Json.createArrayBuilder().add(DSP_NAMESPACE.toIri(DSPACE_TYPE_CONTRACT_AGREEMENT_MESSAGE_TERM)))
+                .add(DSP_NAMESPACE.toIri(DSPACE_PROPERTY_AGREEMENT_TERM), Json.createArrayBuilder().add(
+                        createObjectBuilder()
+                                .add(ODRL_ASSIGNER_ATTRIBUTE, id("assigner"))
+                                .add(ODRL_ASSIGNEE_ATTRIBUTE, id("assignee"))
+                        )
+                )
                 .build();
 
         var result = validator.validate(input);
@@ -46,13 +59,36 @@ class ContractAgreementMessageValidatorTest {
 
     @Test
     void shouldFail_whenMandatoryFieldsAreMissing() {
-        var input = Json.createObjectBuilder()
+        var input = createObjectBuilder()
                 .build();
 
         var result = validator.validate(input);
 
         assertThat(result).isFailed().extracting(ValidationFailure::getViolations).asInstanceOf(list(Violation.class))
-                .hasSize(1)
-                .anySatisfy(violation -> assertThat(violation.path()).isEqualTo(TYPE));
+                .hasSize(2)
+                .anySatisfy(violation -> assertThat(violation.path()).isEqualTo(TYPE))
+                .anySatisfy(violation -> assertThat(violation.path()).isEqualTo(DSP_NAMESPACE.toIri(DSPACE_PROPERTY_AGREEMENT_TERM)));
+    }
+
+    @Test
+    void shouldFail_whenMandatoryFieldsInAgreementAreMissing() {
+        var input = createObjectBuilder()
+                .add(TYPE, Json.createArrayBuilder().add(DSP_NAMESPACE.toIri(DSPACE_TYPE_CONTRACT_AGREEMENT_MESSAGE_TERM)))
+                .add(DSP_NAMESPACE.toIri(DSPACE_PROPERTY_AGREEMENT_TERM), Json.createArrayBuilder().add(
+                                createObjectBuilder()
+                        )
+                )
+                .build();
+
+        var result = validator.validate(input);
+
+        assertThat(result).isFailed().extracting(ValidationFailure::getViolations).asInstanceOf(list(Violation.class))
+                .hasSize(2)
+                .anySatisfy(violation -> assertThat(violation.path()).isEqualTo(DSP_NAMESPACE.toIri(DSPACE_PROPERTY_AGREEMENT_TERM) + "/" + ODRL_ASSIGNER_ATTRIBUTE))
+                .anySatisfy(violation -> assertThat(violation.path()).isEqualTo(DSP_NAMESPACE.toIri(DSPACE_PROPERTY_AGREEMENT_TERM) + "/" + ODRL_ASSIGNEE_ATTRIBUTE));
+    }
+
+    private JsonArrayBuilder id(String id) {
+        return createArrayBuilder().add(Json.createObjectBuilder().add(ID, id));
     }
 }
